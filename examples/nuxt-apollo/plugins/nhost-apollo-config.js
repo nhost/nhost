d2c5916c07a456f17753e7ab491a2357dbfecddb
elitan <johan@eliasson.me>
2021-03-26 20:58:45 +0100

    upgraded @nhost/nuxt package

--- examples/nuxt-apollo/plugins/nhost-apollo-config.js
+++ examples/nuxt-apollo/plugins/nhost-apollo-config.js
@@ -1,7 +1,7 @@
 export default(ctx) => {
   return {
-    httpEndpoint: 'https://hasura-<YOUR ID>.nhost.app/v1/graphql',
-    wsEndpoint: 'wss://hasura-<YOUR ID>.nhost.app/v1/graphql',
+    httpEndpoint: 'https://hasura-fcb83854.nhost.app/v1/graphql',
+    wsEndpoint: 'wss://hasura-fcb83854.nhost.app/v1/graphql',
     getAuth: () => `Bearer ${
       ctx.$nhost.auth.getJWTToken()
     }`
