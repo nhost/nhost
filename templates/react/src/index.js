e9173d9d1a8d47142b8e51151de52441ff8755e0
yashank09 <yashank@hey.com>
2021-07-01 03:46:31 -0700

    adds nhost-auth

--- templates/react/src/index.js
+++ templates/react/src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { NhostApolloProvider } from "@nhost/react-apollo";
+import { NhostAuthProvider } from "@nhost/react-auth";
+import { auth } from "./utils/nhost";
 import { App } from './App';
 
 const root = document.getElementById("root");
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <NhostAuthProvider auth={auth}>
+      <NhostApolloProvider
+        auth={auth}
+        gqlEndpoint="https://hasura-YOUR_GRAPHQL_API.nhost.app/v1/graphql"
+      >
+        <App />
+      </NhostApolloProvider>
+    </NhostAuthProvider>
   </React.StrictMode>, root
-)
+);
