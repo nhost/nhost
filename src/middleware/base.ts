e9b08db88f02cf7d93ae7fd884ade468d8cbc5c8
elitan <johan@eliasson.me>
2021-07-03 18:38:31 +0200

    auth / refresh token middleware updated

--- src/middleware/base.ts
+++ src/middleware/base.ts
@@ -1,55 +1,57 @@
-import { Request, Response, NextFunction } from 'express'
-import { getClaims, getPermissionVariablesFromClaims } from '@/jwt'
-import { gqlSdk } from '@/utils/gqlSDK'
+import { Request, Response, NextFunction } from "express";
+import { getClaims, getPermissionVariablesFromClaims } from "@/jwt";
+import { gqlSdk } from "@/utils/gqlSDK";
 
-export default async function (req: Request, res: Response, next: NextFunction) {
-  let permissionVariables = null
+export default async function (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  let permissionVariables = null;
   try {
-    permissionVariables = getPermissionVariablesFromClaims(getClaims(req.headers.authorization))
+    permissionVariables = getPermissionVariablesFromClaims(
+      getClaims(req.headers.authorization)
+    );
   } catch (e) {
     // noop
   }
 
-  console.log({ permissionVariables })
-  //
+  console.log({ permissionVariables });
 
-  const auth = permissionVariables
-    ? {
-        loggedIn: true,
-        userId: permissionVariables['user-id'],
-        defaultRole: permissionVariables['default-role']
+  req.auth = null;
+  if (permissionVariables) {
+    req.auth = {
+      userId: permissionVariables["user-id"],
+      defaultRole: permissionVariables["default-role"],
+    };
+
+    req.logger.debug(
+      `Request from user ${req.auth.userId}(${req.auth.defaultRole})`,
+      {
+        userId: req.auth.userId,
+        defaultRole: req.auth.defaultRole,
       }
-    : {
-        loggedIn: false,
-        userId: null,
-        defaultRole: null
-      }
+    );
+  }
 
-  req.auth = auth
+  if ("refreshToken" in req.query) {
+    req.logger.debug(`Request with refresh token ${req.refreshToken}`, {
+      refreshToken: req.refreshToken,
+    });
 
-  if ('refreshToken' in req.query) {
-    req.refreshToken = req.query.refreshToken as string
-    delete req.query.refreshToken
+    req.refreshToken = req.query.refreshToken as string;
+    delete req.query.refreshToken;
 
     const user = await gqlSdk
       .usersByRefreshToken({
-        refreshToken: req.refreshToken
+        refreshToken: req.refreshToken,
       })
-      .then((res) => res.authRefreshTokens[0]?.user)
+      .then((res) => res.authRefreshTokens[0]?.user);
 
     if (!user) {
-      return res.boom.unauthorized('Invalid or expired refresh token')
+      return res.boom.unauthorized("Invalid or expired refresh token");
     }
   }
 
-  req.logger.debug(
-    `Request from user ${req.auth.userId}(${req.auth.defaultRole}) with refresh token ${req.refreshToken}`,
-    {
-      userId: req.auth.userId,
-      defaultRole: req.auth.defaultRole,
-      refreshToken: req.refreshToken
-    }
-  )
-
-  next()
+  next();
 }
