234f47899cdb0aba6f06c85cca150aa3f905700f
elitan <johan@eliasson.me>
2021-08-19 09:12:05 +0200

    docker build updates

--- types/express-request.d.ts
+++ types/express-request.d.ts
@@ -1,17 +1,12 @@
-import PinoHttp from 'express-pino-logger';
-
 interface RequestAuth {
   userId: string;
   defaultRole: string;
   isAnonymous: boolean;
 }
 
-declare global {
-  namespace Express {
-    export interface Request {
-      log: PinoHttp.HttpLogger;
-      auth: RequestAuth | null;
-      refreshToken?: string;
-    }
+declare namespace Express {
+  export interface Request {
+    auth: RequestAuth | null;
+    refreshToken?: string;
   }
 }
