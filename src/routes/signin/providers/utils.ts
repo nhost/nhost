ab41a398ec2dfcf47a3b29a44f29de5ed888fc64
Nuno Pato <nunopato@gmail.com>
2021-09-06 18:37:19 +0000

    Use correct ENV var

--- src/routes/signin/providers/utils.ts
+++ src/routes/signin/providers/utils.ts
@@ -297,7 +297,7 @@ export const initProvider = <T extends Strategy>(
           !ENV.AUTH_ALLOWED_REDIRECT_URLS.includes(redirectUrl)
         ) {
           return res.boom.badRequest(
-            'Redirect URL is not same as APP_URL or not in ALLOWED_REDIRECT_URLS'
+            'The redirect URL is not the same as AUTH_CLIENT_URL nor is it in ALLOWED_REDIRECT_URLS'
           );
         }
 
