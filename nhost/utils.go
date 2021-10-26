7a1d12426d7d2b67fb966e1c6bf6998a1a8787e9
Mrinal Wahal <mrinal_w@rocketmail.com>
2021-10-26 15:07:23 +0530

    shifted syscall package to separate file for custom build

--- nhost/utils.go
+++ nhost/utils.go
@@ -8,7 +8,6 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
-	"syscall"
 	"time"
 )
 
@@ -73,17 +72,4 @@ func GetCurrentBranch() string {
 	}
 	payload := strings.Split(string(data), " ")
 	return strings.TrimSpace(filepath.Base(payload[1]))
-}
-
-//	Detects whether the host machine is running on Apple Silicon processor.
-func runningSilicon() bool {
-
-	r, err := syscall.Sysctl("sysctl.proc_translated")
-	if err != nil {
-		if err.Error() == "no such file or directory" {
-			return false
-		}
-	}
-
-	return r == "\x00\x00\x00" || r == "\x01\x00\x00"
 }
---
+++ util/silicon.go
@@ -0,0 +1,18 @@
+// +build darwin,arm64 darwin,amd64
+
+package util
+
+import "syscall"
+
+//	Detects whether the host machine is running on Apple Silicon processor.
+func RunningSilicon() bool {
+
+	r, err := syscall.Sysctl("sysctl.proc_translated")
+	if err != nil {
+		if err.Error() == "no such file or directory" {
+			return false
+		}
+	}
+
+	return r == "\x00\x00\x00" || r == "\x01\x00\x00"
+}
