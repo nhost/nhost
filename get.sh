d56231ba04d2e5de75814046d9f1d05cd9946cee
M. Frink ~ Lemur <frink@frinknet.com>
2022-01-04 07:40:08 -0600

    Reworked get.sh (#131)
    - Streamlined the whole install process (die as early as possible)
    - Fixed fringe cases where power is pulled in the middle of install
    - Fixed passing release numbers to properly install old releases
    - Added comments so that non-bash aficionados can follow along

--- get.sh
+++ get.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-# Some helpful functions
+# helper functions
 yell() { echo -e "${RED}FAILED> $* ${NC}" >&2; }
 die() { yell "$*"; exit 1; }
 try() { "$@" || die "failed executing: $*"; }
 log() { echo -e "--> $*"; }
+maybe_sudo() { $([ $NEED_SUDO ] && echo sudo) "$@"; }
 
-# Colors for colorizing
+# console colors
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 PURPLE='\033[0;35m'
@@ -14,18 +15,21 @@ BLUE='\033[0;34m'
 YELLOW='\033[0;33m'
 NC='\033[0m'
 
+# basic variables
 INSTALL_PATH=${INSTALL_PATH:-"/usr/local/bin"}
-NEED_SUDO=1
-
+NEED_SUDO=${NEED_SUDO:-1}
 REPO="nhost/cli"
 
-function maybe_sudo() {
-    if [[ "$NEED_SUDO" == '1' ]]; then
-        sudo "$@"
-    else
-        "$@"
-    fi
-}
+# check for existing installation
+hasCli=$(which nhost)
+if [ "$?" = "0" ]; then
+    log ""
+    log "${GREEN}You already have the Nhost CLI at '${hasCli}'${NC}"
+    export n=3
+    log "${YELLOW}Downloading again in $n seconds... Press Ctrl+C to cancel.${NC}"
+    log ""
+    sleep $n
+fi
 
 # check for curl
 hasCurl=$(which curl)
@@ -33,12 +37,16 @@ if [ "$?" = "1" ]; then
     die "You need to install curl to use this script."
 fi
 
+# get release version
 release=${1:-latest}
-
 log "Getting $release version..."
+if [[ "$release" == "latest" ]]; then
+    version=$(curl --silent "https://api.github.com/repos/$REPO/releases/latest" | grep '"tag_name":' |  sed -E 's/.*"([^"]+)".*/\1/')
+else
+    version=$(curl --silent "https://api.github.com/repos/nhost/cli/tags" | grep "name" | sed -E 's/.*"([^"]+)".*/\1/' | grep "$release")
+fi
 
-version=$(curl --silent "https://api.github.com/repos/$REPO/releases/latest" | grep '"tag_name":' |  sed -E 's/.*"([^"]+)".*/\1/')
-
+# check version exists
 if [ ! $version ]; then
     log "${YELLOW}"
     log "Failed while attempting to install Nhost CLI. Please manually install:"
@@ -51,20 +59,12 @@ if [ ! $version ]; then
     die "exiting..."
 fi
 
-log "Latest version is $version"
-
-# check for existing nhost installation
-hasCli=$(which nhost)
-if [ "$?" = "0" ]; then
-    log ""
-    log "${GREEN}You already have the Nhost CLI at '${hasCli}'${NC}"
-    export n=3
-    log "${YELLOW}Downloading again in $n seconds... Press Ctrl+C to cancel.${NC}"
-    log ""
-    sleep $n
+# show latest version
+if [[ "$release" == "latest" ]]; then
+    log "Latest version is $version"
 fi
 
-# get platform and arch
+# get platform
 platform='unknown'
 unamestr=`uname`
 if [[ "$unamestr" == 'Linux' ]]; then
@@ -75,40 +75,44 @@ elif [[ "$unamestr" == 'Windows' ]]; then
     platform='windows'
 fi
 
+# die for unknown platform
 if [[ "$platform" == 'unknown' ]]; then
     die "Unknown OS platform"
 fi
 
+# set arch
 arch='unknown'
 archstr=`uname -m`
 if [[ "$archstr" == 'x86_64' ]]; then
     arch='amd64'
 elif [[ "$archstr" == 'arm64' ]]; then
     arch='arm64'
-else 
+else
     arch='386'
 fi
 
-# some variables
-suffix="-${platform}-${arch}"
-targetFile="cli-$version$suffix.tar.gz"
+# variables for install
+targetFile="cli-${version}-${platform}-${arch}.tar.gz"
+url="https://github.com/${REPO}/releases/download/${version}/${targetFile}"
 
+# remove previous download
 if [ -e $targetFile ]; then
     rm $targetFile
 fi
 
-log "${PURPLE}Downloading Nhost for $platform-$arch to ${targetFile}${NC}"
-url=https://github.com/$REPO/releases/download/$version/$targetFile
+# tell what we are downloading
+log "${PURPLE}Downloading Nhost ${version} for ${platform}-${arch} to ${targetFile}${NC}"
 
+# download and extract files
 try curl -L -f -o $targetFile "$url"
-try chmod +x $targetFile
 try tar -xvf $targetFile
 try rm ./$targetFile
+try mv cli nhost
 
+# install and test
 log "${GREEN}Download complete!${NC}"
 echo
-try sudo mv ./cli ${INSTALL_PATH}/nhost
+try sudo_maybe mv ./nhost ${INSTALL_PATH}/nhost
 nhost version
 echo
 log "${BLUE}Use Nhost CLI with: nhost --help${NC}"
-
