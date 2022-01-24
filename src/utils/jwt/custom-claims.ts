6c8f81fb1ba2b9636f9c40acd4fbc0eb00d5cd44
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2022-01-24 20:38:38 +0100

    docs: improve lisibility of the main graphql/jsonata mechanism

--- src/utils/jwt/custom-claims.ts
+++ src/utils/jwt/custom-claims.ts
@@ -30,7 +30,16 @@ export const escapeValueToPg = (value: any): string => {
   }
 };
 
+/**
+ * Generate a GraphQL request from the values of all the keys of the given jwtFields
+ * All values are set together to prepare one single query, to avoid calling the
+ * server as many times as there are keys in jwtFields
+ * @param jwtFields
+ * @returns
+ */
 const createCustomFieldQuery = (jwtFields: Record<string, string>): string => {
+  // * Build the transitional Js object that will be used by json-to-graphql-query
+  // ! Advanced JSONata expressions won't work, only basic 'dot' paths e.g. author.books.title
   const fields = Object.values(jwtFields).reduce<Record<string, unknown>>(
     (aggr, path) => {
       set(aggr, path, true);
@@ -38,6 +47,7 @@ const createCustomFieldQuery = (jwtFields: Record<string, string>): string => {
     },
     {}
   );
+  // * Prepare the query so it will accept userId as a variable
   const query = {
     query: {
       __variables: {
@@ -51,35 +61,51 @@ const createCustomFieldQuery = (jwtFields: Record<string, string>): string => {
       },
     },
   };
+  // * Generate the GraphQL from the above object with json-to-graphql-query
   return jsonToGraphQLQuery(query);
 };
 
+/**
+ * Generate `x-hasura-` custom claims from a user id
+ * @param userId
+ * @returns
+ */
 export const generateCustomClaims = async (userId: string) => {
+  // * Don't error if no custom claim is configured, but return an empty object
   if (Object.keys(ENV.AUTH_JWT_CUSTOM_CLAIMS).length === 0) return {};
+  // * Generate the GraphQL request from the configuration
   const request = createCustomFieldQuery(ENV.AUTH_JWT_CUSTOM_CLAIMS);
   try {
+    // * Fetch user data that is required for all custom expression to be evaluated
     const {
       data: { user },
     } = await client.rawRequest(request, {
       userId,
     });
 
+    // * Aggregate each 'name' key of the object with its value evaluated in JSONata
     return Object.entries(ENV.AUTH_JWT_CUSTOM_CLAIMS).reduce<
       Record<string, unknown>
     >((aggr, [name, path]) => {
-      const expression = jsonata(path);
       try {
-        const jsonataValue = escapeValueToPg(
-          expression.evaluate(user, expression)
-        );
+        // * Parse the path into a JSONata AST
+        const expression = jsonata(path);
+        // * Evaluate the JSONata expression from the fetched user data
+        const value = expression.evaluate(user, expression);
+        // * Convert value into a PostgreSQL format that can be used by the Hasura permissions system
+        // * see {@link escapeValueToPg}
+        const jsonataValue = escapeValueToPg(value);
+        // * Add the result to the aggregated object, prefixed with `x-hasura-`
         aggr[`x-hasura-${name}`] = jsonataValue;
       } catch {
-        logger.warn(`Invalid JSONata expression`, { user, expression });
+        // * Don't raise errors if JSONata fails. Set the currently processed claim and log a warning
+        logger.warn(`Invalid JSONata expression`, { user, path });
         aggr[`x-hasura-${name}`] = null;
       }
       return aggr;
     }, {});
   } catch {
+    // * If the query fail, don't raise an error, but log a warning and return an empty object
     logger.warn(`Invalid custom JWT GraphQL Query`, { request });
     return {};
   }
