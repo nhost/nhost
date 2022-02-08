224243dc2825da742caa778314defc73e8f2e711
Guido Curcio <guidomaurocurcio@gmail.com>
2022-02-08 08:52:37 -0300

    Update constants.ts

--- docs/utils/constants.ts
+++ docs/utils/constants.ts
@@ -1,1 +1,1 @@
-export const DOCS_GITHUB_ENDPOINT = 'https://github.com/nhost/nhost/tree/main/docs/'
+export const DOCS_GITHUB_ENDPOINT = 'https://github.com/nhost/nhost/tree/main/docs/content/docs/'
