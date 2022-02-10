fc4055c807682305f45ad12a62a9abae00783a10
Vadim Smirnov <smirnou.vadzim@gmail.com>
2022-02-10 13:20:34 +0300

    comments resolve

--- docs/components/Container.tsx
+++ docs/components/Container.tsx
@@ -1,6 +1,6 @@
 export function Container({ children }) {
   return (
-    <div className="mx-10 px-2 sm:px-10 md:px-20 lg:px-0 flex flex-row md:max-w-container pb-20 md:mx-auto mt-8 lg:space-x-20 min-h-screen">
+    <div className="mx-10 px-2 sm:px-10 md:px-20 lg:px-0 flex flex-row md:max-w-container pb-20 md:mx-auto mt-8 lg:space-x-20">
       {children}
     </div>
   )
