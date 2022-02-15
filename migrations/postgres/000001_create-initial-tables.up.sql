195cee0572c59d5b129f0e385286507d67c3e790
Nuno Pato <nunopato@gmail.com>
2022-02-15 11:00:38 -0100

    replace "create or replace" with "drop/create" for triggers (#26)

--- migrations/postgres/000001_create-initial-tables.up.sql
+++ migrations/postgres/000001_create-initial-tables.up.sql
@@ -95,21 +95,25 @@ BEGIN
 END $$;
 
 -- triggers
-CREATE OR REPLACE TRIGGER set_storage_buckets_updated_at
+DROP TRIGGER IF EXISTS set_storage_buckets_updated_at ON storage.buckets;
+CREATE TRIGGER set_storage_buckets_updated_at
   BEFORE UPDATE ON storage.buckets
   FOR EACH ROW
   EXECUTE FUNCTION storage.set_current_timestamp_updated_at ();
 
-CREATE OR REPLACE TRIGGER set_storage_files_updated_at
+DROP TRIGGER IF EXISTS set_storage_files_updated_at ON storage.files;
+CREATE TRIGGER set_storage_files_updated_at
   BEFORE UPDATE ON storage.files
   FOR EACH ROW
   EXECUTE FUNCTION storage.set_current_timestamp_updated_at ();
 
+DROP TRIGGER IF EXISTS check_default_bucket_delete ON storage.buckets;
 CREATE TRIGGER check_default_bucket_delete
   BEFORE DELETE ON storage.buckets
   FOR EACH ROW
     EXECUTE PROCEDURE storage.protect_default_bucket_delete ();
 
+DROP TRIGGER IF EXISTS check_default_bucket_update ON storage.buckets;
 CREATE TRIGGER check_default_bucket_update
   BEFORE UPDATE ON storage.buckets
   FOR EACH ROW
