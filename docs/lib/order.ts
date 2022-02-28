9c11023dea063a1264a80b39839cc70139122e52
Johan Eliasson <johan@eliasson.me>
2022-02-28 17:51:12 +0100

    added Google

--- docs/lib/order.ts
+++ docs/lib/order.ts
@@ -2,12 +2,24 @@ export const orderTwo = {
   'get-started': {
     'quick-start': ['index', 'schema', 'javascript-client', 'permissions'],
     authentication: ['index'],
-    'cli-workflow': ['index', 'workflow-setup', 'install-cli', 'local-changes', 'metadata-and-serverless-functions'],
+    'cli-workflow': [
+      'index',
+      'workflow-setup',
+      'install-cli',
+      'local-changes',
+      'metadata-and-serverless-functions'
+    ],
     upgrade: ['index']
   },
   platform: {
     database: ['index', 'permissions', 'graphql'],
-    authentication: ['index', 'user-management', 'sign-in-methods', 'social-login'],
+    authentication: [
+      'index',
+      'user-management',
+      'sign-in-methods',
+      'social-login',
+      'sign-in-with-google'
+    ],
     storage: ['index'],
     'serverless-functions': ['index', 'event-triggers'],
     nhost: ['index', 'environment-variables', 'github-integration', 'local-development']
