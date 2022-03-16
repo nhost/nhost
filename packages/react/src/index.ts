54d6d0a9b2f4172ecc1c2f6f81facfd383b6691a
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2022-03-16 12:26:09 +0100

    fix: correct paths to cjs dist file, and reactivate warning suppression about useLayoutEffect

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,11 +1,11 @@
 import { NhostClient as VanillaClient, NhostClientConstructorParams } from '@nhost/nhost-js'
 
-// import { suppressConsoleMessage } from './utils'
+import { suppressConsoleMessage } from './utils'
 /* Suppress NextJS/SSR warning. It triggers a warning whereas xstate does not use
  * useLayoutEffect on server-side
  * See https://gist.github.com/gaearon/e7d97cdf38a2907924ea12e4ebdf3c85
  */
-// suppressConsoleMessage('useLayoutEffect does nothing on the server', 'error')
+suppressConsoleMessage('useLayoutEffect does nothing on the server', 'error')
 
 export * from './hooks'
 export * from './provider'
