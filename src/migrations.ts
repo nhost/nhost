6a0856a9e6a18bf264579a783adae6c55efc4351
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2022-04-03 23:22:37 +0200

    fix: revert 00002 migration name to previous name when migration fails because of it

--- src/migrations.ts
+++ src/migrations.ts
@@ -1,10 +1,12 @@
 import { migrate } from '@djgrant/postgres-migrations';
 import { Client } from 'pg';
-// import logger from './logger';
+import fs from 'fs/promises';
+import path from 'path';
 import { ENV } from './utils/env';
+import { logger } from './logger';
 
 export async function applyMigrations(): Promise<void> {
-  // logger.info('Applying migrations');
+  logger.info('Applying migrations');
 
   const dbConfig = {
     connectionString: ENV.HASURA_GRAPHQL_DATABASE_URL,
@@ -16,6 +18,30 @@ export async function applyMigrations(): Promise<void> {
     await migrate({ client }, './migrations', {
       migrationTableName: 'auth.migrations',
     });
+  } catch (error: any) {
+    const userFieldsMigration = path.join(
+      process.cwd(),
+      'migrations',
+      '00002_custom-user-fields.sql'
+    );
+    if (error.message.includes('00002_custom-user-fields.sql')) {
+      logger.info(
+        'Correcting legacy 00002 migration name introduced in v0.2.1'
+      );
+      await fs.stat(userFieldsMigration);
+      await fs.rename(
+        userFieldsMigration,
+        userFieldsMigration.replace(
+          '00002_custom-user-fields',
+          '00002_custom_user_fields'
+        )
+      );
+      await migrate({ client }, './migrations', {
+        migrationTableName: 'auth.migrations',
+      });
+    } else {
+      throw Error(error);
+    }
   } finally {
     await client.end();
   }
