99fd7603c5d5fd0af8325c50c9bfb72ba033ce93
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2022-04-19 14:01:43 +0200

    fix: correct redirectTo
    Allow redirection either starting with AUTH_CLIENT_URL or being strictly included in
    AUTH_ACCESS_CONTROL_ALLOWED_REDIRECT_URLS. When no redirectTo is set, falls back to AUTH_CLIENT_URL
    
    fix #137

--- src/validation/fields.ts
+++ src/validation/fields.ts
@@ -60,10 +60,12 @@ export const metadata = Joi.object().default({}).example({
   lastName: 'Smith',
 });
 
-export const redirectTo = Joi.string()
+export const redirectTo = Joi.alternatives()
   .default(ENV.AUTH_CLIENT_URL)
-  .regex(new RegExp('^' + ENV.AUTH_CLIENT_URL))
-  .valid(...ENV.AUTH_ACCESS_CONTROL_ALLOWED_REDIRECT_URLS)
+  .try(
+    Joi.valid(...ENV.AUTH_ACCESS_CONTROL_ALLOWED_REDIRECT_URLS),
+    Joi.string().regex(new RegExp('^' + ENV.AUTH_CLIENT_URL))
+  )
   .example(`${ENV.AUTH_CLIENT_URL}/catch-redirection`);
 
 export const uuid = Joi.string()
