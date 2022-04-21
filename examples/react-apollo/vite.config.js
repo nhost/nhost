b9cf8172a0334c83d35e3370f6f97a2d4a5eb3ca
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2022-04-21 11:24:32 +0200

    chore(examples): include react-apollo and nextjs examples back in the monorepo workspace

--- examples/react-apollo/vite.config.js
+++ examples/react-apollo/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [tsconfigPaths(), react()]
 })
