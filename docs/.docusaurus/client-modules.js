4c68897d6bffd8b9b9d0f7bacc24bde8c482a613
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2022-04-22 20:17:57 +0200

    chore: remove .docusaurus files

--- docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/pilou/dev/nhost/nhost/node_modules/.pnpm/infima@0.2.0-alpha.38/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/pilou/dev/nhost/nhost/node_modules/.pnpm/@docusaurus+theme-classic@2.0.0-beta.18_6256d3e9141e918aba8f6a22e11a83de/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/pilou/dev/nhost/nhost/node_modules/.pnpm/@docusaurus+theme-classic@2.0.0-beta.18_6256d3e9141e918aba8f6a22e11a83de/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
-  require('/Users/pilou/dev/nhost/nhost/docs/src/css/custom.css'),
-];
