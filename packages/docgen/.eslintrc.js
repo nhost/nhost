2f3715d02a771157d71c2d15c631e35c825dd98c
Szilárd Dóró <szilard.doro@bishop.hu>
2022-05-02 14:05:26 +0200

    fixed linter errors

---
+++ packages/docgen/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: '../../.eslintrc.js',
+  ignorePatterns: [
+    '**/node_modules/**',
+    '**/dist/**',
+    '**/build/**',
+    '**/test/**',
+    '**/__tests__/**',
+    '**/__mocks__/**',
+    'tsup.config.ts',
+    'jest.config.ts'
+  ],
+  parserOptions: {
+    project: ['./tsconfig.json']
+  },
+  plugins: ['@typescript-eslint', 'simple-import-sort'],
+  rules: {
+    // we are already using a Prettier formatter and this rule is conflicting
+    // with it
+    'simple-import-sort/imports': 'off'
+  }
+}
