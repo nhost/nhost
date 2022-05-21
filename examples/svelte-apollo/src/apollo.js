2d15118aaeaa026af02da1d1073639036e53a968
sonichigo <kurosakiichigo.songoku@gmail.com>
2022-05-21 09:17:20 +0530

    :rocket: New Updates
    Signed-off-by: sonichigo <kurosakiichigo.songoku@gmail.com>

--- examples/svelte-apollo/src/apollo.js
+++ examples/svelte-apollo/src/apollo.js
@@ -4,13 +4,4 @@ import { NhostClient } from '@nhost/nhost-js'
 const nhost = new NhostClient({
   backendUrl: import.meta.env.VITE_NHOST_URL ?? 'http://localhost:1337'
 })
-
-const accessToken = nhost.auth.getAccessToken()
-
-export const client = createApolloClient({ nhost, headers:
-  {
-  Authorization:`Bearer ${accessToken}`,
-  "content-type": "application/json",
-  'x-hasura-admin-secret': import.meta.env.VITE_HASURA_ADMIN_SECRET ?? 'nhost-admin-secret'
-} 
-})
+export const client = createApolloClient({ nhost })
