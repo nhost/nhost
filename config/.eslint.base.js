2cb1c36c76d99d7dbf0d6eb7bd4116ed8f82de1f
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2022-06-02 21:35:04 +0200

    chore: remove line from .eslint.base.js (merge mistake), remove eslintrc in example folder

--- config/.eslint.base.js
+++ config/.eslint.base.js
@@ -59,8 +59,7 @@ module.exports = {
         allowArrowFunction: true,
         allowAnonymousFunction: true
       }
-    ],
-    'import/extensions': ['error', { js: 'always' }]
+    ]
   },
   overrides: [
     {
