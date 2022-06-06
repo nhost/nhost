73b1569af39ce26dcc7fb5f25ed2cae84508decd
Svarto <svarto@protonmail.com>
2022-06-06 09:18:04 +0200

    enabled change password to work with ticket

--- src/routes/user/password.ts
+++ src/routes/user/password.ts
@@ -7,15 +7,39 @@ import { Joi, password } from '@/validation';
 
 export const userPasswordSchema = Joi.object({
   newPassword: password.required(),
+  ticket: Joi.string(),
 }).meta({ className: 'UserPasswordSchema' });
 
 export const userPasswordHandler: RequestHandler<
   {},
   {},
-  { newPassword: string }
+  { newPassword: string, ticket?: string }
 > = async (req, res) => {
-  // check if user is logged in
-  if (!req.auth?.userId) {
+
+  const { ticket } = req.body;
+
+  // get the user from the ticket
+  const ticketed_user = await gqlSdk
+      .users({
+        where: {
+          _and: [
+            {
+              ticket: {
+                _eq: ticket,
+              },
+            },
+            {
+              ticketExpiresAt: {
+                _gt: new Date(),
+              },
+            },
+          ],
+        },
+      })
+      .then((gqlRes) => gqlRes.users[0]);
+
+  // check if user is logged in or has valid ticket
+  if (!req.auth?.userId && !ticketed_user) {
     return sendError(res, 'unauthenticated-user');
   }
 
@@ -23,7 +47,7 @@ export const userPasswordHandler: RequestHandler<
 
   const newPasswordHash = await hashPassword(newPassword);
 
-  const { userId } = req.auth;
+  const userId = req.auth?.userId || ticketed_user.id;
 
   const { user } = await gqlSdk.user({
     id: userId,
