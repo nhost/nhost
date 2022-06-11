49cdb2843ef9c5875e0394e16e085dc533c76702
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2022-06-11 21:58:03 +0200

    test: add one test

--- packages/vue/tests/main.test.ts
+++ packages/vue/tests/main.test.ts
@@ -1,12 +1,20 @@
 import { ref } from 'vue'
 import { nestedUnref } from '../src/helpers'
 describe('nestedUnref', () => {
-  it('shoud unref ref values in a unref property', async () => {
+  it('should unref ref values in a unref property', async () => {
     const a = ref('value of a')
     const b = ref('value of b')
     const input = { metadata: { a, b } }
     const result = nestedUnref(input)
 
     expect(result).toEqual({ metadata: { a: a.value, b: b.value } })
+  })
+
+  it('should unref a property', async () => {
+    const metadataValue = { firstName: 'John', lastName: 'Doe' }
+    const input = { metadata: ref(metadataValue) }
+    const result = nestedUnref(input)
+
+    expect(result).toEqual({ metadata: metadataValue })
   })
 })
