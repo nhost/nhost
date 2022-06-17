0b0efc947ddaf501d68919cf104eff6f3f008000
Johan Eliasson <johan@eliasson.me>
2022-06-17 09:43:40 +0200

    small refactor + prettier

--- src/routes/user/password.ts
+++ src/routes/user/password.ts
@@ -13,24 +13,23 @@ export const userPasswordSchema = Joi.object({
 export const userPasswordHandler: RequestHandler<
   {},
   {},
-  { newPassword: string, ticket?: string }
+  { newPassword: string; ticket?: string }
 > = async (req, res) => {
-
   const { ticket } = req.body;
 
   // get the user from the ticket, but if no ticket then return null
-  const userByTicket = await getUserByTicket(ticket)
+  const userByTicket = await getUserByTicket(ticket);
 
   // check if user is logged in or has valid ticket
   if (!req.auth?.userId && !userByTicket) {
     return sendError(res, 'unauthenticated-user');
   }
 
-  const { newPassword } = req.body;
-
-  const newPasswordHash = await hashPassword(newPassword);
-
   const userId = req.auth?.userId || userByTicket?.id;
+
+  if (!userId) {
+    return sendError(res, 'unauthenticated-user');
+  }
 
   const { user } = await gqlSdk.user({
     id: userId,
@@ -39,6 +38,9 @@ export const userPasswordHandler: RequestHandler<
   if (!user) {
     throw new Error('Unable to get user');
   }
+
+  const { newPassword } = req.body;
+  const newPasswordHash = await hashPassword(newPassword);
 
   // set new password for user
   await gqlSdk.updateUser({
