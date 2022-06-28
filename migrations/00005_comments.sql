0b926e3d16ee4a7462ae30f0978b0f29a46818ca
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2022-06-28 08:38:51 +0200

    docs: complete comments

--- migrations/00005_comments.sql
+++ migrations/00005_comments.sql
@@ -1,4 +1,3 @@
--- TODO complete comments
 comment on schema auth is 'Schema required by Hasura Auth to work. Don''t modify its structure as Hasura Auth relies on it to function properly.';
 comment on table auth.migrations is 'Internal table for tracking migrations. Don''t modify its structure as Hasura Auth relies on it to function properly.';
 comment on column auth.migrations.id is 'Unique identifier for the migration.';
@@ -16,7 +15,7 @@ comment on table auth.users is 'User account information. Don''t modify its stru
 comment on column auth.users.id is 'Auto-generated unique identifier for the user.';
 comment on column auth.users.created_at is 'Auto-generated timestamp for when the user was created.';
 comment on column auth.users.updated_at is 'Auto-generated timestamp for when the user was last updated.';
--- TODO partially implemented
+-- TODO only partially implemented - last_seen should be updated every time a JWT is generated for the user
 comment on column auth.users.last_seen is 'Timestamp on when an access token has been created for the user.';
 comment on column auth.users.disabled is 'Disabled users cannot sign in.';
 comment on column auth.users.display_name is 'User display name, populated on sign-up either from the options sent in the http payload, or from the provider when signing up with Oauth. Falls back to the email when not provided.';
@@ -28,8 +27,7 @@ comment on column auth.users.password_hash is 'User password hashed with bcrypt.
 comment on column auth.users.email_verified is 'Is true when the user''s email address has been verified with a link sent by email.';
 comment on column auth.users.phone_number_verified is 'Is true when the user''s phone number has been verified with code sent by SMS.';
 comment on column auth.users.new_email is 'Tentative new email address, used to verify the new email address.';
--- TODO not sure if this field is needed
--- comment on column auth.users.otp_method_last_used is '';
+comment on column auth.users.otp_method_last_used is 'Last used OTP method for the user.';
 comment on column auth.users.otp_hash is 'One-time password hashed with bcrypt, used to verify the user''s phone number.';
 comment on column auth.users.otp_hash_expires_at is 'One-time password hash expiration timestamp.';
 comment on column auth.users.default_role is 'The user''s default role to be used in the x-hasura-default-role JWT claim.';
@@ -43,18 +41,18 @@ comment on table auth.refresh_tokens is 'User refresh tokens. Hasura auth uses t
 comment on column auth.refresh_tokens.refresh_token is 'Auto-generated refresh token id.';
 comment on column auth.refresh_tokens.created_at is 'Auto-generated timestamp for when the refresh token was created.';
 comment on column auth.refresh_tokens.expires_at is 'Auto-generated timestamp for when the refresh token will expire.';
-comment on column auth.refresh_tokens.user_id is 'User id associated with the refresh token.';
+comment on column auth.refresh_tokens.user_id is 'User identifier associated with the refresh token.';
 comment on table auth.user_providers is 'Active providers for a given user. Don''t modify its structure as Hasura Auth relies on it to function properly.';
-comment on column auth.user_providers.id is 'Oauth provider id referenced by auth..providers.id.';
+comment on column auth.user_providers.id is 'Oauth provider identifier referenced by auth..providers.id.';
 comment on column auth.user_providers.created_at is 'Auto-generated timestamp for when the provider was added to the user.';
 comment on column auth.user_providers.updated_at is 'Auto-generated timestamp for when this provider''s user was last updated.';
-comment on column auth.user_providers.user_id is 'User id referenced by auth.users.id.';
--- comment on column auth.user_providers.access_token is '';
--- comment on column auth.user_providers.refresh_token is '';
--- comment on column auth.user_providers.provider_id is '';
--- comment on column auth.user_providers.provider_user_id is '';
+comment on column auth.user_providers.user_id is 'User identifier referenced by auth.users.id.';
+comment on column auth.user_providers.access_token is 'Access token given by the provider.';
+comment on column auth.user_providers.refresh_token is 'Refresh token given by the provider.';
+comment on column auth.user_providers.provider_id is 'Provider identifier referenced by auth.providers.id.';
+comment on column auth.user_providers.provider_user_id is 'Unique user identifier determined by the provider.';
 comment on table auth.user_roles is 'Roles of users. Don''t modify its structure as Hasura Auth relies on it to function properly.';
 comment on column auth.user_roles.id is 'Auto-generated unique identifier for the user role.';
 comment on column auth.user_roles.created_at is 'Auto-generated timestamp for when the role was added to the user.';
-comment on column auth.user_roles.user_id is 'User id referenced by auth.users.id.';
-comment on column auth.user_roles.role is 'Role id referenced by auth.roles.role.';
+comment on column auth.user_roles.user_id is 'User identifier referenced by auth.users.id.';
+comment on column auth.user_roles.role is 'Role identifier referenced by auth.roles.role.';
