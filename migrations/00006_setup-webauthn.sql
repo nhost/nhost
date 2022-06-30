c08167dd7399c988f5d3ba677f6e51c9a927c76c
Asen Lekov <asenlekoff@gmail.com>
2022-06-30 21:14:04 +0300

    chore(webauthn): rename webauth migration and add comment on table

--- migrations/00006_setup-webauthn.sql
+++ migrations/00006_setup-webauthn.sql
@@ -13,6 +13,9 @@ CREATE TABLE auth.user_authenticators(
     transports varchar(255) DEFAULT '' NOT NULL
 );
 
+COMMENT ON TABLE auth.user_authenticators IS 'User webauthn authenticators. Don''t modify its structure as Hasura Auth relies on it to function properly.';
+
+
 -- FKs
 ALTER TABLE auth.user_authenticators
   ADD CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES auth.users (id) ON UPDATE CASCADE ON DELETE CASCADE;
