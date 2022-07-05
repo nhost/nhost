f522808e0f2856406e0c6dab5e02dcc65f52338e
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2022-07-05 11:06:39 +0200

    test: David's feedback

--- packages/config-schema/tests/index.test.ts
+++ packages/config-schema/tests/index.test.ts
@@ -2,7 +2,8 @@ import fs from 'fs'
 import path from 'path'
 import yaml from 'yaml'
 import Ajv from 'ajv'
-
+import glob from 'glob'
+import findWorkspaceRoot from '@pnpm/find-workspace-dir'
 import draft6MetaSchema from 'ajv/dist/refs/json-schema-draft-06.json'
 
 const ajv = new Ajv()
@@ -11,23 +12,43 @@ ajv.addMetaSchema(draft6MetaSchema)
 const schema = JSON.parse(fs.readFileSync('schema.json', 'utf8'))
 
 describe('test valid schemas', () => {
-  const basePath = path.join(__dirname, 'valid')
-  fs.readdirSync(basePath).forEach((file) => {
-    it(`${file} should be valid`, () => {
-      const contents = yaml.parse(fs.readFileSync(path.join(basePath, file), 'utf8'))
-      const validation = ajv.validate(schema, contents)
-      expect(validation).toBe(true)
+  const validList = ['base']
+  validList
+    .map((name) => path.join(__dirname, 'valid', `${name}.yaml`))
+    .forEach((file) => {
+      it(`${file} should be valid`, () => {
+        const contents = yaml.parse(fs.readFileSync(file, 'utf8'))
+        const validation = ajv.validate(schema, contents)
+        expect(validation).toBe(true)
+      })
     })
-  })
 })
 
 describe('test invalid schemas', () => {
-  const basePath = path.join(__dirname, 'invalid')
-  fs.readdirSync(basePath).forEach((file) => {
-    it(`${file} should be valid`, () => {
-      const contents = yaml.parse(fs.readFileSync(path.join(basePath, file), 'utf8'))
-      const validation = ajv.validate(schema, contents)
-      expect(validation).toBe(false)
+  const invalidList = ['cli']
+  invalidList
+    .map((name) => path.join(__dirname, 'invalid', `${name}.yaml`))
+    .forEach((file) => {
+      it(`${file} should be invalid`, () => {
+        const contents = yaml.parse(fs.readFileSync(file, 'utf8'))
+        const validation = ajv.validate(schema, contents)
+        expect(validation).toBe(false)
+      })
     })
+})
+
+describe('test configurations from the monorepo', async () => {
+  const root = await findWorkspaceRoot(process.cwd())
+  const nhostConfigs = glob.sync('**/nhost/config.yaml', {
+    cwd: root,
+    absolute: true,
+    realpath: true
   })
+  for (const file of nhostConfigs) {
+    it(`${file} should be valid`, () => {
+      const contents = yaml.parse(fs.readFileSync(file, 'utf8'))
+      const validation = ajv.validate(schema, contents)
+      expect(validation).toBe(true)
+    })
+  }
 })
