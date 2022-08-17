0680fa53bd644b1ccc81ef5765762212d4cc7054
Siarhei Lipchyk <contact@siarhei.dev>
2022-08-17 15:32:38 +0200

    Bump hasura version to 2.10.1

--- nhost/compose/config.go
+++ nhost/compose/config.go
@@ -43,7 +43,7 @@ const (
 	svcFunctionsDefaultImage = "nhost/functions:0.0.3"
 	svcMinioDefaultImage     = "minio/minio:RELEASE.2022-07-08T00-05-23Z"
 	svcMailhogDefaultImage   = "mailhog/mailhog"
-	svcHasuraDefaultImage    = "hasura/graphql-engine:v2.10.0"
+	svcHasuraDefaultImage    = "hasura/graphql-engine:v2.10.1"
 	svcTraefikDefaultImage   = "traefik:v2.8"
 	// --
 
