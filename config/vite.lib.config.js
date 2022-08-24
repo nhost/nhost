8b8412d5fadea6a62f29659fe36b39584547013c
Johan Eliasson <johan@eliasson.me>
2022-08-24 10:10:46 +0200

    exclude paths for vitest

--- config/vite.lib.config.js
+++ config/vite.lib.config.js
@@ -38,7 +38,8 @@ export default defineConfig({
     coverage: {
       enabled: process.env.CI === 'true',
       reporter: ['json']
-    }
+    },
+    exclude: ['node_modules', 'dist', '.git', '.cache', 'build', '.nhost']
   },
   build: {
     sourcemap: true,
