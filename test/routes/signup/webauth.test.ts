b5933359ce3da86047e8cc23e73ad623c2fef605
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2022-09-01 10:40:04 +0200

    test: adjust tests to less restrictive webauthn policy

--- test/routes/signup/webauth.test.ts
+++ test/routes/signup/webauth.test.ts
@@ -121,16 +121,21 @@ describe('webauthn', () => {
       },
       pubKeyCredParams: [
         { alg: -7, type: 'public-key' },
+        { alg: -8, type: 'public-key' },
+        { alg: -36, type: 'public-key' },
+        { alg: -37, type: 'public-key' },
+        { alg: -38, type: 'public-key' },
+        { alg: -39, type: 'public-key' },
         { alg: -257, type: 'public-key' },
+        { alg: -258, type: 'public-key' },
+        { alg: -259, type: 'public-key' },
       ],
       timeout: 60000,
       attestation: 'indirect',
       excludeCredentials: [],
       authenticatorSelection: {
-        requireResidentKey: true,
-        residentKey: 'required',
-        authenticatorAttachment: 'platform',
-        userVerification: 'required',
+        requireResidentKey: false,
+        userVerification: 'preferred',
       },
     });
   });
