4e30b4ee213e4c9012e9cd6595817b295d260f1d
Johan Eliasson <johan@eliasson.me>
2022-09-06 21:14:21 +0200

    allowed updated

--- packages/stripe-graphql/src/utils.ts
+++ packages/stripe-graphql/src/utils.ts
@@ -3,14 +3,30 @@ import { Context } from './types'
 export const isAllowed = (stripeCustomerId: string, context: Context) => {
   const { request, allowedCustomerIds } = context
 
-  console.log(request.headers)
-  return true
-
   const adminSecretFromHeader = request.headers.get('x-hasura-admin-secret')
   const adminSecret = process.env.NHOST_ADMIN_SECRET
 
-  if (adminSecretFromHeader !== adminSecret && !allowedCustomerIds?.includes(stripeCustomerId)) {
-    return false
+  if (adminSecretFromHeader === adminSecret) {
+    return true
   }
-  return true
+
+  if (allowedCustomerIds?.includes(stripeCustomerId)) {
+    return true
+  }
+
+  // check if the request is from Hasura
+  const nhostWebhookSecretFromHeader = request.headers.get('x-nhost-webhook-secret')
+  const nhostWebhookSecret = process.env.NHOST_WEBHOOK_SECRET
+
+  if (nhostWebhookSecretFromHeader === nhostWebhookSecret) {
+    // if the request is from Hsaura, we can trust the `x-hasura-role` header.
+    const role = request.headers.get('x-hasura-role')
+
+    // this is the same as if the request was using the correct admin secret
+    if (role === 'admin') {
+      return true
+    }
+  }
+
+  return false
 }
