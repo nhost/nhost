3e4b58e144048393b7f10f6212312bf76b4ce828
Siarhei Lipchyk <contact@siarhei.dev>
2022-09-07 12:54:52 +0200

    Bump nhost/functions to 0.1.3

--- nhost/compose/config.go
+++ nhost/compose/config.go
@@ -35,7 +35,7 @@ const (
 	svcPostgresDefaultImage  = "nhost/postgres:12-v0.0.6"
 	svcAuthDefaultImage      = "nhost/hasura-auth:0.10.0"
 	svcStorageDefaultImage   = "nhost/hasura-storage:0.2.4"
-	svcFunctionsDefaultImage = "nhost/functions:0.1.2"
+	svcFunctionsDefaultImage = "nhost/functions:0.1.3"
 	svcMinioDefaultImage     = "minio/minio:RELEASE.2022-07-08T00-05-23Z"
 	svcMailhogDefaultImage   = "mailhog/mailhog"
 	svcHasuraDefaultImage    = "hasura/graphql-engine:v2.10.1"
