fdf69932db1c326ac08b558e4824b349c4f9d00c
Nuno Pato <nunopato@gmail.com>
2022-09-28 16:43:47 +0000

    Update hasura auth to v0.13.2 (#371)

--- nhost/compose/config.go
+++ nhost/compose/config.go
@@ -35,7 +35,7 @@ const (
 
 	// default docker images
 	svcPostgresDefaultImage  = "nhost/postgres:14.5-20220831-1"
-	svcAuthDefaultImage      = "nhost/hasura-auth:0.13.1"
+	svcAuthDefaultImage      = "nhost/hasura-auth:0.13.2"
 	svcStorageDefaultImage   = "nhost/hasura-storage:0.2.4"
 	svcFunctionsDefaultImage = "nhost/functions:0.1.5"
 	svcMinioDefaultImage     = "minio/minio:RELEASE.2022-07-08T00-05-23Z"
