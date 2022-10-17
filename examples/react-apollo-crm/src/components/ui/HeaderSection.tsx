1c0b9c642a16727dee78b470e41f3cdcd3b0b376
Johan Eliasson <johan@eliasson.me>
2022-10-17 19:37:49 +0200

    removing crm example

--- examples/react-apollo-crm/src/components/ui/HeaderSection.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export function HeaderSection({ children }: { children: React.ReactNode }) {
-  return <div className="mt-2 md:flex md:items-center md:justify-between">{children}</div>
-}
