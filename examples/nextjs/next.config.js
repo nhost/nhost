087a62972d07aa17c35adb90b5f5fda5003ea16c
Szilárd Dóró <doroszilard@gmail.com>
2022-11-01 18:48:20 +0100

    fix Next.js example

--- examples/nextjs/next.config.js
+++ examples/nextjs/next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    esmExternals: 'loose'
+  }
+}
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
