4cf69d03c020c1bc29b02869edf13772fccc73cd
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2022-11-02 17:11:03 +0100

    refactor: vite 3 in vue example

--- examples/vue-apollo/vite.config.js
+++ examples/vue-apollo/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 import vue from '@vitejs/plugin-vue'
-import vuetify from '@vuetify/vite-plugin'
+import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
