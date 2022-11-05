b7fb18eda381cd00934903dd2e1ad9701883befe
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2022-11-05 22:25:00 +0100

    feat: from/to arguments

--- packages/functions/functions-bundler.js
+++ packages/functions/functions-bundler.js
@@ -3,17 +3,17 @@ const esbuild = require('esbuild')
 const path = require('path')
 const fs = require('fs-extra')
 
-const origin = 'tests'
-const destination = 'functions'
+const from = process.argv.find((arg) => arg.startsWith('--from'))?.replace('--from', '') || 'tests'
+const to = process.argv.find((arg) => arg.startsWith('--to'))?.replace('--to', '') || 'functions'
 
-fs.emptydirSync(destination)
+fs.emptydirSync(to)
 
-fs.writeJsonSync(path.join(destination, 'package.json'), {
+fs.writeJsonSync(path.join(to, 'package.json'), {
   name: '@nhost-dev/functions',
   version: '0.0.0'
 })
 
-fs.writeJsonSync(path.join(destination, 'tsconfig.json'), {
+fs.writeJsonSync(path.join(to, 'tsconfig.json'), {
   compilerOptions: {
     allowJs: true,
     skipLibCheck: true,
@@ -26,40 +26,41 @@ fs.writeJsonSync(path.join(destination, 'tsconfig.json'), {
   include: ['**/*.js']
 })
 
-const targetFile = (file) => {
-  const basename = path.basename(file, path.extname(file))
-  return path.join(destination, path.dirname(file), basename + '.js')
+const targetPath = (filePath) => {
+  const fileName = `${path.basename(filePath, path.extname(filePath))}.js`
+  return path.join(to, path.dirname(filePath), fileName)
 }
 
-const bundle = (file) => {
-  const destinationFile = targetFile(file)
-  console.log(`Bundling ${path.join(origin, file)} -> ${destinationFile}`)
+const bundle = (filePath) => {
+  const destinationPath = targetPath(filePath)
+  console.log(`Bundling ${path.join(from, filePath)} -> ${destinationPath}`)
   esbuild.buildSync({
-    entryPoints: [path.join(origin, file)],
+    entryPoints: [path.join(from, filePath)],
     platform: 'node',
-    outfile: destinationFile,
+    outfile: destinationPath,
     target: ['node16'],
     format: 'esm',
     bundle: true
   })
 }
 
-const remove = async (file) => {
-  const outfile = targetFile(file)
-  console.log(`Removing ${path.join(origin, file)} -> ${outfile}`)
-  fs.unlinkSync(outfile)
+const remove = async (filePath) => {
+  const destinationPath = targetPath(filePath)
+  console.log(`Removing ${path.join(from, filePath)} -> ${destinationPath}`)
+  fs.unlinkSync(destinationPath)
 }
 
-const watcher = chokidar.watch('**/*.{js,ts}', {
-  cwd: path.join(__dirname, origin),
-  ignored: ['**/_*/**', '**/*.spec.{js,ts}', '**/tests/**', '**/*.test.{js,ts}']
-})
-
-watcher.on('add', bundle).on('change', bundle).on('unlink', remove)
-
-watcher.on('ready', () => {
-  if (!process.argv.includes('--watch')) {
-    return watcher.close()
-  }
-  console.log('Watching for changes...')
-})
+const watcher = chokidar
+  .watch('**/*.{js,ts}', {
+    cwd: path.join(__dirname, from),
+    ignored: ['**/_*/**', '**/*.spec.{js,ts}', '**/tests/**', '**/*.test.{js,ts}']
+  })
+  .on('add', bundle)
+  .on('change', bundle)
+  .on('unlink', remove)
+  .on('ready', () => {
+    if (!process.argv.includes('--watch')) {
+      return watcher.close()
+    }
+    console.log('Watching for changes...')
+  })
