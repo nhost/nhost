7724ab2861f4c64906f6c9575836ee3abf8c367e
Szilárd Dóró <doroszilard@gmail.com>
2022-11-11 18:30:14 +0100

    fix (dashboard): failing tests

--- dashboard/vitest.config.ts
+++ dashboard/vitest.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   // @ts-ignore
-  plugins: [tsconfigPaths(), react()],
+  plugins: [tsconfigPaths({ projects: ['./tsconfig.test.json'] }), react()],
   test: {
     environment: 'jsdom',
     globals: true,
