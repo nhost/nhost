435efd2bc509d10d9a122064e8302b16d42f6991
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2022-11-21 15:22:05 +0100

    fix: 🐛 Set same-site cookie to `lax`

--- packages/hasura-auth-js/src/local-storage.ts
+++ packages/hasura-auth-js/src/local-storage.ts
@@ -84,7 +84,7 @@ export const localStorageSetter = (
         if (value) {
           // TODO: Set expires based on the actual refresh token expire time
           // For now, we're using 30 days so the cookie is not removed when the browser is closed because if `expiers` is omitted, the cookie becomes a session cookie.
-          Cookies.set(key, value, { expires: 30 })
+          Cookies.set(key, value, { httpOnly: false, sameSite: 'lax', expires: 30 })
         } else {
           Cookies.remove(key)
         }
