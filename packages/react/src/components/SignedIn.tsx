964af2912b38779953453cdf8499de2763e5a052
Johan Eliasson <johan@eliasson.me>
2022-11-22 17:04:39 +0100

    inline docs

--- packages/react/src/components/SignedIn.tsx
+++ packages/react/src/components/SignedIn.tsx
@@ -1,5 +1,29 @@
 import { useAuthenticationStatus } from '../useAuthenticationStatus'
 
+/**
+ * Use `<SignedIn />` to control the rendering of components for users. Components inside `<SignedIn />` are only rendered if the user is authenticated.
+ *
+ * @example
+ * ```tsx
+ * import { NhostProvider, SignedOut } from "@nhost/react";
+ * import { nhost } from '@/utils/nhost';
+ *
+ * function Page() {
+ *   return (
+ *    <NhostProvider nhost={nhost}>
+ *     <SignedIn>
+ *      <h1>Only rendered if the user is authenticated</h1>
+ *    </SignedIn>
+ *   </NhostProvider>
+ *  )
+ * }
+ * ```
+ *
+ * @docs https://docs.nhost.io/reference/react/signed-in
+ *
+ * @component
+ */
+
 export function SignedIn({ children }: React.PropsWithChildren<unknown>): JSX.Element | null {
   const { isAuthenticated } = useAuthenticationStatus()
 
--- packages/react/src/components/SignedOut.tsx
+++ packages/react/src/components/SignedOut.tsx
@@ -1,5 +1,29 @@
 import { useAuthenticationStatus } from '../useAuthenticationStatus'
 
+/**
+ * Use `<SignedOut />` to control the rendering of components for users. Components inside `<SignedOut />` are only rendered if the user is not authenticated.
+ *
+ * @example
+ * ```tsx
+ * import { NhostProvider, SignedOut } from "@nhost/react";
+ * import { nhost } from '@/utils/nhost';
+ *
+ * function Page() {
+ *   return (
+ *    <NhostProvider nhost={nhost}>
+ *     <SignedOut>
+ *      <h1>Only rendered if the user is not authenticated</h1>
+ *    </SignedOut>
+ *   </NhostProvider>
+ *  )
+ * }
+ * ```
+ *
+ * @docs https://docs.nhost.io/reference/react/signed-out
+ *
+ * @component
+ */
+
 export function SignedOut({ children }: React.PropsWithChildren<unknown>): JSX.Element | null {
   const { isAuthenticated } = useAuthenticationStatus()
 
