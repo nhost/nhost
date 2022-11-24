3d151c448cf0b3a2722b7a06c3c2e43b2adf43d0
Szilárd Dóró <doroszilard@gmail.com>
2022-11-24 19:42:23 +0100

    chore(precommit): extend precommit hook with docgen build

--- config/.lintstagedrc.js
+++ config/.lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   '(packages|integrations)/(docgen|hasura-auth-js|hasura-storage-js|nextjs|nhost-js|react|core|vue)/src/**/*.{js,ts,jsx,tsx}':
-    ['pnpm docgen', 'git add docs'],
+    ['pnpm build --filter=@nhost/docgen', 'pnpm docgen', 'git add docs'],
   '(nhost-cloud.yaml|**/nhost/config.yaml)': () => [
     'pnpm sync-versions',
     "git add ':(glob)**/nhost/config.yaml'"
