ef117c284e8047fb147ce0fd1e9949fd78786bee
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2022-11-29 11:49:59 +0100

    fix: 🐛 Distribute the access token to all the sub-clients

--- packages/nhost-js/src/clients/nhost.ts
+++ packages/nhost-js/src/clients/nhost.ts
@@ -56,12 +56,11 @@ export class NhostClient {
     this.functions = createFunctionsClient({ adminSecret, ...urlParams })
     this.graphql = createGraphqlClient({ adminSecret, ...urlParams })
 
-    // * Set current token if token is already accessable
-    this.storage.setAccessToken(this.auth.getAccessToken())
-    this.functions.setAccessToken(this.auth.getAccessToken())
-    this.graphql.setAccessToken(this.auth.getAccessToken())
-
-    this.auth.client?.onStart(() => {
+    this.auth.client.onStart(() => {
+      // * Set current token if token is already accessable
+      this.storage.setAccessToken(this.auth.getAccessToken())
+      this.functions.setAccessToken(this.auth.getAccessToken())
+      this.graphql.setAccessToken(this.auth.getAccessToken())
       // * Set access token when signing out
       this.auth.onAuthStateChanged((_event, session) => {
         if (_event === 'SIGNED_OUT') {
