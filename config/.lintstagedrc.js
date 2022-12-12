195adfb04a2da9bb321826dc53972866cceeaa79
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2022-12-12 21:26:21 +0100

    chore: remove generated documentation from the repository

--- config/.lintstagedrc.js
+++ config/.lintstagedrc.js
@@ -1,6 +1,4 @@
 module.exports = {
-  '(packages|integrations)/(docgen|hasura-auth-js|hasura-storage-js|nextjs|nhost-js|react|core|vue)/src/**/*.{js,ts,jsx,tsx}':
-    ['pnpm docgen', 'git add docs'],
   '(nhost-cloud.yaml|**/nhost/config.yaml)': () => [
     'pnpm sync-versions',
     "git add ':(glob)**/nhost/config.yaml'"
