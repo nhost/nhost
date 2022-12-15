8d494710518e46913467022695981727814a7cca
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2022-12-15 15:01:02 +0100

    fix: correct sha256

--- migrations/00011_add_refresh_token_hash_column.sql
+++ migrations/00011_add_refresh_token_hash_column.sql
@@ -1,5 +1,5 @@
 BEGIN;
 ALTER TABLE "auth"."refresh_tokens"
-    ADD COLUMN "hashed_refresh_token" VARCHAR(255) GENERATED ALWAYS AS (refresh_token (refresh_token::text::bytea)) STORED;
+    ADD COLUMN "hashed_refresh_token" VARCHAR(255) GENERATED ALWAYS AS (sha256 (refresh_token::text::bytea)) STORED;
 COMMIT;
 
