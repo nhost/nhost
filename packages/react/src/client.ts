1be6d324558bb2707fc13a3011a21ef2962179db
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2023-01-02 16:19:00 +0100

    Only export what is required by the user or `@nhost/nextjs`

--- packages/react/src/client.ts
+++ packages/react/src/client.ts
@@ -1,12 +1,18 @@
 import {
+  AuthMachine,
   BackendUrl,
   NhostAuthConstructorParams,
-  NhostClient as VanillaNhostClient,
+  NhostClient as _VanillaNhostClient,
+  NhostSession,
+  NHOST_REFRESH_TOKEN_KEY,
   Subdomain
 } from '@nhost/nhost-js'
 
-export * from '@nhost/nhost-js'
-export { VanillaNhostClient }
+export type { NhostSession, NhostAuthConstructorParams, AuthMachine, Subdomain, BackendUrl }
+export { NHOST_REFRESH_TOKEN_KEY }
+
+/** @internal */
+export const VanillaNhostClient = _VanillaNhostClient
 
 export interface NhostReactClientConstructorParams
   extends Partial<BackendUrl>,
