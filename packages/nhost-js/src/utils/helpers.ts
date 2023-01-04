437dacaa9efedd6658bfbb0b6ed05d880366c0ac
Szilárd Dóró <doroszilard@gmail.com>
2023-01-04 19:00:58 +0100

    chore(nhost-js): refactor port default value

--- packages/nhost-js/src/utils/helpers.ts
+++ packages/nhost-js/src/utils/helpers.ts
@@ -29,7 +29,7 @@ export function urlFromSubdomain(
   // check if subdomain is [http[s]://]localhost[:port] or [http[s]://]localdev[:port]
   const subdomainLocalhostFound = subdomain.match(LOCALHOST_REGEX)
   if (subdomainLocalhostFound?.groups) {
-    const { protocol = 'http', host, port = 1337 } = subdomainLocalhostFound.groups
+    const { protocol = 'http', host, port } = subdomainLocalhostFound.groups
 
     const urlFromEnv = getValueFromEnv(service)
     if (urlFromEnv) {
@@ -37,11 +37,11 @@ export function urlFromSubdomain(
     }
 
     if (host === 'localhost') {
-      return `${protocol}://localhost:${port}/v1/${service}`
+      return `${protocol}://localhost:${port || 1337}/v1/${service}`
     }
 
     // we omit the port if it's the default port
-    if (port !== 1337) {
+    if (port) {
       return `${protocol}://${host}.nhost.run:${port}/v1/${service}`
     }
 
