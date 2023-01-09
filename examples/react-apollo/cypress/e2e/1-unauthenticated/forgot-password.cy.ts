d0d94d9239338a2c432d097fbe856facc73d645b
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2023-01-09 13:27:28 +0100

    chore: use email+password sign-up

--- examples/react-apollo/cypress/e2e/1-unauthenticated/forgot-password.cy.ts
+++ examples/react-apollo/cypress/e2e/1-unauthenticated/forgot-password.cy.ts
@@ -3,18 +3,19 @@ import { faker } from '@faker-js/faker'
 context('Forgot password', () => {
   it('should reset password', () => {
     const email = faker.internet.email()
+    const password = faker.internet.password(8)
 
-    cy.signUpAndConfirmEmail(email)
-    cy.signOut().then((e) => {
-      cy.visit('/sign-in')
-      cy.findByRole('button', { name: /Continue with email \+ password/i }).click()
-      cy.findByRole('button', { name: /Forgot Password?/i }).click()
+    cy.signUpEmailPassword(email, password)
+    cy.contains('Verification email sent').should('be.visible')
 
-      cy.findByPlaceholderText('Email Address').type(email)
-      cy.findByRole('button', { name: /Reset your password/i }).click()
+    cy.visit('/sign-in')
+    cy.findByRole('button', { name: /Continue with email \+ password/i }).click()
+    cy.findByRole('button', { name: /Forgot Password?/i }).click()
 
-      cy.confirmEmail(email)
-      cy.contains('Profile page')
-    })
+    cy.findByPlaceholderText('Email Address').type(email)
+    cy.findByRole('button', { name: /Reset your password/i }).click()
+
+    cy.confirmEmail(email)
+    cy.contains('Profile page')
   })
 })
