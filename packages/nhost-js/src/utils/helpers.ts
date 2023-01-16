53a704fc7db931b6894284a14a26192957530fb3
Szilárd Dóró <doroszilard@gmail.com>
2023-01-16 19:20:37 +0100

    chore(nhost-js): add TODO comments

--- packages/nhost-js/src/utils/helpers.ts
+++ packages/nhost-js/src/utils/helpers.ts
@@ -1,6 +1,7 @@
 import { NhostClientConstructorParams } from './types'
 
 // a port can be a number or a placeholder string with leading and trailing double underscores, f.e. "8080" or "__PLACEHOLDER_NAME__"
+// TODO: Change `localdev` to `local`
 const LOCALHOST_REGEX =
   /^((?<protocol>http[s]?):\/\/)?(?<host>(localhost|localdev))(:(?<port>(\d+|__\w+__)))?$/
 
@@ -44,6 +45,7 @@ export function urlFromSubdomain(
       return `${protocol || 'http'}://localhost:${port || 1337}/v1/${service}`
     }
 
+    // TODO: Change `localdev` to `local`
     if (port) {
       return `${protocol || 'https'}://localdev.nhost.run:${port}/v1/${service}`
     }
