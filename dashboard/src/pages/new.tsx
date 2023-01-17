27f4ee22937b58b4e1b5adc144348c1d59d991e4
Szilárd Dóró <doroszilard@gmail.com>
2023-01-17 09:25:46 +0100

    feat(dashboard): dark mode for project creation

--- dashboard/src/pages/new.tsx
+++ dashboard/src/pages/new.tsx
@@ -5,8 +5,10 @@ import features from '@/data/features.json';
 import { useGetAllUserWorkspacesAndApplications } from '@/hooks/useGetAllUserWorkspacesAndApplications';
 import { useLazyRefetchUserData } from '@/hooks/useLazyRefetchUserData';
 import { useSubmitState } from '@/hooks/useSubmitState';
+import { Alert } from '@/ui/Alert';
 import DelayedLoading from '@/ui/DelayedLoading';
 import { Modal } from '@/ui/Modal';
+import Box from '@/ui/v2/Box';
 import Button from '@/ui/v2/Button';
 import Checkbox from '@/ui/v2/Checkbox';
 import IconButton from '@/ui/v2/IconButton';
@@ -221,19 +223,19 @@ export function NewProjectPageContent({
   if (!selectedWorkspace) {
     return (
       <Container>
-        <div className="mx-auto my-64 max-w-full bg-white subpixel-antialiased">
+        <Box className="mx-auto my-64 max-w-full subpixel-antialiased">
           <div className="relative transform">
             <div className="mx-auto max-w-3xl text-center">
-              <h1 className="text-center text-6xl font-semibold text-dark">
+              <Text variant="h1" className="text-center text-6xl font-semibold">
                 Workspace Error
-              </h1>
-              <pre className="mt-2">
+              </Text>
+              <Text className="mt-2">
                 There is no workspace. You must create a workspace before
                 creating an application.
-              </pre>
+              </Text>
             </div>
           </div>
-        </div>
+        </Box>
       </Container>
     );
   }
@@ -344,12 +346,18 @@ export function NewProjectPageContent({
               helperText={
                 <div className="grid max-w-xs grid-flow-row gap-2">
                   {passwordError && (
-                    <Text variant="subtitle2" className="!text-red">
+                    <Text
+                      variant="subtitle2"
+                      sx={{
+                        color: (theme) =>
+                          `${theme.palette.error.main} !important`,
+                      }}
+                    >
                       {passwordError}
                     </Text>
                   )}
 
-                  <div className="font-medium text-greyscaleDark">
+                  <Box className="font-medium">
                     The root Postgres password for your database - it must be
                     strong and hard to guess.{' '}
                     <Button
@@ -362,7 +370,7 @@ export function NewProjectPageContent({
                     >
                       Generate a password
                     </Button>
-                  </div>
+                  </Box>
                 </div>
               }
               onChange={async (e) => {
@@ -477,8 +485,8 @@ export function NewProjectPageContent({
                 const checked = plan.id === currentPlan.id;
 
                 return (
-                  <div
-                    className="border-t border-gray-200 py-4 last-of-type:border-b"
+                  <Box
+                    className="border-t py-4 last-of-type:border-b"
                     key={currentPlan.id}
                   >
                     <Checkbox
@@ -532,7 +540,7 @@ export function NewProjectPageContent({
                         setPlan(currentPlan);
                       }}
                     />
-                  </div>
+                  </Box>
                 );
               })}
             </div>
@@ -540,13 +548,14 @@ export function NewProjectPageContent({
         </div>
 
         {submitState.error && (
-          <div className="mt-10 rounded-md bg-warning px-4 py-2 text-dark">
-            <Text className="font-medium text-textOrange">Warning</Text>
-            <Text className="pt-2 font-medium text-dimBlack" size="normal">
+          <Alert severity="error" className="text-left">
+            <Text className="font-medium">Warning</Text>{' '}
+            <Text className="font-medium">
               {submitState.error &&
-                getErrorMessage(submitState.error, 'application')}
+                getErrorMessage(submitState.error, 'application')}{' '}
+              asdsda
             </Text>
-          </div>
+          </Alert>
         )}
 
         <div className="flex justify-end">
