9ffb4d0295098321bfbdc9d3afbf557b1580d53a
Szilárd Dóró <doroszilard@gmail.com>
2023-01-19 08:51:05 +0100

    fix(dashboard): use fallbacks for services

--- dashboard/src/utils/env.ts
+++ dashboard/src/utils/env.ts
@@ -23,64 +23,70 @@ export function getHasuraAdminSecret() {
 }
 
 /**
- * Suffix for the migration service exposed by Hasura.
- */
-export function getMigrationsApiSuffix() {
-  return process.env.NEXT_PUBLIC_NHOST_MIGRATIONS_API_SUFFIX;
-}
-
-/**
- * Port of the migration service exposed by Hasura.
- */
-export function getMigrationsApiPort() {
-  return process.env.NEXT_PUBLIC_NHOST_MIGRATIONS_API_PORT;
-}
-
-/**
  * Custom URL of the Auth service.
  */
 export function getAuthServiceUrl() {
-  return process.env.NEXT_PUBLIC_NHOST_AUTH_URL;
+  return (
+    process.env.NEXT_PUBLIC_NHOST_AUTH_URL || 'https://local.auth.nhost.run/v1'
+  );
 }
 
 /**
  * Custom URL of the GraphQL service.
  */
 export function getGraphqlServiceUrl() {
-  return process.env.NEXT_PUBLIC_NHOST_GRAPHQL_URL;
+  return (
+    process.env.NEXT_PUBLIC_NHOST_GRAPHQL_URL ||
+    'https://local.graphql.nhost.run/v1'
+  );
 }
 
 /**
  * Custom URL of the Storage service.
  */
 export function getStorageServiceUrl() {
-  return process.env.NEXT_PUBLIC_NHOST_STORAGE_URL;
+  return (
+    process.env.NEXT_PUBLIC_NHOST_STORAGE_URL ||
+    'https://local.storage.nhost.run/v1'
+  );
 }
 
 /**
  * Custom URL of the Functions service.
  */
 export function getFunctionsServiceUrl() {
-  return process.env.NEXT_PUBLIC_NHOST_FUNCTIONS_URL;
+  return (
+    process.env.NEXT_PUBLIC_NHOST_FUNCTIONS_URL ||
+    'https://local.functions.nhost.run/v1'
+  );
 }
 
 /**
  * Custom URL of the Hasura service.
  */
 export function getHasuraConsoleServiceUrl() {
-  return process.env.NEXT_PUBLIC_NHOST_HASURA_CONSOLE_URL;
+  return (
+    process.env.NEXT_PUBLIC_NHOST_HASURA_CONSOLE_URL ||
+    'https://local.hasura.nhost.run'
+  );
 }
 
 /**
  * Custom URL of the Hasura Migrations API.
  */
 export function getHasuraMigrationsApiUrl() {
-  return process.env.NEXT_PUBLIC_NHOST_HASURA_MIGRATIONS_API_URL;
+  return (
+    process.env.NEXT_PUBLIC_NHOST_HASURA_MIGRATIONS_API_URL ||
+    'https://local.hasura.nhost.run/v1/migrations'
+  );
 }
 
 /**
  * Custom URL of the Hasura Schema and Metadata API.
  */
 export function getHasuraSchemaApiUrl() {
-  return process.env.NEXT_PUBLIC_NHOST_HASURA_SCHEMA_API_URL;
+  return (
+    process.env.NEXT_PUBLIC_NHOST_HASURA_SCHEMA_API_URL ||
+    'https://local.hasura.nhost.run/v1/schema'
+  );
 }
