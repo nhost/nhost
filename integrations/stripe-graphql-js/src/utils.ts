bcb3b79add86f085d268a1831f6a700178f4ffd3
Johan Eliasson <johan@eliasson.me>
2023-01-27 08:54:23 +0100

    updates

--- integrations/stripe-graphql-js/src/utils.ts
+++ integrations/stripe-graphql-js/src/utils.ts
@@ -8,7 +8,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2022-08-01'
+  apiVersion: '2022-11-15'
 })
 
 export const getUserClaims = (req: Request): UserHasuraClaims | undefined => {
