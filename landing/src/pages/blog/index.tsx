8affa64777ced2a97f7194c412681e2473fb415b
Johan Eliasson <johan@eliasson.me>
2023-02-01 10:35:08 +0100

    cleanup

--- landing/src/pages/blog/index.tsx
+++ landing/src/pages/blog/index.tsx
@@ -1,8 +1,10 @@
-import glob from "fast-glob";
-import Link from "next/link";
-import * as path from "path";
+import { ReactElement } from 'react'
+import { Layout } from '@/components/Layout'
+import glob from 'fast-glob'
+import Link from 'next/link'
+import * as path from 'path'
 
-export default function ArticlesIndex({ articles }: { articles: any }) {
+export default function Page({ articles }: { articles: any }) {
   return (
     <div>
       {articles.map((article: any) => {
@@ -14,44 +16,50 @@ export default function ArticlesIndex({ articles }: { articles: any }) {
               <Link href={`/blog/${article.slug}`}>Read more</Link>
             </div>
           </div>
-        );
+        )
       })}
     </div>
-  );
+  )
 }
 
-async function importArticle(articleFilename: any) {
-  // if we change the location of this folder, make sure the path is correct!
-  let { meta, default: component } = await import(`./${articleFilename}`);
-  return {
-    slug: articleFilename.replace(/(\/index)?\.mdx$/, ""),
-    ...meta,
-    component,
-  };
+Page.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>
 }
 
+// SSR
 export async function getStaticProps() {
+  // TODO: Move this function to a separate file
+  const importArticle = async (articleFilename: any) => {
+    // if we change the location of this folder, make sure the path is correct!
+    let { meta, default: component } = await import(`./${articleFilename}`)
+    return {
+      slug: articleFilename.replace(/(\/index)?\.mdx$/, ''),
+      ...meta,
+      component,
+    }
+  }
+
   // TODO: move this function to a separate file
   const getAllArticles = async () => {
-    let articleFilenames = await glob(["*.mdx", "*/index.mdx"], {
-      cwd: path.join(process.cwd(), "src/pages/blog"),
-    });
+    let articleFilenames = await glob(['*.mdx', '*/index.mdx'], {
+      cwd: path.join(process.cwd(), 'src/pages/blog'),
+    })
 
-    let articles = await Promise.all(articleFilenames.map(importArticle));
+    let articles = await Promise.all(articleFilenames.map(importArticle))
 
-    console.log(articles);
+    console.log(articles)
 
-    return articles;
+    return articles
 
     // TODO: fix this sort
     // return articles.sort((a: any, z: any) => {
     //   return !!(new Date(z.date) > new Date(a.date));
     // }
-  };
+  }
 
   return {
     props: {
       articles: (await getAllArticles()).map(({ component, ...meta }) => meta),
     },
-  };
+  }
 }
