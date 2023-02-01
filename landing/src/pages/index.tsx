11ea747280b771a69843993dc26b5d5a1497bccb
Szilárd Dóró <doroszilard@gmail.com>
2023-02-01 18:33:10 +0100

    fix service card layout

--- landing/src/pages/index.tsx
+++ landing/src/pages/index.tsx
@@ -117,7 +117,7 @@ export default function IndexPage() {
           </div>
         </div>
 
-        <div className="grid grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
           <ServiceCard
             icon={<></>}
             title="Postgres Database"
