958add249de9c005076838898addbc78e6c59959
Szilárd Dóró <doroszilard@gmail.com>
2023-02-01 09:09:07 +0100

    cleanup main

--- landing/prettier.config.js
+++
@@ -1,9 +0,0 @@
-// prettier.config.js
-module.exports = {
-  bracketSpacing: true,
-  semi: true,
-  trailingComma: "all",
-  printWidth: 80,
-  tabWidth: 2,
-  plugins: [require("prettier-plugin-tailwindcss")],
-};
