798585c585e669219503ffc4ddd311ca4a075ca3
Szilárd Dóró <doroszilard@gmail.com>
2023-02-07 16:26:38 +0100

    improve customers layout

--- landing/src/components/common/icons/XCircleIcon/XCircleIcon.tsx
+++ landing/src/components/common/icons/XCircleIcon/XCircleIcon.tsx
@@ -13,24 +13,24 @@ export default function XCircleIcon(props: XCircleIconProps) {
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <g clip-path="url(#clip0_51_937)">
+      <g clipPath="url(#clip0_51_937)">
         <path
           d="M7.99967 14.6668C11.6816 14.6668 14.6663 11.6821 14.6663 8.00016C14.6663 4.31826 11.6816 1.3335 7.99967 1.3335C4.31778 1.3335 1.33301 4.31826 1.33301 8.00016C1.33301 11.6821 4.31778 14.6668 7.99967 14.6668Z"
           stroke="white"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M10 6L6 10"
           stroke="white"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M6 6L10 10"
           stroke="white"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
--- landing/src/components/customers/CustomerLayout/CustomerLayout.tsx
+++ landing/src/components/customers/CustomerLayout/CustomerLayout.tsx
@@ -46,59 +46,81 @@ export default function CustomerLayout({
           </Link>
         </div>
 
-        <div className="col-span-3 grid grid-flow-row gap-8">
+        <div className="order-2 col-span-5 grid grid-flow-row gap-8 md:order-1 md:col-span-3">
           <h1 className="text-4.5xl font-bold">{customer.name}</h1>
 
-          <div className="grid grid-flow-row gap-6">
-            <div className="grid grid-flow-row gap-2">
-              <div className="grid grid-flow-col items-center justify-start gap-4">
-                <XCircleIcon className="h-4 w-4" />
+          <div className="relative grid grid-flow-row gap-6">
+            <div className="relative grid grid-flow-row gap-6">
+              <Image
+                src="/common/dashed-line.svg"
+                width={1}
+                height={386}
+                alt="A dashed line with a gradient background"
+                className="absolute left-2 z-0 h-full w-px object-none"
+                loading="eager"
+              />
 
-                <span className="text-base">Problem</span>
+              <div className="relative z-10 grid grid-flow-row gap-2">
+                <div className="grid grid-flow-col items-center justify-start gap-4 bg-black">
+                  <XCircleIcon className="h-4 w-4" />
+
+                  <span className="text-base">Problem</span>
+                </div>
+                <p className="ml-8 text-base text-white text-opacity-65">
+                  {customer.problem}
+                </p>
               </div>
-              <p className="ml-8 text-base text-white text-opacity-65">
-                {customer.problem}
-              </p>
-            </div>
 
-            <div className="grid grid-flow-row gap-2">
-              <div className="grid grid-flow-col items-center justify-start gap-4">
-                <CheckmarkCircleIcon className="h-4 w-4" />
+              <div className="relative z-10 grid grid-flow-row gap-2">
+                <div className="grid grid-flow-col items-center justify-start gap-4 bg-black">
+                  <CheckmarkCircleIcon className="h-4 w-4" />
 
-                <span className="text-base">Solution</span>
+                  <span className="text-base">Solution</span>
+                </div>
+                <p className="ml-8 text-base text-white text-opacity-65">
+                  {customer.problem}
+                </p>
               </div>
-              <p className="ml-8 text-base text-white text-opacity-65">
-                {customer.problem}
-              </p>
             </div>
 
             <div className="text-white text-opacity-65">{children}</div>
           </div>
         </div>
 
-        <div className="col-span-2">
-          <div className="grid grid-cols-2">
-            <div>
-              <Image
-                src={customer.logo.src}
-                width={customer.logo.width}
-                height={customer.logo.height}
-                alt={customer.name}
-              />
-              <div>
-                <div className="flex">
-                  <div>Industry:</div>
-                  <div>{customer.industry}</div>
-                </div>
-                <div className="flex">
-                  <div>Location:</div>
-                  <div>{customer.location}</div>
-                </div>
-                <div className="flex">
-                  <div>Time To Production:</div>
-                  <div>{customer.timeToProduction}</div>
-                </div>
-              </div>
+        <div className="order-1 col-span-5 grid grid-flow-row gap-6 md:order-2 md:col-span-2">
+          <div className="relative flex w-full items-center justify-center rounded-xl border border-divider bg-black px-18 py-14">
+            <Image
+              src={customer.logo.src}
+              width={customer.logo.width}
+              height={customer.logo.height}
+              alt={customer.name}
+              className="h-auto w-full max-w-[140px]"
+              priority
+            />
+          </div>
+
+          <div className="grid grid-flow-row divide-y divide-divider rounded-xl border border-divider px-6 text-base">
+            <div className="grid grid-cols-2 items-center gap-4 py-6">
+              <span>Industry</span>
+              <span className="text-white text-opacity-65">
+                {customer.industry}
+              </span>
+            </div>
+
+            <div className="grid grid-cols-2 items-center gap-4 py-6">
+              <span>Location</span>
+
+              <span className="text-white text-opacity-65">
+                {customer.location}
+              </span>
+            </div>
+
+            <div className="grid grid-cols-2 items-center gap-4 py-6">
+              <span>Time To Production</span>
+
+              <span className="text-white text-opacity-65">
+                {customer.timeToProduction}
+              </span>
             </div>
           </div>
         </div>
