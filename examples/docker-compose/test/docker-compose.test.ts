71cf7d30c7668c6f369f95b485444601ef72280e
Johan Eliasson <johan@eliasson.me>
2023-03-03 20:03:01 +0100

    updated

--- examples/docker-compose/test/docker-compose.test.ts
+++ examples/docker-compose/test/docker-compose.test.ts
@@ -4,69 +4,70 @@ import { promisify } from 'util'
 import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
 const promisifiedExec = promisify(exec)
+const COMPOSE_FILE = 'docker-compose.yaml'
+const ENV_FILE = '.env.example'
+const HEALTH_CHECK_TIMEOUT = 1000
+const HEALTH_CHECK_RETRY_COUNT = 60
+
+const healthCheck = async (url: string) => {
+  const response = await fetch(url)
+  expect(response.status).toEqual(200)
+}
 
 describe(
   'docker-compose should start, work and stop',
   () => {
     beforeAll(async () => {
-      // * Start docker compose
       await promisifiedExec(
-        'docker compose -f docker-compose.yaml --env-file .env.example up --wait --quiet-pull'
+        `docker compose -f ${COMPOSE_FILE} --env-file ${ENV_FILE} up --wait --quiet-pull`
       )
     }, 5 * 60 * 1000)
 
     afterAll(async () => {
-      // * Stop docker compose
-      await promisifiedExec('docker compose -f docker-compose.yaml --env-file .env.example down')
+      await promisifiedExec(`docker compose -f ${COMPOSE_FILE} --env-file ${ENV_FILE} down`)
     }, 5 * 60 * 1000)
 
-    it(
-      'Hasura',
-      async () => {
-        await expect(
-          fetch('http://localhost:1337/healthz').then((res) => res.status)
-        ).resolves.toEqual(200)
-      },
-      { retry: 60, timeout: 1000 }
-    )
+    describe('health checks', () => {
+      it(
+        'Hasura',
+        async () => {
+          await healthCheck('http://localhost:8080/healthz')
+        },
+        { retry: HEALTH_CHECK_RETRY_COUNT, timeout: HEALTH_CHECK_TIMEOUT }
+      )
 
-    it(
-      'Hasura auth',
-      async () => {
-        await expect(
-          fetch('http://localhost:1337/v1/auth/healthz').then((res) => res.status)
-        ).resolves.toEqual(200)
-      },
-      { retry: 60, timeout: 1000 }
-    )
+      it(
+        'Hasura auth',
+        async () => {
+          await healthCheck('http://localhost:4000/healthz')
+        },
+        { retry: HEALTH_CHECK_RETRY_COUNT, timeout: HEALTH_CHECK_TIMEOUT }
+      )
 
-    it(
-      'Hasura storage',
-      async () => {
-        await expect(
-          fetch('http://localhost:1337/v1/storage/version').then((res) => res.status)
-        ).resolves.toEqual(200)
-      },
-      { retry: 60, timeout: 1000 }
-    )
+      it(
+        'Hasura storage',
+        async () => {
+          await healthCheck('http://localhost:4001/version')
+        },
+        { retry: HEALTH_CHECK_RETRY_COUNT, timeout: HEALTH_CHECK_TIMEOUT }
+      )
 
-    it(
-      'Serverless functions',
-      async () => {
-        await expect(
-          fetch('http://localhost:1337/v1/functions/hello').then((res) => res.status)
-        ).resolves.toEqual(200)
-      },
-      { retry: 60, timeout: 1000 }
-    )
+      it(
+        'Serverless functions',
+        async () => 
+          await healthCheck('http://localhost:4002/hello')
+        },
+        { retry: HEALTH_CHECK_RETRY_COUNT, timeout: HEALTH_CHECK_TIMEOUT }
+      )
 
-    it(
-      'Dashboard',
-      async () => {
-        await expect(fetch('http://localhost:3030').then((res) => res.status)).resolves.toEqual(200)
-      },
-      { retry: 60, timeout: 1000 }
-    )
+      it(
+        'Dashboard',
+        async () => {
+          await healthCheck('http://localhost:3030')
+        },
+        { retry: HEALTH_CHECK_RETRY_COUNT, timeout: HEALTH_CHECK_TIMEOUT }
+      )
+    })
   },
   { timeout: 5 * 60 * 1000 }
 )
