5f68ae95c45ca86ba6b05425f42864bd0f51ebe0
Szilárd Dóró <doroszilard@gmail.com>
2023-03-22 15:22:49 +0100

    chore: add extra database UI tests

--- dashboard/e2e/database/table-creation.test.ts
+++ dashboard/e2e/database/table-creation.test.ts
@@ -3,7 +3,7 @@ import {
   TEST_PROJECT_SLUG,
   TEST_WORKSPACE_SLUG,
 } from '@/e2e/env';
-import { openProject } from '@/e2e/utils';
+import { createTable, openProject } from '@/e2e/utils';
 import { faker } from '@faker-js/faker';
 import type { Page } from '@playwright/test';
 import { expect, test } from '@playwright/test';
@@ -32,56 +32,72 @@ test.afterAll(async () => {
 });
 
 test('should create a simple table', async () => {
-  await expect(
-    page.getByRole('button', { name: /schema\.public/i }),
-  ).toBeVisible();
-
   await page.getByRole('button', { name: /new table/i }).click();
   await expect(page.getByText(/create a new table/i)).toBeVisible();
 
-  const tableName = faker.lorem
-    .word({ length: { min: 8, max: 15 } })
-    .toLowerCase();
+  const tableName = faker.random.word().toLowerCase();
 
-  await page.getByRole('textbox', { name: /name/i }).first().fill(tableName);
+  await createTable({
+    page,
+    name: tableName,
+    primaryKey: 'id',
+    columns: [
+      { name: 'id', type: 'uuid', defaultValue: 'gen_random_uuid()' },
+      { name: 'title', type: 'text' },
+    ],
+  });
 
-  // prepare `id` column
-  await page.getByPlaceholder(/name/i).first().fill('id');
-  await page.getByRole('combobox', { name: /type/i }).first().fill('uuid');
-  await page.getByRole('option', { name: /uuid/i }).click();
-  await page
-    .getByRole('combobox', { name: /default value/i })
-    .first()
-    .fill('gen_rand');
-  await page.getByRole('option', { name: /gen_random_uuid()/i }).click();
+  await page.waitForURL(
+    `/${TEST_WORKSPACE_SLUG}/${TEST_PROJECT_SLUG}/database/browser/default/public/${tableName}`,
+  );
 
-  // add new column
-  await page.getByRole('button', { name: /add column/i }).click();
+  await expect(
+    page.getByRole('link', { name: tableName, exact: true }),
+  ).toBeVisible();
+});
 
-  // prepare `title` column
-  await page.getByPlaceholder(/name/i).nth(1).fill('title');
-  await page.getByRole('combobox', { name: /type/i }).nth(1).fill('text');
-  await page.getByRole('option', { name: /text/i }).first().click();
-  await page
-    .getByRole('checkbox', { name: /unique/i })
-    .first()
-    .check();
+test('should create a table with unique constraints', async () => {
+  await page.getByRole('button', { name: /new table/i }).click();
+  await expect(page.getByText(/create a new table/i)).toBeVisible();
 
-  // add new column
-  await page.getByRole('button', { name: /add column/i }).click();
+  const tableName = faker.random.word().toLowerCase();
 
-  // prepare `description` column
-  await page.getByPlaceholder(/name/i).nth(2).fill('description');
-  await page.getByRole('combobox', { name: /type/i }).nth(2).fill('text');
-  await page.getByRole('option', { name: /text/i }).first().click();
-  await page
-    .getByRole('checkbox', { name: /nullable/i })
-    .nth(2)
-    .check();
+  await createTable({
+    page,
+    name: tableName,
+    primaryKey: 'id',
+    columns: [
+      { name: 'id', type: 'uuid', defaultValue: 'gen_random_uuid()' },
+      { name: 'title', type: 'text', unique: true },
+      { name: 'isbn', type: 'text', unique: true },
+    ],
+  });
 
-  await page.getByRole('button', { name: /primary key/i }).click();
-  await page.getByRole('option', { name: /id/i }).click();
-  await page.getByRole('button', { name: /create/i }).click();
+  await page.waitForURL(
+    `/${TEST_WORKSPACE_SLUG}/${TEST_PROJECT_SLUG}/database/browser/default/public/${tableName}`,
+  );
+
+  await expect(
+    page.getByRole('link', { name: tableName, exact: true }),
+  ).toBeVisible();
+});
+
+test('should create a table with nullable columns', async () => {
+  await page.getByRole('button', { name: /new table/i }).click();
+  await expect(page.getByText(/create a new table/i)).toBeVisible();
+
+  const tableName = faker.random.word().toLowerCase();
+
+  await createTable({
+    page,
+    name: tableName,
+    primaryKey: 'id',
+    columns: [
+      { name: 'id', type: 'uuid', defaultValue: 'gen_random_uuid()' },
+      { name: 'title', type: 'text', nullable: true },
+      { name: 'description', type: 'text', nullable: true },
+    ],
+  });
 
   await page.waitForURL(
     `/${TEST_WORKSPACE_SLUG}/${TEST_PROJECT_SLUG}/database/browser/default/public/${tableName}`,
--- dashboard/e2e/utils.ts
+++ dashboard/e2e/utils.ts
@@ -23,3 +23,94 @@ export async function openProject({
   await page.getByRole('link', { name: projectName }).click();
   await page.waitForURL(`/${workspaceSlug}/${projectSlug}`);
 }
+
+/**
+ * Create a new table in the database.
+ *
+ * @param page - The Playwright page object.
+ * @param name - The name of the table to create.
+ * @param columns - The columns to create in the table.
+ * @returns A promise that resolves when the table is created.
+ */
+export async function createTable({
+  page,
+  name: tableName,
+  primaryKey,
+  columns,
+}: {
+  page: Page;
+  name: string;
+  primaryKey: string;
+  columns: Array<{
+    name: string;
+    type: string;
+    nullable?: boolean;
+    unique?: boolean;
+    defaultValue?: string;
+  }>;
+}) {
+  if (!columns.some(({ name }) => name === primaryKey)) {
+    throw new Error('Primary key must be one of the columns.');
+  }
+
+  await page.getByRole('textbox', { name: /name/i }).first().fill(tableName);
+
+  await Promise.all(
+    columns.map(
+      async (
+        { name: columnName, type, nullable, unique, defaultValue },
+        index,
+      ) => {
+        // set name
+        await page.getByPlaceholder(/name/i).nth(index).fill(columnName);
+
+        // set type
+        await page
+          .getByRole('combobox', { name: /type/i })
+          .nth(index)
+          .fill(type);
+        await page.getByRole('option', { name: type }).first().click();
+
+        // optionally set default value
+        if (defaultValue) {
+          await page
+            .getByRole('combobox', { name: /default value/i })
+            .first()
+            .fill(defaultValue);
+          await page
+            .getByRole('option', { name: defaultValue })
+            .first()
+            .click();
+        }
+
+        // optionally check unique
+        if (unique) {
+          await page
+            .getByRole('checkbox', { name: /unique/i })
+            .nth(index)
+            .check();
+        }
+
+        // optionally check nullable
+        if (nullable) {
+          await page
+            .getByRole('checkbox', { name: /nullable/i })
+            .nth(index)
+            .check();
+        }
+
+        // add new column if not last
+        if (index < columns.length - 1) {
+          await page.getByRole('button', { name: /add column/i }).click();
+        }
+      },
+    ),
+  );
+
+  // select the first column as primary key
+  await page.getByRole('button', { name: /primary key/i }).click();
+  await page.getByRole('option', { name: primaryKey, exact: true }).click();
+
+  // create table
+  await page.getByRole('button', { name: /create/i }).click();
+}
