74d4deba28d90f00a2dc5caf78fc12581ebec011
Szilárd Dóró <doroszilard@gmail.com>
2023-03-27 17:00:07 +0200

    feat: cleanup public schema after tests

--- dashboard/global-setup.ts
+++ dashboard/global-setup.ts
@@ -1,19 +1,19 @@
-import { chromium } from '@playwright/test';
 import {
   TEST_DASHBOARD_URL,
   TEST_USER_EMAIL,
   TEST_USER_PASSWORD,
-} from './e2e/env';
+} from '@/e2e/env';
+import { chromium } from '@playwright/test';
 
 async function globalSetup() {
   const browser = await chromium.launch();
-  const page = await browser.newPage();
+  const page = await browser.newPage({ baseURL: TEST_DASHBOARD_URL });
 
-  await page.goto(TEST_DASHBOARD_URL);
-  await page.waitForURL(`${TEST_DASHBOARD_URL}/signin`);
+  await page.goto('/');
+  await page.waitForURL('/signin');
   await page.getByRole('link', { name: /continue with email/i }).click();
 
-  await page.waitForURL(`${TEST_DASHBOARD_URL}/signin/email`);
+  await page.waitForURL('/signin/email');
   await page.getByLabel('Email').fill(TEST_USER_EMAIL);
   await page.getByLabel('Password').fill(TEST_USER_PASSWORD);
   await page.getByRole('button', { name: /sign in/i }).click();
