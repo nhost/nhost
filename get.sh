72529e830964996f20ef0bb26ef05b71a53273e9
Szilárd Dóró <doroszilard@gmail.com>
2023-05-31 14:09:39 +0200

    fix: use provided version explicitly

--- get.sh
+++ get.sh
@@ -43,7 +43,7 @@ log "Getting $release version..."
 if [[ "$release" == "latest" ]]; then
     version=$(curl --silent "https://api.github.com/repos/$REPO/releases/latest" | grep '"tag_name":' |  sed -E 's/.*"([^"]+)".*/\1/')
 else
-    version=$(curl --silent "https://api.github.com/repos/nhost/cli/tags" | grep "name" | sed -E 's/.*"([^"]+)".*/\1/' | grep "$release")
+    version=$(curl --silent "https://api.github.com/repos/nhost/cli/tags" | grep "name" | sed -E 's/.*"([^"]+)".*/\1/' | grep "^$release$")
 fi
 
 # check version exists
@@ -51,7 +51,7 @@ if [ ! $version ]; then
     log "${YELLOW}"
     log "Failed while attempting to install Nhost CLI. Please manually install:"
     log ""
-    log "2. Open your web browser and go to https://github.com/$REPO/releases/latest"
+    log "1. Open your web browser and go to https://github.com/$REPO/releases/latest"
     log "2. Download the CLI from latest release for your platform. Name it 'nhost'."
     log "3. chmod +x ./cli"
     log "4. mv ./cli /usr/local/bin/nhost"
