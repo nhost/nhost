27e5ef8f5abab4eef4d24981ad122beb87c4658f
Szilárd Dóró <doroszilard@gmail.com>
2023-06-01 10:34:10 +0200

    fix: use correct mailhog URL in tests

--- examples/codegen-react-query/src/components/SignIn.tsx
+++ examples/codegen-react-query/src/components/SignIn.tsx
@@ -17,7 +17,7 @@ export function SignIn() {
       return
     }
     alert('Magic Link Sent')
-    window.open('http://localhost:8025/', '_blank')
+    window.open('https://local.mailhog.nhost.run/', '_blank')
   }
 
   return (
