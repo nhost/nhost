b625a6b4d47ed651225b481fe69bf791d3a707d3
Szilárd Dóró <doroszilard@gmail.com>
2023-06-01 16:24:12 +0200

    chore: cleanup unused code

--- dashboard/src/features/projects/common/components/StagingMetadata/StagingMetadata.tsx
+++ dashboard/src/features/projects/common/components/StagingMetadata/StagingMetadata.tsx
@@ -49,7 +49,7 @@ export default function StagingMetadata({
   return (
     isDevOrStaging() && (
       <div className="mx-auto mt-10 max-w-sm">
-        <Box className="mx-auto flex flex-col rounded-md border p-5 text-center">
+        <Box className="mx-auto grid grid-flow-row justify-items-center rounded-md border p-5 text-center">
           <Status status={StatusEnum.Deploying}>Internal info</Status>
           {children}
         </Box>
