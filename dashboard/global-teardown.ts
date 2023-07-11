f4c6e7cfab6ac6e5a7b0a0d8a8d64c6088a762ec
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2023-07-11 13:58:41 +0100

    chore: bring back raw_sql fill

--- dashboard/global-teardown.ts
+++ dashboard/global-teardown.ts
@@ -46,23 +46,18 @@ async function globalTeardown() {
   await hasuraPage.locator('a', { hasText: /data/i }).click();
   await hasuraPage.getByRole('link', { name: /sql/i }).click();
 
-  // Set the value of the Ace code editor using JavaScript evaluation in the browser context
-  await hasuraPage.evaluate(() => {
-    const editor = ace.edit('raw_sql');
-
-    editor.setValue(`
-        DO $$ DECLARE
-          tablename text;
-        BEGIN
-          FOR tablename IN
-            SELECT table_name FROM information_schema.tables
-            WHERE table_schema = 'public'
-          LOOP
-            EXECUTE 'DROP TABLE IF EXISTS public.' || quote_ident(tablename) || ' CASCADE';
-          END LOOP;
-        END $$;
-     `);
-  });
+  await hasuraPage.locator('#raw_sql > textarea').fill(`
+DO $$ DECLARE
+  tablename text;
+BEGIN
+  FOR tablename IN
+    SELECT table_name FROM information_schema.tables
+    WHERE table_schema = 'public'
+  LOOP
+    EXECUTE 'DROP TABLE IF EXISTS public.' || quote_ident(tablename) || ' CASCADE';
+  END LOOP;
+END $$;
+`);
 
   await hasuraPage.getByRole('button', { name: /run!/i }).click();
 
