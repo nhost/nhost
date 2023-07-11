4d82bc5609aa897bdf7c638ef2835bd4525cf1da
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2023-07-11 16:17:30 +0100

    Revert "chore: playwright: increase number of workers"
    This reverts commit b4c10f9f8a5c521f711caff4f87230f9bf32a61a.

--- dashboard/playwright.config.ts
+++ dashboard/playwright.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 5 : undefined,
+  workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   globalTeardown: require.resolve('./global-teardown'),
   use: {
