568251b93997115dab9507504f8e6957e694574b
nevvada <nlemaster47@gmail.com>
2023-07-19 15:27:41 -0700

    chore: add maskedErrors param to create server props for Stripe GraphQL and Google Translation integrations

--- integrations/stripe-graphql-js/src/server.ts
+++ integrations/stripe-graphql-js/src/server.ts
@@ -1,15 +1,16 @@
-import { createServer } from 'node:http'
 import { createYoga, YogaInitialContext } from 'graphql-yoga'
+import { createServer } from 'node:http'
 
 import { schema } from './schema'
 import { Context, CreateServerProps } from './types'
 import { getUserClaims } from './utils'
 
-const createStripeGraphQLServer = (params?: CreateServerProps) => {
-  const cors = params?.cors
-  const isAllowed = params?.isAllowed
-  const graphiql = params?.graphiql
-
+const createStripeGraphQLServer = ({
+  cors,
+  isAllowed,
+  graphiql,
+  maskedErrors = true,
+}: CreateServerProps = {}) => {
   const context = (context: YogaInitialContext): Context => {
     const { request } = context
 
@@ -51,7 +52,8 @@ const createStripeGraphQLServer = (params?: CreateServerProps) => {
     graphiql,
     context,
     schema,
-    graphqlEndpoint: '*'
+    graphqlEndpoint: '*',
+    maskedErrors,
   })
 
   return createServer(yoga)
