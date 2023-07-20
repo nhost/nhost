f12d071e16a2a181eac19c068222415580265875
David Barroso <dbarrosop@dravetech.com>
2023-07-20 16:38:59 +0200

    chore: Cloud Run -> Run (#750)

--- cmd/run/run.go
+++ cmd/run/run.go
@@ -6,7 +6,7 @@ func Command() *cli.Command {
 	return &cli.Command{ //nolint:exhaustruct
 		Name:    "run",
 		Aliases: []string{},
-		Usage:   "Perform operations on Nhost Cloud Run",
+		Usage:   "Perform operations on Nhost Run",
 		Subcommands: []*cli.Command{
 			CommandConfigDeploy(),
 			CommandConfigEditImage(),
