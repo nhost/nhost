f780b1758128d10d7ee9d30aea7317347d5d8a7e
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2023-08-23 16:59:44 +0100

    chore: remove facebook login from react apollo example

--- examples/react-apollo/src/components/OauthLinks.tsx
+++ examples/react-apollo/src/components/OauthLinks.tsx
@@ -1,11 +1,11 @@
-import { FaFacebook, FaGithub, FaGoogle } from 'react-icons/fa/index.js'
+import { FaGithub, FaGoogle } from 'react-icons/fa/index.js'
 
 import { useProviderLink } from '@nhost/react'
 
 import AuthLink from './AuthLink'
 
 export default function OauthLinks() {
-  const { github, google, facebook } = useProviderLink({ redirectTo: window.location.origin })
+  const { github, google } = useProviderLink({ redirectTo: window.location.origin })
   return (
     <>
       <AuthLink leftIcon={<FaGithub />} link={github} color="#333">
@@ -13,9 +13,6 @@ export default function OauthLinks() {
       </AuthLink>
       <AuthLink leftIcon={<FaGoogle />} link={google} color="#de5246">
         Continue with Google
-      </AuthLink>
-      <AuthLink leftIcon={<FaFacebook />} link={facebook} color="#3b5998">
-        Continue with Facebook
       </AuthLink>
     </>
   )
