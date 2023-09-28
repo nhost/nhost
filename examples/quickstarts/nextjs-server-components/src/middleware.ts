d3186aefbd7d350ff336d76fee5c74817abf1749
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2023-09-28 12:37:10 +0100

    refactor: extract session middleware into helper function

--- examples/quickstarts/nextjs-server-components/src/middleware.ts
+++ examples/quickstarts/nextjs-server-components/src/middleware.ts
@@ -1,35 +1,8 @@
-import { NHOST_SESSION_KEY, getNhost } from '@utils/nhost'
+import { manageAuthSession } from '@utils/nhost'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function middleware(request: NextRequest) {
-  const nhost = await getNhost(request)
-  const session = nhost.auth.getSession()
-
-  const url = new URL(request.url)
-  const refreshToken = url.searchParams.get('refreshToken') || undefined
-
-  const currentTime = Math.floor(Date.now() / 1000)
-  const tokenExpirationTime = nhost.auth.getDecodedAccessToken()?.exp
-  const accessTokenExpired = session && tokenExpirationTime && currentTime > tokenExpirationTime
-
-  if (accessTokenExpired || refreshToken) {
-    const { session: newSession, error } = await nhost.auth.refreshSession(refreshToken)
-
-    if (error) {
-      // refresh token has expired or invalid
-      return NextResponse.redirect(new URL('/auth/sign-in', request.url))
-    }
-
-    // remove the refreshToken from the url
-    url.searchParams.delete('refreshToken')
-
-    // overwrite the session cookie with the new session
-    return NextResponse.redirect(url, {
-      headers: {
-        'Set-Cookie': `${NHOST_SESSION_KEY}=${btoa(JSON.stringify(newSession))}; Path=/`
-      }
-    })
-  }
-
-  return NextResponse.next()
+  return manageAuthSession(request, (error) =>
+    NextResponse.redirect(new URL('/auth/sign-in', request.url))
+  )
 }
--- examples/quickstarts/nextjs-server-components/src/utils/nhost.ts
+++ examples/quickstarts/nextjs-server-components/src/utils/nhost.ts
@@ -1,10 +1,10 @@
-import { NhostClient, NhostSession } from '@nhost/nhost-js'
+import { AuthErrorPayload, NhostClient, NhostSession } from '@nhost/nhost-js'
 import { cookies } from 'next/headers'
-import { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import { type StateFrom } from 'xstate/lib/types'
 import { waitFor } from 'xstate/lib/waitFor'
 
-export const NHOST_SESSION_KEY = 'nhostSession'
+export const NHOST_SESSION_KEY = 'nhost-session'
 
 export const getNhost = async (request?: NextRequest) => {
   const $cookies = request?.cookies || cookies()
@@ -22,4 +22,37 @@ export const getNhost = async (request?: NextRequest) => {
   await waitFor(nhost.auth.client.interpreter!, (state: StateFrom<any>) => !state.hasTag('loading'))
 
   return nhost
+}
+
+export const manageAuthSession = async (
+  request: NextRequest,
+  onError?: (error: AuthErrorPayload) => NextResponse
+) => {
+  const nhost = await getNhost(request)
+  const session = nhost.auth.getSession()
+
+  const url = new URL(request.url)
+  const refreshToken = url.searchParams.get('refreshToken') || undefined
+
+  const currentTime = Math.floor(Date.now() / 1000)
+  const tokenExpirationTime = nhost.auth.getDecodedAccessToken()?.exp
+  const accessTokenExpired = session && tokenExpirationTime && currentTime > tokenExpirationTime
+
+  if (accessTokenExpired || refreshToken) {
+    const { session: newSession, error } = await nhost.auth.refreshSession(refreshToken)
+
+    if (error) {
+      onError?.(error)
+    }
+
+    // remove the refreshToken from the url
+    url.searchParams.delete('refreshToken')
+
+    // overwrite the session cookie with the new session
+    return NextResponse.redirect(url, {
+      headers: {
+        'Set-Cookie': `${NHOST_SESSION_KEY}=${btoa(JSON.stringify(newSession))}; Path=/`
+      }
+    })
+  }
 }
