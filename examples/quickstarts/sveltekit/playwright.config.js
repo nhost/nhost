f73672372f175b38178d71758a88de5aa18bdbc9
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2023-11-15 21:08:03 +0100

    chore: update baseURL in playwright.config.js

--- examples/quickstarts/sveltekit/playwright.config.js
+++ examples/quickstarts/sveltekit/playwright.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:5173',
+    baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry'
@@ -51,7 +51,7 @@ export default defineConfig({
 
   webServer: {
     command: 'pnpm dev',
-    url: 'http://localhost:5173',
+    url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI
   }
 })
