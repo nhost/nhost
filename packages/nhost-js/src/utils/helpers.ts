be110df83aa43b4be5f4c2baed93fa23bf598589
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2023-11-30 17:13:24 +0100

    fix: refactor urlFromSubdomain and fix unit tests

--- packages/nhost-js/src/utils/helpers.ts
+++ packages/nhost-js/src/utils/helpers.ts
@@ -5,21 +5,20 @@ export const LOCALHOST_REGEX =
   /^((?<protocol>http[s]?):\/\/)?(?<host>(localhost|local))(:(?<port>(\d+|__\w+__)))?$/
 
 /**
- * \`backendUrl\` should now be used only when self-hosting
- * \`subdomain\` and `region` should be used instead when using the Nhost platform
+ * \`subdomain\` and `region` should be used when running the Nhost platform
  *
- * @param backendOrSubdomain
+ * @param subdomainAndRegion
  * @param service
  * @returns
  */
 export function urlFromSubdomain(
-  backendOrSubdomain: Pick<NhostClientConstructorParams, 'region' | 'subdomain'>,
+  subdomainAndRegion: Pick<NhostClientConstructorParams, 'region' | 'subdomain'>,
   service: string
 ): string {
-  const { subdomain, region } = backendOrSubdomain
+  const { subdomain, region } = subdomainAndRegion
 
   if (!subdomain) {
-    throw new Error('Either `backendUrl` or `subdomain` must be set.')
+    throw new Error('A `subdomain` must be set.')
   }
 
   // check if subdomain is [http[s]://]localhost[:port] or [http[s]://]local[:port]
