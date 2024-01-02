a2fadd043770b21aa3e7737be8d1d8201e06388e
David Barroso <dbarrosop@dravetech.com>
2024-01-02 17:22:12 +0100

    chore: changelog generator

---
+++ changelog_summary.sh
@@ -0,0 +1,13 @@
+#/usr/bin/env bash
+PREV_MONTH=$(date -v-1m +%Y-%m)
+
+files=$(git log --since="$PREV_MONTH-01" --until="$PREV_MONTH-31" --name-only -- '**/CHANGELOG.md' | grep CHANGE | sort -u)
+
+echo "Below you can find the latest release for each individual package released during this month:"
+echo
+
+for file in $files; do
+  name=$(grep '^# ' $file | awk '{ print substr($0, 4) }')
+  last_release=$(grep '^## ' $file | awk '{ print substr($0, 4) }' | head -n 1)
+  echo "@$name: [$last_release](https://github.com/nhost/nhost/blob/main/$file)"
+done
