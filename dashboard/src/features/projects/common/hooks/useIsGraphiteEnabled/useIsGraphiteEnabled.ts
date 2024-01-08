44a9f3a0e033f0d84e220fd205a9dff2e8aecbbd
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2024-01-08 18:40:19 +0100

    fix: refactor useIsGraphiteEnabled hook

--- dashboard/src/features/projects/common/hooks/useIsGraphiteEnabled/useIsGraphiteEnabled.ts
+++ dashboard/src/features/projects/common/hooks/useIsGraphiteEnabled/useIsGraphiteEnabled.ts
@@ -1,23 +1,14 @@
 import { useAdminApolloClient } from '@/features/projects/common/hooks/useAdminApolloClient';
 import { useGetGraphiteSessionsQuery } from '@/utils/__generated__/graphite.graphql';
-import { useEffect, useState } from 'react';
 
 export default function useIsGraphiteEnabled() {
   const { adminClient } = useAdminApolloClient();
 
-  const [isGraphiteEnabled, setIsGraphiteEnabled] = useState(false);
-
-  const { loading, error } = useGetGraphiteSessionsQuery({
+  const { error } = useGetGraphiteSessionsQuery({
     client: adminClient,
   });
 
-  useEffect(() => {
-    if (!loading && error) {
-      setIsGraphiteEnabled(false);
-    }
-  }, [error, loading]);
-
   return {
-    isGraphiteEnabled,
+    isGraphiteEnabled: !error,
   };
 }
