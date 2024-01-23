896e4198330a04a8be09dc63d7baa27d3332f861
David Barroso <dbarrosop@dravetech.com>
2024-01-23 13:41:57 +0100

    asd

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -9,7 +9,7 @@ const cspHeader = `
     default-src 'self' *.nhost.run ws://*.nhost.run;
     script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.segment.com js.stripe.com;
     style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data: avatars.githubusercontent.com *.nhost.run;
+    img-src 'self' blob: data: avatars.githubusercontent.com s.gravatar.com *.nhost.run;
     font-src 'self' data:;
     object-src 'none';
     base-uri 'self';
