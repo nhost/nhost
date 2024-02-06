3ba485e58231f7e7d250a9717c3aa12629d07fca
David Barroso <dbarrosop@dravetech.com>
2024-02-06 11:29:27 +0100

    fix (dashboard): added discord.com to connect-src (#2516)

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -8,6 +8,7 @@ const { version } = require('./package.json');
 const cspHeader = `
     default-src 'self' *.nhost.run ws://*.nhost.run nhost.run ws://nhost.run;
     script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.segment.com js.stripe.com;
+    connect-src 'self' *.nhost.run ws://*.nhost.run nhost.run ws://nhost.run discord.com;
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data: avatars.githubusercontent.com s.gravatar.com *.nhost.run nhost.run;
     font-src 'self' data:;
