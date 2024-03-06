2a43e00b87d6acfc636c384523ee2670c8d86e8e
nhost-build <98952681+nhost-build@users.noreply.github.com>
2024-03-06 11:26:42 -0100

    chore: bump nhost/dashboard to 1.10.0 (#833)
    Co-authored-by: onehassan <onehassan@users.noreply.github.com>
    Co-authored-by: David Barroso <dbarrosop@dravetech.com>

--- cmd/dev/up.go
+++ cmd/dev/up.go
@@ -106,7 +106,7 @@ func CommandUp() *cli.Command { //nolint:funlen
 			&cli.StringFlag{ //nolint:exhaustruct
 				Name:    flagDashboardVersion,
 				Usage:   "Dashboard version to use",
-				Value:   "nhost/dashboard:1.6.7",
+				Value:   "nhost/dashboard:1.10.0",
 				EnvVars: []string{"NHOST_DASHBOARD_VERSION"},
 			},
 			&cli.StringFlag{ //nolint:exhaustruct
