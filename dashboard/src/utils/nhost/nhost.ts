0e00df9d5737dc2aa03f1711e96fec7265d186f2
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2024-03-28 15:43:02 +0100

    refact Nhost initialization in nhost.ts to use service URLs with port

--- dashboard/src/utils/nhost/nhost.ts
+++ dashboard/src/utils/nhost/nhost.ts
@@ -3,18 +3,14 @@ import {
   getFunctionsServiceUrl,
   getGraphqlServiceUrl,
   getStorageServiceUrl,
-  isPlatform,
 } from '@/utils/env';
 import { NhostClient } from '@nhost/nextjs';
 
-// eslint-disable-next-line no-nested-ternary
-const nhost = isPlatform()
-  ? new NhostClient({
-      authUrl: getAuthServiceUrl(),
-      graphqlUrl: getGraphqlServiceUrl(),
-      functionsUrl: getFunctionsServiceUrl(),
-      storageUrl: getStorageServiceUrl(),
-    })
-  : new NhostClient({ subdomain: 'local' });
+const nhost = new NhostClient({
+  authUrl: getAuthServiceUrl(),
+  graphqlUrl: getGraphqlServiceUrl(),
+  functionsUrl: getFunctionsServiceUrl(),
+  storageUrl: getStorageServiceUrl(),
+});
 
 export default nhost;
