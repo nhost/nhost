396ec79836346598d70699cf1fe5285f7bac1659
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2024-04-25 11:44:16 +0100

    add confirmation dialog when deleting smtp settings

--- dashboard/src/features/authentication/settings/components/DeleteSMTPSettings/DeleteSMTPSettings.tsx
+++ dashboard/src/features/authentication/settings/components/DeleteSMTPSettings/DeleteSMTPSettings.tsx
@@ -4,6 +4,7 @@ import { useUI } from '@/components/common/UIProvider';
 import { SettingsContainer } from '@/components/layout/SettingsContainer';
 import { Box } from '@/components/ui/v2/Box';
 import { Button } from '@/components/ui/v2/Button';
+import { Text } from '@/components/ui/v2/Text';
 import { useCurrentWorkspaceAndProject } from '@/features/projects/common/hooks/useCurrentWorkspaceAndProject';
 import { useIsPlatform } from '@/features/projects/common/hooks/useIsPlatform';
 import { useLocalMimirClient } from '@/hooks/useLocalMimirClient';
@@ -13,9 +14,46 @@ import {
   useUpdateConfigMutation,
 } from '@/utils/__generated__/graphql';
 import { useState } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+function ConfirmDeleteSMTPSettingsModal({
+  close,
+  onDelete,
+}: {
+  onDelete?: () => Promise<any>;
+  close: () => void;
+}) {
+  const onClickDelete = async () => {
+    await onDelete();
+    close();
+  };
+
+  return (
+    <Box className={twMerge('w-full rounded-lg p-6 text-left')}>
+      <div className="grid grid-flow-row gap-4">
+        <Text variant="h3" component="h2">
+          Delete SMTP Settings?
+        </Text>
+
+        <Text>This will reset all your SMTP and Postmark settings.</Text>
+
+        <div className="grid grid-flow-row gap-2">
+          <Button color="error" onClick={onClickDelete}>
+            Delete
+          </Button>
+
+          <Button variant="outlined" color="secondary" onClick={close}>
+            Cancel
+          </Button>
+        </div>
+      </div>
+    </Box>
+  );
+}
 
 export default function DeleteSMTPSettings() {
-  const { openDialog } = useDialog();
+  const { openDialog, closeDialog } = useDialog();
+
   const isPlatform = useIsPlatform();
   const localMimirClient = useLocalMimirClient();
   const { maintenanceActive } = useUI();
@@ -27,7 +65,7 @@ export default function DeleteSMTPSettings() {
     ...(!isPlatform ? { client: localMimirClient } : {}),
   });
 
-  const confirmDeleteSMTPSettings = async () => {
+  const deleteSMTPSettings = async () => {
     const updateConfigPromise = updateConfig({
       variables: {
         appId: currentProject.id,
@@ -66,6 +104,17 @@ export default function DeleteSMTPSettings() {
     );
 
     setLoading(false);
+  };
+
+  const confirmDeleteSMTPSettings = async () => {
+    openDialog({
+      component: (
+        <ConfirmDeleteSMTPSettingsModal
+          close={closeDialog}
+          onDelete={deleteSMTPSettings}
+        />
+      ),
+    });
   };
 
   return (
