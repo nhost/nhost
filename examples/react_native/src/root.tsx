0927d9ba46b9a5e9ed0f82db843d2ece9e582765
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2024-05-16 15:47:11 +0100

    chore: set moduleResolution to Node

--- examples/react_native/src/root.tsx
+++ examples/react_native/src/root.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import {NhostClient, NhostProvider} from '@nhost/react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import Main from './screens/Main';
+import Main from '@screens/Main';
 import {NhostApolloProvider} from '@nhost/react-apollo';
 
 const nhost = new NhostClient({
