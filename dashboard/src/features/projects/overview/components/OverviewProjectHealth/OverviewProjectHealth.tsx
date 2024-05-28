f4e1395e7ec475ab102fd44f34ef644632d66977
Barrarroso <correodelnino@gmail.com>
2024-05-28 15:41:39 +0200

    wip: cleanup

--- dashboard/src/features/projects/overview/components/OverviewProjectHealth/OverviewProjectHealth.tsx
+++ dashboard/src/features/projects/overview/components/OverviewProjectHealth/OverviewProjectHealth.tsx
@@ -1,34 +1,45 @@
 import { Text } from '@/components/ui/v2/Text';
 import { useCurrentWorkspaceAndProject } from '@/features/projects/common/hooks/useCurrentWorkspaceAndProject';
-import { InfoCard } from '@/features/projects/overview/components/InfoCard';
 import { HasuraIcon } from '@/components/ui/v2/icons/HasuraIcon';
-import Image from 'next/image';
 import { ProjectHealthCard } from '@/features/projects/overview/components/ProjectHealthCard';
 import { UserIcon } from '@/components/ui/v2/icons/UserIcon';
 import { DatabaseIcon } from '@/components/ui/v2/icons/DatabaseIcon';
 import { StorageIcon } from '@/components/ui/v2/icons/StorageIcon';
-import { DockerIcon } from '@/components/ui/v2/icons/DockerIcon';
 import { AIIcon } from '@/components/ui/v2/icons/AIIcon';
+import { Box } from '@/components/ui/v2/Box';
+import { ServicesIcon } from '@/components/ui/v2/icons/ServicesIcon';
 
 export default function OverviewProjectHealth() {
   const { currentProject } = useCurrentWorkspaceAndProject();
-  const { region, subdomain } = currentProject || {};
-  const isRegionAvailable =
-    region?.awsName && region?.countryCode && region?.city;
+
+  const RunList = [...Array(10).keys()].map((i) =>
+  (<li className="flex flex-row items-center gap-4 text-ellipsis text-nowrap leading-5">
+    <Box sx={{ backgroundColor: "success.dark" }} className="flex-shrink-0 w-3 h-3 bg-success rounded-full" />
+    {`nhost-run-x-y-${i + 1}`}
+  </li>)
+  );
+  const servicesTooltipElem = (<ol className="flex flex-col gap-3 px-4 py-6 m-0">{RunList}</ol>);
+
 
   return (
     <div className="grid grid-flow-row content-start gap-6">
       <Text variant="h3">Project Health</Text>
 
       {currentProject && (
-          <div className="grid grid-flow-row items-center gap-6 xs:grid-cols-3 lg:gap-3 xl:grid-cols-6">
-          <ProjectHealthCard icon={<UserIcon className="h-6 w-6" />} value="Hola" tooltip="Hola hola hola" />
-          <ProjectHealthCard icon={<DatabaseIcon className="h-6 w-6" />} value="Hola" tooltip="Hola hola hola" />
-          <ProjectHealthCard icon={<StorageIcon className="h-6 w-6" />} value="Hola" tooltip="Hola hola hola" />
-          <ProjectHealthCard icon={<HasuraIcon className="h-6 w-6" />} value="Hola" tooltip="Hola hola hola" />
-          <ProjectHealthCard icon={<DockerIcon className="h-6 w-6" />} value="Hola" tooltip="Hola hola hola" />
-          <ProjectHealthCard icon={<AIIcon className="h-6 w-6" />} value="Hola" tooltip="Hola hola hola" />
-          </div>
+        <div className="grid grid-flow-row items-center gap-6 xs:grid-cols-3 lg:gap-3 xl:grid-cols-6">
+          <ProjectHealthCard icon={<UserIcon className="h-6 w-6" />}
+            tooltip={servicesTooltipElem} />
+          <ProjectHealthCard icon={<DatabaseIcon className="h-6 w-6" />}
+            tooltip={servicesTooltipElem} />
+          <ProjectHealthCard icon={<StorageIcon className="h-6 w-6" />}
+            tooltip={servicesTooltipElem} />
+          <ProjectHealthCard icon={<HasuraIcon className="h-6 w-6" />}
+            tooltip={servicesTooltipElem} />
+          <ProjectHealthCard icon={<ServicesIcon className="h-6 w-6" />}
+            tooltip={servicesTooltipElem} />
+          <ProjectHealthCard icon={<AIIcon className="h-6 w-6" />}
+            tooltip={servicesTooltipElem} />
+        </div>
       )}
     </div>
   );
--- dashboard/src/features/projects/overview/components/ProjectHealthCard/ProjectHealthCard.tsx
+++ dashboard/src/features/projects/overview/components/ProjectHealthCard/ProjectHealthCard.tsx
@@ -8,6 +8,7 @@ import { twMerge } from 'tailwind-merge';
 import type { ImageProps } from 'next/image';
 import Image from 'next/image';
 import { CheckIcon } from '@/components/ui/v2/icons/CheckIcon';
+import type { TooltipProps } from '@/components/ui/v2/Tooltip';
 
 export interface ProjectHealthCardProps extends BoxProps {
   /**
@@ -21,7 +22,7 @@ export interface ProjectHealthCardProps extends BoxProps {
   /**
    * Tooltip of the card.
    */
-  tooltip?: string;
+  tooltip?: ReactElement;
   /**
    * Icon to display on the card.
    */
@@ -58,15 +59,9 @@ export default function ProjectHealthCard({
   slotProps = {},
   ...props
 }: ProjectHealthCardProps) {
-  const RunList = [...Array(10).keys()].map((i) =>
-  (<li className="flex flex-row items-center gap-4 text-ellipsis text-nowrap leading-5">
-    <Box sx={{backgroundColor: "success.dark"}} className="flex-shrink-0 w-3 h-3 bg-success rounded-full" />
-    {`nhost-run-x-y-${i + 1}`}
-  </li>)
-  );
   return (
-    // TODO: 'arrow' prop bug?
-    <Tooltip arrow title={<ol className="flex flex-col gap-3 px-4 py-6 m-0">{RunList}</ol>}
+    // TODO: BUG 'arrow' stutters on hover 
+    <Tooltip arrow title={tooltip}
       slotProps={{
         tooltip: { className: '' },
       }}
@@ -80,12 +75,6 @@ export default function ProjectHealthCard({
         {...props}
       >
         <div className="grid grid-flow-col items-center justify-center">
-          {/* <Box
-                className={twMerge('', className,)}
-                sx={{ backgroundColor: 'grey.200' }}
-                {...props}
-              > */}
-
           <Badge color="success" variant="standard" badgeContent={<CheckIcon className="w-2 h-2 stroke-2 text-white" />}>
             {iconIsComponent
               ? icon
@@ -99,14 +88,7 @@ export default function ProjectHealthCard({
                 />
               )}
           </Badge>
-          {/* </Box> */}
         </div>
-
-        {/* {value && (
-          <Text variant="h2" component="p" className="truncate">
-            {value}
-          </Text>
-        )} */}
       </Box>
     </Tooltip>
   );
