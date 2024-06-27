b3ff6adcc2b98053c380c2cc8253994c8c98829e
Zephyr (David B.M.) <correodelnino@gmail.com>
2024-06-27 12:05:57 +0200

    dashboard: chore: project health change modal title (#2765)
    Change modal title from 'Service logs' to 'Service State'

--- dashboard/src/features/projects/overview/components/OverviewProjectHealth/OverviewProjectHealth.tsx
+++ dashboard/src/features/projects/overview/components/OverviewProjectHealth/OverviewProjectHealth.tsx
@@ -212,7 +212,7 @@ export default function OverviewProjectHealth() {
           onClose: closeDialog,
         },
       },
-      title: 'Service logs',
+      title: 'Service State',
     });
   };
 
