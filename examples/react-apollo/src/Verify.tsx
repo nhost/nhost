e2db7e0c8364ee7d09e492a0e9f70593a73987bd
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2024-07-04 12:27:53 +0100

    fix: refactor email verification to redirect on a button click

--- examples/react-apollo/src/Verify.tsx
+++ examples/react-apollo/src/Verify.tsx
@@ -1,32 +1,30 @@
-import { useSearchParams } from 'react-router-dom'
-import { useEffect, useState } from 'react'
+import { Button, Card, Container, Stack, Text } from '@mantine/core'
 import { useNhostClient } from '@nhost/react'
-import { Container } from '@mantine/core'
+import { FaEnvelope } from 'react-icons/fa'
+import { useSearchParams } from 'react-router-dom'
 
 const VerifyPage: React.FC = () => {
   const nhost = useNhostClient()
-  const [loading, setLoading] = useState(false)
   const [searchParams] = useSearchParams()
 
-  useEffect(() => {
+  const redirectToVerificationLink = () => {
     const ticket = searchParams.get('ticket')
     const redirectTo = searchParams.get('redirectTo')
     const type = searchParams.get('type')
 
-    if (ticket && redirectTo && type) {
-      window.location.href = `${nhost.auth.url}/verify?ticket=${ticket}&type=${type}&redirectTo=${redirectTo}`
-    }
-
-    setLoading(false)
-  }, [searchParams, nhost?.auth?.url])
-
-  if (loading) {
-    return null
+    window.location.href = `${nhost.auth.url}/verify?ticket=${ticket}&type=${type}&redirectTo=${redirectTo}`
   }
 
   return (
     <Container>
-      <span>Failed to authenticate with magick link</span>
+      <Card shadow="sm" p="lg" radius="md" withBorder>
+        <Stack align="center">
+          <Text>Please verify your account by clicking the link below.</Text>
+          <Button leftIcon={<FaEnvelope size={14} />} onClick={redirectToVerificationLink}>
+            Verify
+          </Button>
+        </Stack>
+      </Card>
     </Container>
   )
 }
