18c9fbd98469c4c939dc93eb8a7e74cee56895d0
Barrarroso <correodelnino@gmail.com>
2024-07-04 18:53:23 +0200

    chore: poll every 10s

--- dashboard/src/features/projects/common/hooks/useServiceStatus/useServiceStatus.ts
+++ dashboard/src/features/projects/common/hooks/useServiceStatus/useServiceStatus.ts
@@ -2,7 +2,6 @@ import { useCurrentWorkspaceAndProject } from '@/features/projects/common/hooks/
 import { useIsPlatform } from '@/features/projects/common/hooks/useIsPlatform';
 import type { ServiceHealthInfo } from '@/features/projects/overview/health';
 import {
-  ServiceState,
   useGetProjectServicesHealthQuery,
   type GetProjectServicesHealthQuery,
   type GetProjectServicesHealthQueryVariables,
@@ -27,22 +26,18 @@ export default function useServiceStatus(
 
   const isVisible = useVisibilityChange();
 
-  const { data, loading, refetch, startPolling, stopPolling } =
-    useGetProjectServicesHealthQuery({
-      ...options,
-      variables: { ...options.variables, appId: currentProject?.id },
-      skip: !isPlatform || !currentProject,
-      pollInterval: 0,
-      skipPollAttempt: () => !isVisible,
-    });
+  const { data, loading, refetch } = useGetProjectServicesHealthQuery({
+    ...options,
+    variables: { ...options.variables, appId: currentProject?.id },
+    skip: !isPlatform || !currentProject,
+    pollInterval: options.shouldPoll ? options.pollInterval || 10000 : 0,
+    skipPollAttempt: () => !isVisible,
+  });
 
   // Fetch when mounted
   useEffect(() => {
     refetch();
-    return () => {
-      stopPolling();
-    };
-  }, [refetch, stopPolling]);
+  }, [refetch]);
 
   const serviceMap: { [key: string]: ServiceHealthInfo | undefined } = {};
   data?.getProjectStatus?.services.forEach((service) => {
@@ -56,37 +51,6 @@ export default function useServiceStatus(
     ai,
     ...run
   } = serviceMap;
-
-  // Change poll interval based on services states
-  useEffect(() => {
-    if (!options.shouldPoll) {
-      return () => stopPolling();
-    }
-    if (
-      [auth, storage, postgres, hasura, ai, ...Object.values(run)].some(
-        (service) => service?.state !== ServiceState.Running,
-      )
-    ) {
-      // Poll more frequently if a service is not running
-      startPolling(options.pollInterval || 10000);
-    } else {
-      // Poll less frequently when all services are running
-      startPolling(60000);
-    }
-
-    return () => stopPolling();
-  }, [
-    ai,
-    auth,
-    hasura,
-    postgres,
-    run,
-    storage,
-    options.pollInterval,
-    options.shouldPoll,
-    startPolling,
-    stopPolling,
-  ]);
 
   return {
     loading,
