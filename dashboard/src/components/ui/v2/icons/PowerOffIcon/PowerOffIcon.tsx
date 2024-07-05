67f1a7d2c4a7917d3161499172676beaa831c4e3
Barrarroso <correodelnino@gmail.com>
2024-07-05 21:26:39 +0200

    chore: fix css

--- dashboard/src/components/ui/v2/icons/PowerOffIcon/PowerOffIcon.tsx
+++ dashboard/src/components/ui/v2/icons/PowerOffIcon/PowerOffIcon.tsx
@@ -14,7 +14,10 @@ function PowerOffIcon(props: IconProps) {
       strokeWidth={2}
       {...props}
     >
-      <path d="M18.36 6.64A9 9 0 0 1 20.77 15M6.16 6.16a9 9 0 1 0 12.68 12.68M12 2v4M2 2l20 20" />
+      <path
+        fill="none"
+        d="M18.36 6.64A9 9 0 0 1 20.77 15M6.16 6.16a9 9 0 1 0 12.68 12.68M12 2v4M2 2l20 20"
+      />
     </SvgIcon>
   );
 }
