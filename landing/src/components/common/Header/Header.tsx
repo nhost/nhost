5c628b7a7704667b12ae80bf3212a4719722113a
Barrarroso <correodelnino@gmail.com>
2024-07-08 14:00:11 +0200

    feat: show repo star count on header

--- landing/src/components/common/Header/Header.tsx
+++ landing/src/components/common/Header/Header.tsx
@@ -1,6 +1,6 @@
 import { Transition } from '@headlessui/react'
 import Image from 'next/image'
-import { Fragment, useEffect, useState } from 'react'
+import { Fragment, useEffect, useMemo, useState } from 'react'
 import { twMerge } from 'tailwind-merge'
 import { Button } from '../Button'
 import { Container, ContainerProps } from '../Container'
@@ -26,6 +26,38 @@ export default function Header({
       document.body.style.overflowY = 'visible'
     }
   }, [mobileMenuVisible])
+
+  const [stargazersCount, setStargazersCount] = useState<string>('7K')
+
+  useEffect(() => {
+    const fetchStargazersCount = async () => {
+      try {
+        const res = await fetch('https://api.github.com/repos/nhost/nhost')
+        if (!res.ok) {
+          throw new Error(`HTTP error: Status ${res.status}`)
+        }
+        const data = await res.json()
+
+        const newStargazersCount = data?.stargazers_count
+        if (!newStargazersCount) {
+          return
+        }
+
+        const formatter = Intl.NumberFormat('en', {
+          notation: 'compact',
+          compactDisplay: 'short',
+        })
+
+        const stargazersCountFormatted = formatter.format(newStargazersCount)
+
+        setStargazersCount(stargazersCountFormatted)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    fetchStargazersCount()
+  }, [])
 
   return (
     <>
@@ -107,7 +139,7 @@ export default function Header({
           </ul>
         </nav>
 
-        <div className="flex justify-between">
+        <div className="hidden justify-between lg:flex">
           <a
             className="flex flex-row items-center justify-center rounded px-2.5 py-1.5 text-base font-medium leading-snug opacity-50 transition-all duration-200 ease-in-out hover:opacity-100"
             href="https://github.com/nhost/nhost"
@@ -121,7 +153,7 @@ export default function Header({
               height={22}
               alt="Nhost on GitHub"
             />
-            <span className="truncate">Star us on GitHub</span>
+            <span className="truncate">{stargazersCount}</span>
           </a>
 
           <div className="hidden grid-flow-col gap-4 lg:grid">
