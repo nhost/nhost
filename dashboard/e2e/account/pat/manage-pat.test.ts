813bf46a489544fb6a8ac569996ace7e9335486c
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2024-07-25 10:25:20 +0100

    fix: remove waitForLoadState in PAT e2e test

--- dashboard/e2e/account/pat/manage-pat.test.ts
+++ dashboard/e2e/account/pat/manage-pat.test.ts
@@ -17,7 +17,6 @@ test.afterAll(async () => {
 });
 
 test('should be able to create then delete a personal access token', async () => {
-  await page.waitForLoadState('networkidle');
   await page.getByRole('banner').getByRole('button').last().click();
   await page.getByRole('link', { name: /account settings/i }).click();
   await page
