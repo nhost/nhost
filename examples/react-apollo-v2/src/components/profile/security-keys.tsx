4f94d0ed93264df301fa7944f617dbb256b1b816
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2024-08-15 11:07:44 +0100

    feat: profile security keys

--- examples/react-apollo-v2/src/components/profile/security-keys.tsx
+++ examples/react-apollo-v2/src/components/profile/security-keys.tsx
@@ -1,6 +1,49 @@
+// import { gql, useApolloClient } from '@apollo/client'
+// import { useAddSecurityKey, useUserId } from '@nhost/react'
+// import { useState } from 'react'
 import { Card, CardDescription, CardHeader, CardTitle } from '../ui/card'
+import { useAuthQuery } from '@nhost/react-apollo'
+import { gql } from '@apollo/client'
+import { useState } from 'react'
+import { useUserId } from '@nhost/react'
+
+type SecurityKey = {
+  id: string
+  nickname: string | null
+}
+
+type SecurityKeysQuery = {
+  authUserSecurityKeys: SecurityKey[]
+}
 
 export default function SecurityKeys() {
+  const userId = useUserId()
+  // const client = useApolloClient()
+  // const { add } = useAddSecurityKey()
+
+  // Nickname of the security key
+  // const [nickname, setNickname] = useState('')
+  const [list, setList] = useState<SecurityKey[]>([])
+
+  useAuthQuery<SecurityKeysQuery>(
+    gql`
+      query securityKeys($userId: uuid!) {
+        authUserSecurityKeys(where: { userId: { _eq: $userId } }) {
+          id
+          nickname
+        }
+      }
+    `,
+    {
+      variables: { userId },
+      onCompleted: ({ authUserSecurityKeys }) => {
+        if (authUserSecurityKeys) {
+          setList(authUserSecurityKeys || [])
+        }
+      }
+    }
+  )
+
   return (
     <Card className="w-full">
       <CardHeader>
