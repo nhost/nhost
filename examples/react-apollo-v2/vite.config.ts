87eba96ce54d3780a57214b6dd6e205b4de89980
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2024-08-22 17:20:48 +0100

    chore: update Vite server port to 3000

--- examples/react-apollo-v2/vite.config.ts
+++ examples/react-apollo-v2/vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
