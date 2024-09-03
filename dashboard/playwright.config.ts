1e71116826cb90c850a41bdbaccbaaa0bcf170f5
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2024-09-03 16:58:43 +0100

    chore: add slowMo option to launch settings for better test observation

--- dashboard/playwright.config.ts
+++ dashboard/playwright.config.ts
@@ -10,16 +10,19 @@ export default defineConfig({
   expect: {
     timeout: 5000,
   },
-  fullyParallel: true,
+  fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   reporter: 'html',
   globalTeardown: require.resolve('./global-teardown'),
   use: {
     actionTimeout: 0,
     trace: 'on-first-retry',
     baseURL: process.env.NHOST_TEST_DASHBOARD_URL,
+    launchOptions: {
+      slowMo: 1000,
+    },
   },
   projects: [
     {
