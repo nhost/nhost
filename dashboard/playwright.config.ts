43d9a0a6c09df639336ae83db91d33e69dd0124e
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2024-09-03 18:52:03 +0100

    fix: adjust slowMo and increase timeout

--- dashboard/playwright.config.ts
+++ dashboard/playwright.config.ts
@@ -6,7 +6,7 @@ dotenv.config({ path: path.resolve(__dirname, '.env.test') });
 
 export default defineConfig({
   testDir: './e2e',
-  timeout: 30 * 1000,
+  timeout: 60 * 1000,
   expect: {
     timeout: 5000,
   },
@@ -21,7 +21,7 @@ export default defineConfig({
     trace: 'on-first-retry',
     baseURL: process.env.NHOST_TEST_DASHBOARD_URL,
     launchOptions: {
-      slowMo: 1000,
+      slowMo: 500,
     },
   },
   projects: [
