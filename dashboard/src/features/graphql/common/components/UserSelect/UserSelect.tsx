526183ab881cafd21ae8a11ceffd80fdc4d2198b
David BM <correodelnino@gmail.com>
2024-09-09 05:55:49 -0400

    feat (dashboard): allow filtering users in "make request as" feature of graphql section (#2805)
    Resolves #2593

--- dashboard/src/features/graphql/common/components/UserSelect/UserSelect.tsx
+++ dashboard/src/features/graphql/common/components/UserSelect/UserSelect.tsx
@@ -1,11 +1,12 @@
-import { ActivityIndicator } from '@/components/ui/v2/ActivityIndicator';
-import { Option } from '@/components/ui/v2/Option';
-import { Select } from '@/components/ui/v2/Select';
+import { Autocomplete } from '@/components/ui/v2/Autocomplete';
 import { DEFAULT_ROLES } from '@/features/graphql/common/utils/constants';
-import { useCurrentWorkspaceAndProject } from '@/features/projects/common/hooks/useCurrentWorkspaceAndProject';
 import { useRemoteApplicationGQLClient } from '@/hooks/useRemoteApplicationGQLClient';
-import type { RemoteAppGetUsersCustomQuery } from '@/utils/__generated__/graphql';
-import { useRemoteAppGetUsersCustomQuery } from '@/utils/__generated__/graphql';
+import {
+  useRemoteAppGetUsersCustomLazyQuery,
+  type RemoteAppGetUsersCustomQuery,
+} from '@/utils/__generated__/graphql';
+import { debounce } from '@mui/material/utils';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 
 export interface UserSelectProps {
   /**
@@ -13,7 +14,7 @@ export interface UserSelectProps {
    */
   onUserChange: (userId: string, availableRoles?: string[]) => void;
   /**
-   * Class name to be applied to the `<Select />` element.
+   * Class name to be applied to the `<Autocomplete />` element.
    */
   className?: string;
 }
@@ -22,35 +23,87 @@ export default function UserSelect({
   onUserChange,
   ...props
 }: UserSelectProps) {
-  const { currentProject } = useCurrentWorkspaceAndProject();
+  const [inputValue, setInputValue] = useState('');
+  const [users, setUsers] = useState([]);
+  const [active, setActive] = useState(true);
+
   const userApplicationClient = useRemoteApplicationGQLClient();
-  const { data, loading, error } = useRemoteAppGetUsersCustomQuery({
+
+  const [fetchAppUsers, { loading }] = useRemoteAppGetUsersCustomLazyQuery({
     client: userApplicationClient,
-    variables: { where: {}, limit: 250, offset: 0 },
-    skip: !currentProject,
+    variables: {
+      where: {},
+      limit: 250,
+      offset: 0,
+    },
   });
 
-  if (loading) {
-    return (
-      <div className={props.className}>
-        <ActivityIndicator label="Loading users..." delay={500} />
-      </div>
-    );
-  }
+  const fetchUsers = useCallback(
+    async (
+      request: { input: string },
+      callback: (results?: RemoteAppGetUsersCustomQuery['users']) => void,
+    ) => {
+      const ilike = `%${request.input === 'Admin' ? '' : request.input}%`;
+      const { data } = await fetchAppUsers({
+        client: userApplicationClient,
+        variables: {
+          where: {
+            displayName: { _ilike: ilike },
+          },
+          limit: 250,
+          offset: 0,
+        },
+      });
 
-  if (error) {
-    throw error;
-  }
+      callback(data?.users);
+    },
+    [fetchAppUsers, userApplicationClient],
+  );
+
+  const fetchOptions = useMemo(() => debounce(fetchUsers, 1000), [fetchUsers]);
+
+  useEffect(() => {
+    fetchOptions({ input: inputValue }, (results) => {
+      if (active || inputValue === '') {
+        setUsers(results);
+      }
+    });
+  }, [inputValue, fetchOptions, active]);
+
+  const autocompleteOptions = [
+    {
+      value: 'admin',
+      label: 'Admin',
+      group: 'Admin',
+    },
+    ...users.map((user) => ({
+      value: user.id,
+      label: user.displayName,
+      group: 'Users',
+    })),
+  ];
 
   return (
-    <Select
+    <Autocomplete
       {...props}
       id="user-select"
-      label="Make Request As"
-      hideEmptyHelperText
-      defaultValue="admin"
-      slotProps={{ root: { className: 'truncate' } }}
-      onChange={(_event, userId) => {
+      label="Make request as"
+      options={autocompleteOptions}
+      defaultValue={{
+        value: 'admin',
+        label: 'Admin',
+        group: 'Admin',
+      }}
+      autoComplete
+      fullWidth
+      autoSelect
+      groupBy={(option) => option.group}
+      autoHighlight
+      includeInputInList
+      loading={loading}
+      onChange={(_event, _value, reason, details) => {
+        setActive(false);
+        const userId = details.option.value;
         if (typeof userId !== 'string') {
           return;
         }
@@ -61,22 +114,23 @@ export default function UserSelect({
           return;
         }
 
-        const user: RemoteAppGetUsersCustomQuery['users'][0] = data?.users.find(
+        const user: RemoteAppGetUsersCustomQuery['users'][0] = users.find(
           ({ id }) => id === userId,
         );
 
-        const roles = user?.roles.map(({ role }) => role);
+        const roles = user?.roles?.map(({ role }) => role);
 
-        onUserChange(user.id, roles);
+        onUserChange(userId, roles ?? DEFAULT_ROLES);
+
+        fetchUsers({ input: '' }, (results) => {
+          if (results) {
+            setUsers(results);
+          }
+        });
       }}
-    >
-      <Option value="admin">Admin</Option>
-
-      {data?.users.map(({ id, displayName, email, phoneNumber }) => (
-        <Option key={id} value={id}>
-          {displayName || email || phoneNumber || id}
-        </Option>
-      ))}
-    </Select>
+      onInputChange={(event, newInputValue) => {
+        setInputValue(newInputValue);
+      }}
+    />
   );
 }
