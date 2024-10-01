2d01d9a85dc139deaca26417198e7ce225fd0c0e
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2024-10-01 15:31:28 +0100

    fix: update slug for database section URL

--- dashboard/src/components/layout/Header/ProjectPagesComboBox.tsx
+++ dashboard/src/components/layout/Header/ProjectPagesComboBox.tsx
@@ -46,79 +46,79 @@ const projectPages = [
   {
     label: 'Overview',
     value: 'overview',
-    icon: <HomeIcon className="h-4 w-4" />,
+    icon: <HomeIcon className="w-4 h-4" />,
     slug: '',
   },
   {
     label: 'Database',
     value: 'database',
-    icon: <DatabaseIcon className="h-4 w-4" />,
-    slug: 'database',
+    icon: <DatabaseIcon className="w-4 h-4" />,
+    slug: '/database/browser/default',
   },
   {
     label: 'GraphQL',
     value: 'graphql',
-    icon: <GraphQLIcon className="h-4 w-4" />,
+    icon: <GraphQLIcon className="w-4 h-4" />,
     slug: 'graphql',
   },
   {
     label: 'Hasura',
     value: 'hasura',
-    icon: <HasuraIcon className="h-4 w-4" />,
+    icon: <HasuraIcon className="w-4 h-4" />,
     slug: 'hasura',
   },
   {
     label: 'Auth',
     value: 'users',
-    icon: <UserIcon className="h-4 w-4" />,
+    icon: <UserIcon className="w-4 h-4" />,
     slug: 'users',
   },
   {
     label: 'Storage',
     value: 'storage',
-    icon: <StorageIcon className="h-4 w-4" />,
+    icon: <StorageIcon className="w-4 h-4" />,
     slug: 'storage',
   },
   {
     label: 'Run',
     value: 'run',
-    icon: <ServicesIcon className="h-4 w-4" />,
+    icon: <ServicesIcon className="w-4 h-4" />,
     slug: 'run',
   },
   {
     label: 'AI',
     value: 'ai',
-    icon: <AIIcon className="h-4 w-4" />,
+    icon: <AIIcon className="w-4 h-4" />,
     slug: 'ai',
   },
   {
     label: 'Deployments',
     value: 'deployments',
-    icon: <RocketIcon className="h-4 w-4" />,
+    icon: <RocketIcon className="w-4 h-4" />,
     slug: 'deployments',
   },
   {
     label: 'Backups',
     value: 'backups',
-    icon: <CloudIcon className="h-4 w-4" />,
+    icon: <CloudIcon className="w-4 h-4" />,
     slug: 'backups',
   },
   {
     label: 'Logs',
     value: 'logs',
-    icon: <FileTextIcon className="h-4 w-4" />,
+    icon: <FileTextIcon className="w-4 h-4" />,
     slug: 'logs',
   },
   {
     label: 'Metrics',
     value: 'metrics',
-    icon: <GaugeIcon className="h-4 w-4" />,
+    icon: <GaugeIcon className="w-4 h-4" />,
     slug: 'metrics',
   },
   {
     label: 'Settings',
     value: 'settings',
-    icon: <CogIcon className="h-4 w-4" />,
+    icon: <CogIcon className="w-4 h-4" />,
     slug: 'settings',
   },
 ];
@@ -173,7 +173,7 @@ export default function ProjectPagesComboBox() {
           ) : (
             <>Select a page</>
           )}
-          <ChevronsUpDown className="h-5 w-5 text-muted-foreground" />
+          <ChevronsUpDown className="w-5 h-5 text-muted-foreground" />
         </Button>
       </PopoverTrigger>
       <PopoverContent className="p-0" side="bottom" align="start">
@@ -204,7 +204,7 @@ export default function ProjectPagesComboBox() {
                   />
                   <div className="flex flex-row items-center gap-2">
                     {option.icon}
-                    <span className="max-w-52 truncate">{option.label}</span>
+                    <span className="truncate max-w-52">{option.label}</span>
                   </div>
                 </CommandItem>
               ))}
