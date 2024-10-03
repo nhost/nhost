06a47093fa37d0d588333073f0c416cb2fb2d488
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2024-10-03 15:27:46 +0100

    feat: add settings > secrets under orgs

---
+++ dashboard/src/features/orgs/projects/secrets/settings/components/CreateSecretForm/CreateSecretForm.tsx
@@ -0,0 +1,104 @@
+import { ApplyLocalSettingsDialog } from '@/components/common/ApplyLocalSettingsDialog';
+import { useDialog } from '@/components/common/DialogProvider';
+import type {
+  BaseSecretFormProps,
+  BaseSecretFormValues,
+} from '@/features/orgs/projects/secrets/settings/components/BaseSecretForm';
+import {
+  BaseSecretForm,
+  baseSecretFormValidationSchema,
+} from '@/features/orgs/projects/secrets/settings/components/BaseSecretForm';
+import {
+  GetSecretsDocument,
+  useInsertSecretMutation,
+} from '@/utils/__generated__/graphql';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { FormProvider, useForm } from 'react-hook-form';
+
+import { useIsPlatform } from '@/features/orgs/projects/common/hooks/useIsPlatform';
+import { useLocalMimirClient } from '@/features/orgs/projects/hooks/useLocalMimirClient';
+import { useProject } from '@/features/orgs/projects/hooks/useProject';
+import { execPromiseWithErrorToast } from '@/features/orgs/utils/execPromiseWithErrorToast';
+
+export interface CreateSecretFormProps
+  extends Pick<BaseSecretFormProps, 'onCancel'> {
+  /**
+   * Function to be called when the form is submitted.
+   */
+  onSubmit?: () => Promise<any>;
+}
+
+export default function CreateSecretForm({
+  onSubmit,
+  ...props
+}: CreateSecretFormProps) {
+  const { openDialog } = useDialog();
+  const isPlatform = useIsPlatform();
+  const localMimirClient = useLocalMimirClient();
+
+  const form = useForm<BaseSecretFormValues>({
+    defaultValues: {
+      name: '',
+      value: '',
+    },
+    reValidateMode: 'onSubmit',
+    resolver: yupResolver(baseSecretFormValidationSchema),
+  });
+
+  const { project } = useProject();
+  const [insertSecret] = useInsertSecretMutation({
+    refetchQueries: [GetSecretsDocument],
+    ...(!isPlatform ? { client: localMimirClient } : {}),
+  });
+
+  async function handleSubmit({ name, value }: BaseSecretFormValues) {
+    const insertSecretPromise = insertSecret({
+      variables: {
+        appId: project?.id,
+        secret: {
+          name,
+          value,
+        },
+      },
+    });
+
+    try {
+      await execPromiseWithErrorToast(
+        async () => {
+          await insertSecretPromise;
+          await onSubmit?.();
+
+          if (!isPlatform) {
+            openDialog({
+              title: 'Apply your changes',
+              component: <ApplyLocalSettingsDialog />,
+              props: {
+                PaperProps: {
+                  className: 'max-w-2xl',
+                },
+              },
+            });
+          }
+        },
+        {
+          loadingMessage: 'Creating secret...',
+          successMessage: 'Secret has been created successfully.',
+          errorMessage: 'An error occurred while creating the secret.',
+        },
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <FormProvider {...form}>
+      <BaseSecretForm
+        mode="create"
+        submitButtonText="Create"
+        onSubmit={handleSubmit}
+        {...props}
+      />
+    </FormProvider>
+  );
+}
--- dashboard/src/pages/orgs/[orgSlug]/projects/[appSlug]/settings/secrets.tsx
+++ dashboard/src/pages/orgs/[orgSlug]/projects/[appSlug]/settings/secrets.tsx
@@ -1,15 +1,256 @@
-import { OrgProjectLayout } from '@/components/layout/OrgProjectLayout';
-import { RetryableErrorBoundary } from '@/components/presentational/RetryableErrorBoundary';
+import { ApplyLocalSettingsDialog } from '@/components/common/ApplyLocalSettingsDialog';
+import { useDialog } from '@/components/common/DialogProvider';
+import { useUI } from '@/components/common/UIProvider';
+import { Container } from '@/components/layout/Container';
+import { SettingsContainer } from '@/components/layout/SettingsContainer';
+import { SettingsLayout } from '@/components/layout/SettingsLayout';
+import { InlineCode } from '@/components/presentational/InlineCode';
+import { ActivityIndicator } from '@/components/ui/v2/ActivityIndicator';
+import { Box } from '@/components/ui/v2/Box';
+import { Button } from '@/components/ui/v2/Button';
+import { Divider } from '@/components/ui/v2/Divider';
+import { Dropdown } from '@/components/ui/v2/Dropdown';
+import { IconButton } from '@/components/ui/v2/IconButton';
+import { DotsVerticalIcon } from '@/components/ui/v2/icons/DotsVerticalIcon';
+import { PlusIcon } from '@/components/ui/v2/icons/PlusIcon';
+import { List } from '@/components/ui/v2/List';
+import { ListItem } from '@/components/ui/v2/ListItem';
+import { Text } from '@/components/ui/v2/Text';
+import { CreateSecretForm } from '@/features/orgs/projects/secrets/settings/components/CreateSecretForm';
+import { EditSecretForm } from '@/features/orgs/projects/secrets/settings/components/EditSecretForm';
+import type { Secret } from '@/types/application';
+import {
+  GetSecretsDocument,
+  useDeleteSecretMutation,
+  useGetSecretsQuery,
+} from '@/utils/__generated__/graphql';
 import type { ReactElement } from 'react';
+import { Fragment } from 'react';
+import { twMerge } from 'tailwind-merge';
 
-export default function SettingsSecrets() {
+import { useIsPlatform } from '@/features/orgs/projects/common/hooks/useIsPlatform';
+import { useLocalMimirClient } from '@/features/orgs/projects/hooks/useLocalMimirClient';
+import { useProject } from '@/features/orgs/projects/hooks/useProject';
+import { execPromiseWithErrorToast } from '@/features/orgs/utils/execPromiseWithErrorToast';
+
+export default function SecretsPage() {
+  const { project } = useProject();
+  const isPlatform = useIsPlatform();
+  const { maintenanceActive } = useUI();
+  const localMimirClient = useLocalMimirClient();
+  const { openDialog, openAlertDialog } = useDialog();
+
+  const { data, loading, error, refetch } = useGetSecretsQuery({
+    variables: { appId: project?.id },
+    ...(!isPlatform ? { client: localMimirClient } : {}),
+  });
+
+  const [deleteSecret] = useDeleteSecretMutation({
+    refetchQueries: [GetSecretsDocument],
+    ...(!isPlatform ? { client: localMimirClient } : {}),
+  });
+
+  if (loading) {
+    return <ActivityIndicator delay={1000} label="Loading secrets..." />;
+  }
+
+  if (error) {
+    throw error;
+  }
+
+  async function handleDeleteSecret(secret: Secret) {
+    const deleteSecretPromise = deleteSecret({
+      variables: {
+        appId: project?.id,
+        name: secret.name,
+      },
+    });
+
+    await execPromiseWithErrorToast(
+      async () => {
+        await deleteSecretPromise;
+        await refetch();
+
+        if (!isPlatform) {
+          openDialog({
+            title: 'Apply your changes',
+            component: <ApplyLocalSettingsDialog />,
+            props: {
+              PaperProps: {
+                className: 'max-w-2xl',
+              },
+            },
+          });
+        }
+      },
+      {
+        loadingMessage: 'Deleting secret...',
+        successMessage: 'Secret has been deleted successfully.',
+        errorMessage: 'An error occurred while deleting the secret.',
+      },
+    );
+  }
+
+  function handleOpenCreator() {
+    openDialog({
+      title: 'Create Secret',
+      component: <CreateSecretForm onSubmit={refetch} />,
+      props: {
+        titleProps: { className: '!pb-0' },
+        PaperProps: { className: 'gap-2 max-w-md' },
+      },
+    });
+  }
+
+  function handleOpenEditor(originalSecret: Secret) {
+    openDialog({
+      title: 'Edit Secret',
+      component: <EditSecretForm originalSecret={originalSecret} />,
+      props: {
+        titleProps: { className: '!pb-0' },
+        PaperProps: { className: 'gap-2 max-w-md' },
+      },
+    });
+  }
+
+  function handleConfirmDelete(originalSecret: Secret) {
+    openAlertDialog({
+      title: 'Delete Secret',
+      payload: (
+        <Text>
+          Are you sure you want to delete the &quot;
+          <strong>{originalSecret.name}</strong>&quot; secret? This cannot be
+          undone.
+        </Text>
+      ),
+      props: {
+        primaryButtonColor: 'error',
+        primaryButtonText: 'Delete',
+        onPrimaryAction: () => handleDeleteSecret(originalSecret),
+      },
+    });
+  }
+
+  const secrets = data?.appSecrets || [];
+
   return (
-    <RetryableErrorBoundary>
-      <span>Secrets</span>
-    </RetryableErrorBoundary>
+    <Container
+      className="grid max-w-5xl grid-flow-row gap-6 bg-transparent"
+      rootClassName="bg-transparent"
+    >
+      <SettingsContainer
+        title="Secrets"
+        description={
+          <span>
+            To prevent exposing sensitive information, use secrets in your
+            configuration by replacing the actual value with{' '}
+            <InlineCode className="rounded-sm py-0.5 text-xs">
+              &#123;&#123; secrets.SECRET_NAME &#125;&#125;
+            </InlineCode>{' '}
+            in any configuration placeholder.
+          </span>
+        }
+        rootClassName="gap-0 pb-0"
+        className={twMerge('my-2 px-0', secrets.length === 0 && 'gap-2')}
+        slotProps={{
+          submitButton: { className: 'hidden' },
+          footer: { className: 'hidden' },
+        }}
+      >
+        <Box className="grid grid-cols-2 gap-2 px-4 py-3 border-b-1">
+          <Text className="font-medium">Secret Name</Text>
+        </Box>
+
+        <Box className="grid grid-flow-row gap-2">
+          {secrets.length > 0 && (
+            <List>
+              {secrets.map((secret, index) => (
+                <Fragment key={secret.name}>
+                  <ListItem.Root
+                    className="grid grid-cols-2 gap-2 px-4"
+                    secondaryAction={
+                      <Dropdown.Root>
+                        <Dropdown.Trigger
+                          asChild
+                          hideChevron
+                          className="absolute -translate-y-1/2 right-4 top-1/2"
+                        >
+                          <IconButton
+                            variant="borderless"
+                            color="secondary"
+                            disabled={maintenanceActive}
+                          >
+                            <DotsVerticalIcon />
+                          </IconButton>
+                        </Dropdown.Trigger>
+
+                        <Dropdown.Content
+                          menu
+                          PaperProps={{ className: 'w-32' }}
+                          anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'right',
+                          }}
+                          transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                          }}
+                        >
+                          <Dropdown.Item
+                            onClick={() => handleOpenEditor(secret)}
+                          >
+                            <Text className="font-medium">Edit</Text>
+                          </Dropdown.Item>
+
+                          <Divider component="li" />
+
+                          <Dropdown.Item
+                            onClick={() => handleConfirmDelete(secret)}
+                          >
+                            <Text
+                              className="font-medium"
+                              sx={{
+                                color: (theme) => theme.palette.error.main,
+                              }}
+                            >
+                              Delete
+                            </Text>
+                          </Dropdown.Item>
+                        </Dropdown.Content>
+                      </Dropdown.Root>
+                    }
+                  >
+                    <ListItem.Text className="truncate">
+                      {secret.name}
+                    </ListItem.Text>
+                  </ListItem.Root>
+
+                  <Divider
+                    component="li"
+                    className={twMerge(
+                      index === secrets.length - 1 ? '!mt-4' : '!my-4',
+                    )}
+                  />
+                </Fragment>
+              ))}
+            </List>
+          )}
+
+          <Button
+            className="mx-4 justify-self-start"
+            variant="borderless"
+            startIcon={<PlusIcon />}
+            onClick={handleOpenCreator}
+            disabled={maintenanceActive}
+          >
+            Create Secret
+          </Button>
+        </Box>
+      </SettingsContainer>
+    </Container>
   );
 }
 
-SettingsSecrets.getLayout = function getLayout(page: ReactElement) {
-  return <OrgProjectLayout>{page}</OrgProjectLayout>;
+SecretsPage.getLayout = function getLayout(page: ReactElement) {
+  return <SettingsLayout>{page}</SettingsLayout>;
 };
