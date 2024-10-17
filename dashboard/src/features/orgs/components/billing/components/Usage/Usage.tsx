d311acb35a7a97ddf7b00b7e57cec4d302ded9b7
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2024-10-17 11:47:06 +0100

    feat: add org billing page

--- dashboard/src/features/orgs/components/billing/components/Usage/Usage.tsx
+++ dashboard/src/features/orgs/components/billing/components/Usage/Usage.tsx
@@ -1,4 +1,3 @@
-import { ActivityIndicator } from '@/components/ui/v2/ActivityIndicator';
 import { Progress } from '@/components/ui/v3/progress';
 import {
   Table,
@@ -16,8 +15,7 @@ import { useBillingGetNextInvoiceQuery } from '@/utils/__generated__/graphql';
 export default function Usage() {
   const { org } = useCurrentOrg();
   const { billingCycleRange, progress } = getBillingCycleInfo();
-  const { data, loading } = useBillingGetNextInvoiceQuery({
-    fetchPolicy: 'cache-first',
+  const { data } = useBillingGetNextInvoiceQuery({
     variables: {
       organizationID: org?.id,
     },
@@ -30,65 +28,42 @@ export default function Usage() {
   return (
     <div className="font-medium">
       <div className="flex flex-col w-full border rounded-md bg-background">
-        <div className="flex flex-col w-full gap-1 p-4">
+        <div className="flex flex-col w-full gap-1 p-4 border-b">
           <span>Usage</span>
         </div>
-        <div className="flex flex-col">
-          <div className="flex flex-row items-center justify-between w-full p-4 border-t border-b">
+        <div className="flex flex-col border-b">
+          <div className="flex flex-row items-center justify-between w-full p-4 border-b">
             <span>Billing cycle ({billingCycleRange})</span>
             <Progress value={progress} className="h-2 max-w-xl" />
           </div>
           <div className="flex flex-col gap-4 p-4">
-            {loading && (
-              <div className="flex h-32 place-content-center">
-                <ActivityIndicator
-                  label="Loading usage stats..."
-                  className="justify-center text-sm"
-                />
-              </div>
-            )}
-
-            {!loading && data && (
-              <>
-                <span>Breakdown</span>
-                <div className="border rounded-md">
-                  <Table>
-                    <TableHeader className="w-full bg-accent">
-                      <TableRow>
-                        <TableHead colSpan={3} className="w-full rounded-tl-md">
-                          Item
-                        </TableHead>
-                        <TableHead className="text-right rounded-tr-md">
-                          Amount
-                        </TableHead>
-                      </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                      {billingItems.map((billingItem) => (
-                        <TableRow key={billingItem.Description}>
-                          <TableCell colSpan={3}>
-                            {billingItem.Description}
-                          </TableCell>
-                          <TableCell colSpan={3} className="text-right">
-                            ${billingItem.Amount}
-                          </TableCell>
-                        </TableRow>
-                      ))}
-                    </TableBody>
-                    <TableFooter className="bg-accent">
-                      <TableRow>
-                        <TableCell colSpan={3} className="rounded-bl-md">
-                          Total
-                        </TableCell>
-                        <TableCell className="text-right rounded-br-md">
-                          ${amountDue}
-                        </TableCell>
-                      </TableRow>
-                    </TableFooter>
-                  </Table>
-                </div>
-              </>
-            )}
+            <span>Breakdown</span>
+            <Table className="border rounded-md">
+              <TableHeader className="w-full">
+                <TableRow>
+                  <TableHead colSpan={3} className="w-full">
+                    Item
+                  </TableHead>
+                  <TableHead className="text-right">Amount</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {billingItems.map((billingItem) => (
+                  <TableRow key={billingItem.Description}>
+                    <TableCell colSpan={3}>{billingItem.Description}</TableCell>
+                    <TableCell colSpan={3} className="text-right">
+                      {billingItem.Amount}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+              <TableFooter>
+                <TableRow>
+                  <TableCell colSpan={3}>Total</TableCell>
+                  <TableCell className="text-right">${amountDue}</TableCell>
+                </TableRow>
+              </TableFooter>
+            </Table>
           </div>
         </div>
       </div>
