cc031284f4608fbbe0c3237e0c9d346d23dcf9de
David BM <correodelnino@gmail.com>
2024-10-18 18:37:18 -0400

    wip: add alerting toggle setting

---
+++ dashboard/src/features/orgs/projects/metrics/settings/components/GrafanaAlertingSettings/GrafanaAlertingSettings.tsx
@@ -0,0 +1,143 @@
+import { ApplyLocalSettingsDialog } from '@/components/common/ApplyLocalSettingsDialog';
+import { useDialog } from '@/components/common/DialogProvider';
+import { useUI } from '@/components/common/UIProvider';
+import { Form } from '@/components/form/Form';
+import { SettingsContainer } from '@/components/layout/SettingsContainer';
+import { ActivityIndicator } from '@/components/ui/v2/ActivityIndicator';
+import {
+  GetObservabilitySettingsDocument,
+  useGetObservabilitySettingsQuery,
+  useUpdateConfigMutation,
+} from '@/generated/graphql';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useEffect } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+import * as Yup from 'yup';
+
+import { useIsPlatform } from '@/features/orgs/projects/common/hooks/useIsPlatform';
+import { useLocalMimirClient } from '@/features/orgs/projects/hooks/useLocalMimirClient';
+import { useProject } from '@/features/orgs/projects/hooks/useProject';
+import { execPromiseWithErrorToast } from '@/features/orgs/utils/execPromiseWithErrorToast';
+
+const validationSchema = Yup.object({
+  enabled: Yup.boolean(),
+});
+
+export type GrafanaAlertingSettingsFormValues = Yup.InferType<
+  typeof validationSchema
+>;
+
+export default function GrafanaAlertingSettings() {
+  const { openDialog } = useDialog();
+  const isPlatform = useIsPlatform();
+  const { maintenanceActive } = useUI();
+  const localMimirClient = useLocalMimirClient();
+  const { project, refetch: refetchProject } = useProject();
+  const [updateConfig] = useUpdateConfigMutation({
+    refetchQueries: [GetObservabilitySettingsDocument],
+    ...(!isPlatform ? { client: localMimirClient } : {}),
+  });
+
+  const { data, loading, error } = useGetObservabilitySettingsQuery({
+    variables: { appId: project?.id },
+    ...(!isPlatform ? { client: localMimirClient } : {}),
+  });
+
+  const { enabled: alertingEnabled } =
+    data?.config?.observability.grafana.alerting || {};
+
+  const form = useForm<GrafanaAlertingSettingsFormValues>({
+    reValidateMode: 'onSubmit',
+    defaultValues: {
+      enabled: alertingEnabled,
+    },
+    resolver: yupResolver(validationSchema),
+  });
+
+  useEffect(() => {
+    if (!loading) {
+      form.reset({
+        enabled: alertingEnabled,
+      });
+    }
+  }, [loading, alertingEnabled, form]);
+
+  if (loading) {
+    return (
+      <ActivityIndicator
+        delay={1000}
+        label="Loading Alerting settings..."
+        className="justify-center"
+      />
+    );
+  }
+
+  if (error) {
+    throw error;
+  }
+
+  async function handleSubmit(formValues: GrafanaAlertingSettingsFormValues) {
+    const updateConfigPromise = updateConfig({
+      variables: {
+        appId: project.id,
+        config: {
+          observability: {
+            grafana: {
+              alerting: {
+                enabled: formValues.enabled,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    await execPromiseWithErrorToast(
+      async () => {
+        await updateConfigPromise;
+        form.reset(formValues);
+        await refetchProject();
+
+        if (!isPlatform) {
+          openDialog({
+            title: 'Apply your changes',
+            component: <ApplyLocalSettingsDialog />,
+            props: {
+              PaperProps: {
+                className: 'max-w-2xl',
+              },
+            },
+          });
+        }
+      },
+      {
+        loadingMessage: 'Alerting settings are being updated...',
+        successMessage: 'Alerting settings have been updated successfully.',
+        errorMessage:
+          'An error occurred while trying to update alerting settings.',
+      },
+    );
+  }
+
+  return (
+    <FormProvider {...form}>
+      <Form onSubmit={handleSubmit}>
+        <SettingsContainer
+          title="Alerting"
+          description="Enable or disable Alerting."
+          slotProps={{
+            submitButton: {
+              disabled: !form.formState.isDirty || maintenanceActive,
+              loading: form.formState.isSubmitting,
+            },
+          }}
+          switchId="enabled"
+          docsTitle="enabling or disabling Grafana Alerting"
+          docsLink="https://docs.nhost.io/platform/metrics#alerting"
+          showSwitch
+          className="hidden"
+        />
+      </Form>
+    </FormProvider>
+  );
+}
