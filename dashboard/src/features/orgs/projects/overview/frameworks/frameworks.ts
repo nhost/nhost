787df00b0e689697ea0861ffd5fc221b0546bcb0
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2024-10-25 13:53:12 +0100

    fix docs links in overview page and remove redwoodjs

--- dashboard/src/features/orgs/projects/overview/frameworks/frameworks.ts
+++ dashboard/src/features/orgs/projects/overview/frameworks/frameworks.ts
@@ -5,7 +5,7 @@ const frameworks: CardProps[] = [
     title: 'React',
     description: 'Guide to build a simple React app',
     icon: '/assets/frameworks/react.svg',
-    link: 'https://docs.nhost.io/platform/quickstarts/react',
+    link: 'https://docs.nhost.io/guides/quickstarts/react',
     iconIsComponent: false,
   },
   {
@@ -14,21 +14,14 @@ const frameworks: CardProps[] = [
     icon: '/assets/frameworks/nextjs.svg',
     lightIcon: '/assets/frameworks/light/nextjs.svg',
     disableIconBackground: true,
-    link: 'https://docs.nhost.io/platform/quickstarts/nextjs',
-    iconIsComponent: false,
-  },
-  {
-    title: 'RedwoodJS',
-    description: 'Quickstart for RedwoodJS on Nhost',
-    icon: '/assets/frameworks/redwood.svg',
-    link: 'https://docs.nhost.io/platform/quickstarts/redwoodjs',
+    link: 'https://docs.nhost.io/guides/quickstarts/nextjs',
     iconIsComponent: false,
   },
   {
     title: 'Vue.js',
     description: 'Learn how to use Vue.js with Nhost',
     icon: '/assets/frameworks/vue.svg',
-    link: 'https://docs.nhost.io/platform/quickstarts/vue',
+    link: 'https://docs.nhost.io/guides/quickstarts/vue',
     iconIsComponent: false,
   },
 ];
