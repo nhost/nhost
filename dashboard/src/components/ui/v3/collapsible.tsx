ec34d626a59dd1e9999fe48cfcccdf3e6e9f7424
David BM <correodelnino@gmail.com>
2024-10-30 17:16:42 -0400

    chore: remove collapsible

--- dashboard/src/components/ui/v3/collapsible.tsx
+++
@@ -1,9 +0,0 @@
-import * as CollapsiblePrimitive from '@radix-ui/react-collapsible';
-
-const Collapsible = CollapsiblePrimitive.Root;
-
-const { CollapsibleTrigger } = CollapsiblePrimitive;
-
-const { CollapsibleContent } = CollapsiblePrimitive;
-
-export { Collapsible, CollapsibleTrigger, CollapsibleContent };
