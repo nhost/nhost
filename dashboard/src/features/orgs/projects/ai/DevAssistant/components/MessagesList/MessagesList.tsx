70907b3e15f3288537315ffc1bb3a080636213f3
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2024-10-31 16:57:05 +0100

    fix: update dev assistant under orgs structure

--- dashboard/src/features/orgs/projects/ai/DevAssistant/components/MessagesList/MessagesList.tsx
+++ dashboard/src/features/orgs/projects/ai/DevAssistant/components/MessagesList/MessagesList.tsx
@@ -1,8 +1,8 @@
 import { Box } from '@/components/ui/v2/Box';
-import { LoadingAssistantMessage } from '@/features/ai/DevAssistant/components/LoadingAssistantMessage';
 import { MessageBox } from '@/features/ai/DevAssistant/components/MessageBox';
-import { projectMessagesState } from '@/features/ai/DevAssistant/state';
-import { useCurrentWorkspaceAndProject } from '@/features/projects/common/hooks/useCurrentWorkspaceAndProject';
+import { LoadingAssistantMessage } from '@/features/orgs/projects/ai/DevAssistant/components/LoadingAssistantMessage';
+import { projectMessagesState } from '@/features/orgs/projects/ai/DevAssistant/state';
+import { useProject } from '@/features/orgs/projects/hooks/useProject';
 import { memo, useEffect, useRef } from 'react';
 import { useRecoilValue } from 'recoil';
 
@@ -11,9 +11,9 @@ interface MessagesListProps {
 }
 
 function MessagesList({ loading }: MessagesListProps) {
+  const { project } = useProject();
   const bottomElement = useRef(null);
-  const { currentProject } = useCurrentWorkspaceAndProject();
-  const messages = useRecoilValue(projectMessagesState(currentProject.id));
+  const messages = useRecoilValue(projectMessagesState(project?.id));
 
   const scrollToBottom = () =>
     bottomElement?.current?.scrollIntoView({ behavior: 'instant' });
