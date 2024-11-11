753f04bf842488ec4699a37bd29062cff6407a3a
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2024-11-11 12:24:23 +0100

    adapt example to the free version of @react-native-google-signin/google-signin

--- examples/react_native/src/components/SignInWithGoogleButton.tsx
+++ examples/react_native/src/components/SignInWithGoogleButton.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
 import Button from '@components/Button';
-import {StyleSheet, Text, View} from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {useSignInIdToken} from '@nhost/react';
 import {
   GoogleSignin,
   isErrorWithCode,
-  isNoSavedCredentialFoundResponse,
   isSuccessResponse,
   statusCodes,
 } from '@react-native-google-signin/google-signin';
-import {useSignInIdToken} from '@nhost/react';
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 GoogleSignin.configure({
   webClientId: process.env.GOOGLE_CLIENT_ID,
@@ -22,30 +21,23 @@ export default function SignInWithGoogleButton() {
     try {
       await GoogleSignin.hasPlayServices();
       const response = await GoogleSignin.signIn();
-
       if (isSuccessResponse(response)) {
-        await signInIdToken('google', response?.data?.idToken as string);
-      } else if (isNoSavedCredentialFoundResponse(response)) {
-        // Android and Apple only.
-        // No saved credential found (user has not signed in yet, or they revoked access)
-        // call `createAccount()`
+        const idToken = response?.data?.idToken as string;
+        signInIdToken('google', idToken);
+      } else {
+        // sign in was cancelled by user
       }
     } catch (error) {
-      console.error(error);
       if (isErrorWithCode(error)) {
         switch (error.code) {
-          case statusCodes.ONE_TAP_START_FAILED:
-            // Android-only, you probably have hit rate limiting.
-            // You can still call `presentExplicitSignIn` in this case.
+          case statusCodes.IN_PROGRESS:
+            // operation (eg. sign in) already in progress
             break;
           case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
-            // Android: play services not available or outdated.
-            // Get more details from `error.userInfo`.
-            // Web: when calling an unimplemented api (requestAuthorization)
-            // or when the Google Client Library is not loaded yet.
+            // Android only, play services not available or outdated
             break;
           default:
-          // something else happened
+          // some other error happened
         }
       } else {
         // an error that's not related to google sign in occurred
