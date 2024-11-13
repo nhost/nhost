74011afcc9b012add190cbdb791b96fab817a9ea
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2024-11-13 20:53:56 +0100

    mock toast lib and fix test

--- examples/react_native/setup-jest.js
+++ examples/react_native/setup-jest.js
@@ -6,6 +6,11 @@ import '@react-native-google-signin/google-signin';
 
 jest.useFakeTimers();
 
+jest.mock('expo-modules-core', () => ({
+  EventEmitter: jest.fn(),
+  requireNativeModule: jest.fn().mockReturnValue({}), // Mock this function
+}));
+
 Object.assign(global, {
   WebSocket,
   BroadcastChannel: MockBroadcastChannel,
