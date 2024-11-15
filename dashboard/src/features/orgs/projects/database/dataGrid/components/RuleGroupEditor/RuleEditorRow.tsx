5dd256f546283e36c010384a53df716e76f1c5dd
David BM <correodelnino@gmail.com>
2024-11-14 21:30:24 -0500

    feat: add multi select for _in hasura permissions

--- dashboard/src/features/orgs/projects/database/dataGrid/components/RuleGroupEditor/RuleEditorRow.tsx
+++ dashboard/src/features/orgs/projects/database/dataGrid/components/RuleGroupEditor/RuleEditorRow.tsx
@@ -1,6 +1,10 @@
-import { ControlledSelect } from '@/components/form/ControlledSelect';
-import { Option } from '@/components/ui/v2/Option';
-import { Text } from '@/components/ui/v2/Text';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/v3/select';
 import { ColumnAutocomplete } from '@/features/orgs/projects/database/dataGrid/components/ColumnAutocomplete';
 import type { HasuraOperator } from '@/features/orgs/projects/database/dataGrid/types/dataBrowser';
 import type { DetailedHTMLProps, HTMLProps } from 'react';
@@ -74,17 +78,13 @@ function renderOption({
   helperText,
 }: (typeof commonOperators)[number]) {
   return (
-    <Option key={value} value={value} className="grid grid-flow-col gap-2">
-      <Text component="span" className="inline-block w-16">
-        {label || value}
-      </Text>
+    <SelectItem key={value} value={value} className="grid grid-flow-col gap-2">
+      <span className="inline-block w-16">{label || value}</span>
 
       {helperText && (
-        <Text component="span" color="disabled">
-          {helperText}
-        </Text>
+        <span className="text-muted-foreground">{helperText}</span>
       )}
-    </Option>
+    </SelectItem>
   );
 }
 
@@ -97,12 +97,14 @@ export default function RuleEditorRow({
   ...props
 }: RuleEditorRowProps) {
   const { schema, table, disabled } = useRuleGroupEditor();
-  const { control, setValue, getFieldState } = useFormContext();
+  const { control, setValue, getFieldState, watch } = useFormContext();
   const rowName = `${name}.rules.${index}`;
 
   const columnState = getFieldState(`${rowName}.column`);
   const operatorState = getFieldState(`${rowName}.operator`);
   const valueState = getFieldState(`${rowName}.value`);
+
+  const operator = watch(`${rowName}.operator`);
 
   const [selectedTablePath, setSelectedTablePath] = useState<string>('');
   const [selectedColumnType, setSelectedColumnType] = useState<string>('');
@@ -125,10 +127,18 @@ export default function RuleEditorRow({
       : []),
   ];
 
+  const handleSelect = (value: string) => {
+    if (['_in', '_nin'].includes(value)) {
+      setValue(`${rowName}.value`, [], { shouldDirty: true });
+    }
+
+    setValue(`${rowName}.operator`, value, { shouldDirty: true });
+  };
+
   return (
     <div
       className={twMerge(
-        'grid grid-flow-row space-y-1 lg:max-h-10 lg:grid-cols-[320px_140px_minmax(100px,_1fr)_40px] lg:space-y-0',
+        'grid grid-flow-row space-y-1 lg:grid-cols-[320px_140px_minmax(100px,_1fr)_40px] lg:space-y-0',
         className,
       )}
       {...props}
@@ -138,7 +148,7 @@ export default function RuleEditorRow({
         disabled={disabled}
         schema={schema}
         table={table}
-        rootClassName="h-10"
+        // rootClassName="h-10"
         slotProps={{
           input: {
             className: 'lg:!rounded-r-none',
@@ -180,46 +190,16 @@ export default function RuleEditorRow({
           });
         }}
       />
-
-      <ControlledSelect
+      <Select
         disabled={disabled}
-        name={`${rowName}.operator`}
-        className="h-10"
-        slotProps={{
-          root: {
-            className: 'lg:!rounded-none',
-            sx: !disabled
-              ? {
-                  backgroundColor: (theme) =>
-                    theme.palette.mode === 'dark'
-                      ? `${theme.palette.grey[300]} !important`
-                      : `${theme.palette.common.white} !important`,
-                }
-              : {},
-          },
-          listbox: { className: 'max-h-[300px]' },
-          popper: { disablePortal: false, className: 'z-[10000]' },
-        }}
-        fullWidth
-        error={Boolean(operatorState?.error?.message)}
-        onChange={(_event, value: HasuraOperator) => {
-          if (!['_in', '_nin'].includes(value)) {
-            return;
-          }
-
-          setValue(`${rowName}.value`, [], { shouldDirty: true });
-        }}
-        renderValue={(option) => {
-          if (!option?.value) {
-            return <span />;
-          }
-
-          return <span>{option.value}</span>;
-        }}
+        onValueChange={handleSelect}
+        defaultValue={operator}
       >
-        {availableOperators.map(renderOption)}
-      </ControlledSelect>
-
+        <SelectTrigger className="rounded-l-none [&>*>*:nth-child(2)]:hidden [&>*>*]:block [&>*>*]:w-auto">
+          <SelectValue placeholder="Select a verified email to display" />
+        </SelectTrigger>
+        <SelectContent>{availableOperators.map(renderOption)}</SelectContent>
+      </Select>
       <RuleValueInput
         selectedTablePath={selectedTablePath}
         name={rowName}
