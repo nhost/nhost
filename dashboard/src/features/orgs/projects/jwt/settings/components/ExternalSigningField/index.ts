385a18bacde3b5927706d27f539ba91692c8bc0a
David BM <correodelnino@gmail.com>
2024-11-21 14:14:35 -0500

    chore: refactor, extract signing field component

---
+++ dashboard/src/features/orgs/projects/jwt/settings/components/ExternalSigningField/index.ts
@@ -0,0 +1,1 @@
+export { default as ExternalSigningField } from './ExternalSigningField';
--- dashboard/src/features/orgs/projects/jwt/settings/components/ExternalSigningFormSection/ExternalSigningFormSection.tsx
+++ dashboard/src/features/orgs/projects/jwt/settings/components/ExternalSigningFormSection/ExternalSigningFormSection.tsx
@@ -1,19 +1,12 @@
-import { Input } from '@/components/ui/v2/Input';
 import { Label } from '@/components/ui/v3/label';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/v3/radio-group';
-import { useFormContext } from 'react-hook-form';
 
 import { Alert } from '@/components/ui/v2/Alert';
 import { Box } from '@/components/ui/v2/Box';
-import { Option } from '@/components/ui/v2/Option';
-import { Select } from '@/components/ui/v2/Select';
 import { Text } from '@/components/ui/v2/Text';
-import { ASYMMETRIC_ALGORITHMS } from '@/features/orgs/projects/jwt/settings/utils/constants';
 
-import {
-  type ExternalSigningType,
-  type JWTSettingsFormValues,
-} from '@/features/orgs/projects/jwt/settings/types';
+import { ExternalSigningField } from '@/features/orgs/projects/jwt/settings/components/ExternalSigningField';
+import type { ExternalSigningType } from '@/features/orgs/projects/jwt/settings/types';
 
 interface ExternalSigningFormSectionProps {
   externalSigningType: ExternalSigningType;
@@ -24,15 +17,6 @@ export default function ExternalSigningFormSection({
   externalSigningType,
   handleExternalSigningTypeChange,
 }: ExternalSigningFormSectionProps) {
-  const {
-    register,
-    formState: { errors },
-    setValue,
-    watch,
-  } = useFormContext<JWTSettingsFormValues>();
-
-  const type = watch('type');
-
   return (
     <div className="flex flex-col gap-6">
       <Alert severity="warning">
@@ -59,63 +43,8 @@ export default function ExternalSigningFormSection({
             </div>
           </RadioGroup>
         </div>
-        {externalSigningType === 'jwk-endpoint' && (
-          <Input
-            {...register('jwkUrl')}
-            name="jwkUrl"
-            id="jwkUrl"
-            placeholder="https://acme.com/jwks.json"
-            className="col-span-5 lg:col-span-4"
-            label="JWK URL"
-            fullWidth
-            hideEmptyHelperText
-            error={!!errors?.jwkUrl}
-            helperText={errors?.jwkUrl?.message}
-          />
-        )}
-        {externalSigningType === 'public-key' && (
-          <>
-            <Select
-              id="type"
-              className="col-span-5 lg:col-span-2"
-              placeholder="RS256"
-              hideEmptyHelperText
-              variant="normal"
-              defaultValue={ASYMMETRIC_ALGORITHMS[0]}
-              error={!!errors.type}
-              helperText={errors?.type?.message}
-              label="Hashing algorithm"
-              value={type}
-              onChange={(_event, value) =>
-                setValue('type', value as string, { shouldDirty: true })
-              }
-            >
-              {ASYMMETRIC_ALGORITHMS.map((algorithm) => (
-                <Option key={algorithm} value={algorithm}>
-                  {algorithm}
-                </Option>
-              ))}
-            </Select>
-
-            <Input
-              {...register('key')}
-              name="key"
-              id="key"
-              placeholder="-----BEGIN PUBLIC KEY-----"
-              className="col-span-5 lg:col-span-4"
-              label="Public Key"
-              fullWidth
-              multiline
-              hideEmptyHelperText
-              error={!!errors?.key}
-              helperText={errors?.key?.message}
-              inputProps={{
-                className: 'resize-y min-h-[130px]',
-              }}
-            />
-          </>
-        )}
       </Box>
+      <ExternalSigningField externalSigningType={externalSigningType} />
     </div>
   );
 }
