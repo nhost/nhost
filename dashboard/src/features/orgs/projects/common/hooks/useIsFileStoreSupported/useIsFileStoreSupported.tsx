3f450e9b06928b7c4f4095a8f38c55373083a6eb
Nuno Pato <nunopato@gmail.com>
2024-11-29 14:34:18 +0800

    Update dashboard/src/features/orgs/projects/common/hooks/useIsFileStoreSupported/useIsFileStoreSupported.tsx
    Co-authored-by: Hassan Ben Jobrane <hsanbenjobrane@gmail.com>

--- dashboard/src/features/orgs/projects/common/hooks/useIsFileStoreSupported/useIsFileStoreSupported.tsx
+++ dashboard/src/features/orgs/projects/common/hooks/useIsFileStoreSupported/useIsFileStoreSupported.tsx
@@ -1,7 +1,7 @@
 
 import { useProject } from '@/features/orgs/projects/hooks/useProject';
-import { useIsPlatform } from '@/features/projects/common/hooks/useIsPlatform';
-import { useLocalMimirClient } from '@/hooks/useLocalMimirClient';
+import { useIsPlatform } from '@/features/orgs/projects/common/hooks/useIsPlatform';
+import { useLocalMimirClient } from '@/features/orgs/projects/hooks/useLocalMimirClient';
 import { useGetConfiguredVersionsQuery } from '@/utils/__generated__/graphql';
 import { useEffect, useState } from 'react';
 
