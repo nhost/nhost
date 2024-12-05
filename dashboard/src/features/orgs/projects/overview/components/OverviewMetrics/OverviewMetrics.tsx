86ecf27b23334e6f4c7612f93d1d3b1e43cc7d86
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2024-12-05 16:14:15 +0100

    feat(dashboard): add support for additional metrics in overview (#3052)
    ### **User description**
    resolves https://github.com/nhost/nhost/issues/3017
    
    
    ___
    
    ### **PR Type**
    Enhancement, Other
    
    
    ___
    
    ### **Description**
    - Introduced new metrics in the dashboard overview, including monthly
    and daily active users, total users, and storage.
    - Implemented a new GraphQL query `GetUserProjectMetrics` to fetch
    user-related metrics.
    - Updated the `OverviewMetrics` component to display the newly added
    metrics.
    - Enhanced the GraphQL schema with new fields and queries to support the
    additional metrics.
    - Added a changeset to document the new feature in the dashboard.
    
    
    
    ___
    
    
    
    ### **Changes walkthrough** 📝
    <table><thead><tr><th></th><th align="left">Relevant
    files</th></tr></thead><tbody><tr><td><strong>Enhancement</strong></td><td><table>
    <tr>
      <td>
        <details>
    <summary><strong>OverviewMetrics.tsx</strong><dd><code>Add support for
    additional user and storage metrics in overview</code></dd></summary>
    <hr>
    
    
    dashboard/src/features/orgs/projects/overview/components/OverviewMetrics/OverviewMetrics.tsx
    
    <li>Added new metrics for monthly and daily active users, total users,
    and <br>storage.<br> <li> Integrated
    <code>useGetUserProjectMetricsQuery</code> for fetching user-related
    <br>metrics.<br> <li> Updated metrics card elements to display new
    metrics.<br> <li> Removed redundant data checks and improved error
    handling.<br>
    
    
    </details>
    
    
      </td>
    <td><a
    href="https://github.com/nhost/nhost/pull/3052/files#diff-de881837e53f594075bb725282b02e92c2cb281f8f6a438fdbaa2e3254907fd1">+90/-17</a>&nbsp;
    </td>
    
    </tr>
    
    <tr>
      <td>
        <details>
    <summary><strong>graphql.ts</strong><dd><code>Update GraphQL types and
    queries for user metrics</code>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; </dd></summary>
    <hr>
    
    dashboard/src/utils/__generated__/graphql.ts
    
    <li>Added new GraphQL query types for user project metrics.<br> <li>
    Introduced <code>automaticDeploys</code> field in the <code>Apps</code>
    type.<br> <li> Updated generated types to include new fields and
    queries.<br>
    
    
    </details>
    
    
      </td>
    <td><a
    href="https://github.com/nhost/nhost/pull/3052/files#diff-fbd5db84b560b1c91675004448c6c7fa0dcbfb28b9eb05d53b03e6cb7b83ebac">+87/-0</a>&nbsp;
    &nbsp; </td>
    
    </tr>
    
    <tr>
      <td>
        <details>
    <summary><strong>getUserProjectMetrics.gql</strong><dd><code>Add GraphQL
    query for user project metrics</code>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    </dd></summary>
    <hr>
    
    dashboard/src/gql/organizations/getUserProjectMetrics.gql
    
    - Created new GraphQL query for fetching user project metrics.
    
    
    
    </details>
    
    
      </td>
    <td><a
    href="https://github.com/nhost/nhost/pull/3052/files#diff-902523302d8b32d218ef665a252dec5b9cbcf5fbab0cbb32845c441b01eaa28e">+28/-0</a>&nbsp;
    &nbsp; </td>
    
    </tr>
    </table></td></tr><tr><td><strong>Documentation</strong></td><td><table>
    <tr>
      <td>
        <details>
    <summary><strong>khaki-pets-argue.md</strong><dd><code>Add changeset for
    dashboard metrics feature</code>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    </dd></summary>
    <hr>
    
    .changeset/khaki-pets-argue.md
    
    - Added changeset for new feature in dashboard.
    
    
    
    </details>
    
    
      </td>
    <td><a
    href="https://github.com/nhost/nhost/pull/3052/files#diff-51eb36ca77dae90a865c185e0d528fcaa4b836f3a0469550513f68003bf11c9a">+5/-0</a>&nbsp;
    &nbsp; &nbsp; </td>
    
    </tr>
    </table></td></tr></tr></tbody></table>
    
    ___
    
    > 💡 **PR-Agent usage**: Comment `/help "your question"` on any pull
    request to receive relevant information
    
    ---------
    
    Co-authored-by: David Barroso <dbarrosop@dravetech.com>

--- dashboard/src/features/orgs/projects/overview/components/OverviewMetrics/OverviewMetrics.tsx
+++ dashboard/src/features/orgs/projects/overview/components/OverviewMetrics/OverviewMetrics.tsx
@@ -1,54 +1,132 @@
 import { Text } from '@/components/ui/v2/Text';
+import { useRemoteApplicationGQLClient } from '@/features/orgs/hooks/useRemoteApplicationGQLClient';
 import { useProject } from '@/features/orgs/projects/hooks/useProject';
 import type { MetricsCardProps } from '@/features/orgs/projects/overview/components/MetricsCard';
 import { MetricsCard } from '@/features/orgs/projects/overview/components/MetricsCard';
 import { prettifyNumber } from '@/utils/prettifyNumber';
-import { prettifySize } from '@/utils/prettifySize';
-import { useGetProjectMetricsQuery } from '@/utils/__generated__/graphql';
+import {
+  useGetProjectMetricsQuery,
+  useGetProjectRequestsMetricQuery,
+  useGetUserProjectMetricsQuery,
+} from '@/utils/__generated__/graphql';
 import { twMerge } from 'tailwind-merge';
+
+import { prettifySize } from '@/utils/prettifySize';
+import { formatISO, startOfDay, startOfMonth, subMinutes } from 'date-fns';
 
 const now = new Date();
 
 export default function OverviewMetrics() {
   const { project } = useProject();
-  const { data, loading, error } = useGetProjectMetricsQuery({
+  const remoteProjectGQLClient = useRemoteApplicationGQLClient();
+
+  const {
+    data: {
+      allUsers: { aggregate: { count: allUsers = 0 } = {} } = {},
+      dailyActiveUsers: {
+        aggregate: { count: dailyActiveUsers = 0 } = {},
+      } = {},
+      monthlyActiveUsers: {
+        aggregate: { count: monthlyActiveUsers = 0 } = {},
+      } = {},
+      filesAggregate: {
+        aggregate: { sum: { size: totalStorage = 0 } = {} } = {},
+      } = {},
+    } = {},
+  } = useGetUserProjectMetricsQuery({
+    client: remoteProjectGQLClient,
     variables: {
-      appId: project?.id,
+      startOfMonth: startOfMonth(new Date()),
+      today: startOfDay(new Date()),
+    },
+    skip: !project,
+  });
+
+  const {
+    data: {
+      totalRequests: { value: totalRequestsInLastFiveMinutes = 0 } = {},
+    } = {},
+  } = useGetProjectRequestsMetricQuery({
+    variables: {
+      appId: project.id,
+      from: formatISO(subMinutes(new Date(), 6)), // 6 mns earlier
+      to: formatISO(subMinutes(new Date(), 1)), // 1 mn earlier
+    },
+    skip: !project,
+    pollInterval: 1000 * 60 * 5, // Poll every 5 minutes
+  });
+
+  const {
+    data: {
+      functionsDuration: { value: functionsDuration = 0 } = {},
+      totalRequests: { value: totalRequests = 0 } = {},
+      postgresVolumeUsage: { value: postgresVolumeUsage = 0 } = {},
+      egressVolume: { value: egressVolume = 0 } = {},
+    } = {},
+    loading,
+    error,
+  } = useGetProjectMetricsQuery({
+    variables: {
+      appId: project.id,
       subdomain: project?.subdomain,
       from: new Date(now.getFullYear(), now.getMonth(), 1),
     },
-    skip: !project?.id,
+    skip: !project,
   });
 
   const cardElements: MetricsCardProps[] = [
     {
-      label: 'CPU Usage Seconds',
-      tooltip: 'Total time the service has used the CPUs',
-      value: prettifyNumber(data?.cpuSecondsUsage?.value || 0),
+      label: 'Daily Active Users',
+      tooltip: 'Unique users active today',
+      value: prettifyNumber(dailyActiveUsers),
+    },
+    {
+      label: 'Monthly Active Users',
+      tooltip: 'Unique users active this month',
+      value: prettifyNumber(monthlyActiveUsers),
+    },
+    {
+      label: 'All Users',
+      tooltip: 'Total registered users',
+      value: prettifyNumber(allUsers),
+    },
+    {
+      label: 'RPS',
+      tooltip: 'Requests Per Second (RPS) measured in the last 5 minutes',
+      value: prettifyNumber(totalRequestsInLastFiveMinutes / 300, {
+        numberOfDecimals: 2,
+      }),
     },
     {
       label: 'Total Requests',
-      tooltip:
-        'Total amount of requests your services have received excluding functions',
-      value: prettifyNumber(data?.totalRequests?.value || 0, {
-        numberOfDecimals: data?.totalRequests?.value > 1000 ? 2 : 0,
+      tooltip: 'Total service requests this month so far (excluding functions)',
+      value: prettifyNumber(totalRequests || 0, {
+        numberOfDecimals: totalRequests > 1000 ? 2 : 0,
       }),
     },
     {
-      label: 'Function Invocations',
-      tooltip: 'Number of times your functions have been called',
-      value: prettifyNumber(data?.functionInvocations?.value || 0, {
-        numberOfDecimals: 0,
-      }),
+      label: 'Egress',
+      tooltip: 'Total outgoing data transfer this month so far',
+      value: prettifySize(egressVolume),
     },
     {
-      label: 'Logs',
-      tooltip: 'Amount of logs stored',
-      value: prettifySize(data?.logsVolume?.value || 0),
+      label: 'Functions Duration',
+      tooltip: 'Total Functions execution this month so far',
+      value: prettifyNumber(functionsDuration),
+    },
+    {
+      label: 'Storage',
+      tooltip: 'Total size of stored files in the storage service',
+      value: prettifySize(totalStorage || 0),
+    },
+    {
+      label: 'Postgres Volume Usage',
+      tooltip: 'Used storage in the Postgres database',
+      value: prettifySize(postgresVolumeUsage),
     },
   ];
 
-  if (!data && error) {
+  if (error) {
     throw error;
   }
 
