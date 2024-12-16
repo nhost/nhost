dfdfed32fb5035e008145e55ca18f92ef0eabfed
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2024-12-16 02:25:13 +0100

    asd

--- examples/quickstarts/sveltekit/vite.config.js
+++ examples/quickstarts/sveltekit/vite.config.js
@@ -1,9 +1,22 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
   plugins: [sveltekit()],
+
+  optimizeDeps: {
+    include: ['@nhost/nhost-js']
+  },
+  build: {
+    commonjsOptions: {
+      include: [path.resolve(__dirname, '../../../packages/nhost-js'), /node_modules/] // Resolve monorepo package
+    }
+  },
   server: {
     port: 3000
+  },
+  resolve: {
+    preserveSymlinks: true // Optional, for monorepo symlink resolution
   }
 })
