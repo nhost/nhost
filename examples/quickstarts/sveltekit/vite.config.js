8b174d1e4887f6b8d7a68c1ddaadadfb6980e462
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2024-12-16 16:24:50 +0100

    asd

--- examples/quickstarts/sveltekit/vite.config.js
+++ examples/quickstarts/sveltekit/vite.config.js
@@ -1,18 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
-import path from 'path'
 
 export default defineConfig({
   plugins: [sveltekit()],
   optimizeDeps: {
-    include: ['@nhost/nhost-js']
+    include: ['@nhost/nhos-js']
   },
   build: {
     commonjsOptions: {
-      include: [path.resolve(__dirname, '../../../packages/nhost-js'), /node_modules/]
+      include: [/@nhost\/nhos-js/, /node_modules/]
     }
   },
   server: {
     port: 3000
+  },
+  resolve: {
+    preserveSymlinks: false
   }
 })
