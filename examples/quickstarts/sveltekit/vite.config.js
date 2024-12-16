6696172bcb0978821a9eae733671a0b15409c76b
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2024-12-16 19:48:12 +0100

    chore: upgrade to pnpm 9.15.0 (#3081)
    ### **PR Type**
    enhancement, configuration changes
    
    
    ___
    
    ### **Description**
    - Upgraded `pnpm` to version 9.15.0 across the project, including GitHub
    Actions and root package configuration.
    - Updated Node.js version to 20 in GitHub Actions and root package
    configuration.
    - Moved `@nhost/nhost-js` from `devDependencies` to `dependencies` in
    the SvelteKit example.
    - Removed `pnpm` from `devDependencies` in the Vue quickstart example.
    - Added `compilerOptions` with an empty `types` array in the
    `tsconfig.json` for `hasura-auth-js`.
    
    
    
    ___
    
    
    
    ### **Changes walkthrough** 📝
    <table><thead><tr><th></th><th align="left">Relevant
    files</th></tr></thead><tbody><tr><td><strong>Configuration
    changes</strong></td><td><table>
    <tr>
      <td>
        <details>
    <summary><strong>action.yaml</strong><dd><code>Upgrade pnpm and Node.js
    versions in GitHub Actions</code>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; </dd></summary>
    <hr>
    
    .github/actions/install-dependencies/action.yaml
    
    <li>Upgraded <code>pnpm</code> version from 8.10.5 to 9.15.0.<br> <li>
    Updated Node.js version from 18 to 20.<br>
    
    
    </details>
    
    
      </td>
    <td><a
    href="https://github.com/nhost/nhost/pull/3081/files#diff-342d59190b4737ee45e2062eb625ada477bcea5b4a843b25900ad55d7982f200">+3/-3</a>&nbsp;
    &nbsp; &nbsp; </td>
    
    </tr>
    
    <tr>
      <td>
        <details>
    <summary><strong>package.json</strong><dd><code>Update pnpm and Node.js
    engine in root package.json</code>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; </dd></summary>
    <hr>
    
    package.json
    
    <li>Updated <code>pnpm</code> version in <code>packageManager</code>
    field to 9.15.0.<br> <li> Changed Node.js engine requirement to version
    20 or higher.<br>
    
    
    </details>
    
    
      </td>
    <td><a
    href="https://github.com/nhost/nhost/pull/3081/files#diff-7ae45ad102eab3b6d7e7896acd08c427a9b25b346470d7bc6507b6481575d519">+2/-3</a>&nbsp;
    &nbsp; &nbsp; </td>
    
    </tr>
    </table></td></tr><tr><td><strong>Enhancement</strong></td><td><table>
    <tr>
      <td>
        <details>
    <summary><strong>package.json</strong><dd><code>Adjust dependencies in
    SvelteKit example</code>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    </dd></summary>
    <hr>
    
    examples/quickstarts/sveltekit/package.json
    
    - Moved `@nhost/nhost-js` from `devDependencies` to `dependencies`.
    
    
    
    </details>
    
    
      </td>
    <td><a
    href="https://github.com/nhost/nhost/pull/3081/files#diff-6288951fff74ec246c9cc023b7b7e3e9aad31423891bc4ea25b5d84a5f5b061f">+1/-1</a>&nbsp;
    &nbsp; &nbsp; </td>
    
    </tr>
    
    <tr>
      <td>
        <details>
    <summary><strong>package.json</strong><dd><code>Remove pnpm from Vue
    quickstart devDependencies</code>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </dd></summary>
    <hr>
    
    examples/vue-quickstart/package.json
    
    - Removed `pnpm` from `devDependencies`.
    
    
    
    </details>
    
    
      </td>
    <td><a
    href="https://github.com/nhost/nhost/pull/3081/files#diff-85166d1137e29a5275f991e1e94a0c9d5b83ac7504463ba76f9187b2b750c895">+0/-1</a>&nbsp;
    &nbsp; &nbsp; </td>
    
    </tr>
    
    <tr>
      <td>
        <details>
    <summary><strong>tsconfig.json</strong><dd><code>Add compiler options to
    TypeScript configuration</code>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; </dd></summary>
    <hr>
    
    packages/hasura-auth-js/tsconfig.json
    
    - Added `compilerOptions` with empty `types` array.
    
    
    
    </details>
    
    
      </td>
    <td><a
    href="https://github.com/nhost/nhost/pull/3081/files#diff-f4ce71cf32b93f403010fe5002bdc1081000207e40a6fea24b82346d6ea0dac7">+4/-1</a>&nbsp;
    &nbsp; &nbsp; </td>
    
    </tr>
    </table></td></tr></tr></tbody></table>
    
    ___
    
    > 💡 **PR-Agent usage**: Comment `/help "your question"` on any pull
    request to receive relevant information

--- examples/quickstarts/sveltekit/vite.config.js
+++ examples/quickstarts/sveltekit/vite.config.js
@@ -3,7 +3,18 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [sveltekit()],
+  optimizeDeps: {
+    include: ['@nhost/nhos-js']
+  },
+  build: {
+    commonjsOptions: {
+      include: [/@nhost\/nhos-js/, /node_modules/]
+    }
+  },
   server: {
     port: 3000
+  },
+  resolve: {
+    preserveSymlinks: false
   }
 })
