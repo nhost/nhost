234f48f390b7e4a23e0717d6885800552c60e5f2
David B.M. <correodelnino@gmail.com>
2024-12-31 18:31:25 +0100

    chore: add clsx to vitest

--- dashboard/vitest.config.ts
+++ dashboard/vitest.config.ts
@@ -11,5 +11,8 @@ export default defineConfig({
     globals: true,
     setupFiles: 'src/setupTests.ts',
     include: ['src/**/*.(spec|test).{js,jsx,ts,tsx}'],
+    deps: {
+      inline: ['clsx'],
+    },
   },
 });
