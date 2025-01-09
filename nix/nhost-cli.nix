b2755045c9eddc09dbd3066f15c52b2d2dac5e18
David Barroso <dbarrosop@dravetech.com>
2025-01-09 15:36:17 +0100

    chore: added overlay for nhost cli (#3126)
    ### **PR Type**
    Enhancement
    
    
    ___
    
    ### **Description**
    - Add Nhost CLI package to Nix configuration
    
    - Define Nhost CLI version and platform-specific distributions
    
    - Implement Nix derivation for Nhost CLI installation
    
    - Update overlay to include Nhost CLI package
    
    
    ___
    
    
    
    ### **Changes walkthrough** 📝
    <table><thead><tr><th></th><th align="left">Relevant
    files</th></tr></thead><tbody><tr><td><strong>Enhancement</strong></td><td><table>
    <tr>
      <td>
        <details>
    <summary><strong>nhost-cli.nix</strong><dd><code>Implement Nix package
    for Nhost CLI</code>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </dd></summary>
    <hr>
    
    nix/nhost-cli.nix
    
    <li>Define Nhost CLI version and platform-specific distributions<br>
    <li> Implement Nix derivation for Nhost CLI installation<br> <li> Set up
    build and installation process<br> <li> Define package metadata and
    maintainer information
    
    
    </details>
    
    
      </td>
    <td><a
    href="https://github.com/nhost/nhost/pull/3126/files#diff-9ecb33bccf0f24a938368b6152474086a6520c7f62d1b366fe76bdcb6cc6cc4a">+63/-0</a>&nbsp;
    &nbsp; </td>
    
    </tr>
    </table></td></tr><tr><td><strong>Configuration
    changes</strong></td><td><table>
    <tr>
      <td>
        <details>
    <summary><strong>overlay.nix</strong><dd><code>Update overlay to include
    Nhost CLI package</code>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </dd></summary>
    <hr>
    
    nix/overlay.nix
    
    - Add Nhost CLI package to the Nix overlay
    
    
    </details>
    
    
      </td>
    <td><a
    href="https://github.com/nhost/nhost/pull/3126/files#diff-0f31c68216d617b465827b69d5b5cd4c2c0c4489008cd14ee0e88b9887d15295">+1/-0</a>&nbsp;
    &nbsp; &nbsp; </td>
    
    </tr>
    </table></td></tr></tr></tbody></table>
    
    ___
    
    > 💡 **PR-Agent usage**: Comment `/help "your question"` on any pull
    request to receive relevant information

---
+++ nix/nhost-cli.nix
@@ -0,0 +1,63 @@
+{ final }:
+let
+  version = "v1.28.3";
+  dist = {
+    aarch64-darwin = {
+      url = "https://github.com/nhost/cli/releases/download/${version}/cli-${version}-darwin-arm64.tar.gz";
+      sha256 = "042qkyv94x5iinqphqrh01gvlr5cy26855g6x03mr8zw96rnz1hg";
+    };
+    x86_64-darwin = {
+      url = "https://github.com/nhost/cli/releases/download/${version}/cli-${version}-darwin-amd64.tar.gz";
+      sha256 = "1p8ji6l1a2nbf7na9agzfpklvfyh8gvnhfx0ib1w8gn69y337060";
+    };
+    aarch64-linux = {
+      url = "https://github.com/nhost/cli/releases/download/${version}/cli-${version}-linux-arm64.tar.gz";
+      sha256 = "0vr7qp9wr49cgbcw45zl3x26yhl7vmraymqqzxbg3pk8ifkvm505";
+    };
+    x86_64-linux = {
+      url = "https://github.com/nhost/cli/releases/download/${version}/cli-${version}-linux-amd64.tar.gz";
+      sha256 = "17niy09gr9pf8wy1361pnq7p3ic53asrl6x32g151jhary0yy84r";
+    };
+  };
+
+in
+final.stdenvNoCC.mkDerivation {
+  pname = "nhost-cli";
+  inherit version;
+
+  src = final.fetchurl {
+    inherit (dist.${final.stdenvNoCC.hostPlatform.system} or
+      (throw "Unsupported system: ${final.stdenvNoCC.hostPlatform.system}")) url sha256;
+  };
+
+
+  sourceRoot = ".";
+
+  nativeBuildInputs = [
+    final.unzip
+    final.makeWrapper
+    final.installShellFiles
+  ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    mv cli $out/bin/nhost
+
+    # installShellCompletion --cmd nhost \
+    #   --bash <($out/bin/nhost completion bash) \
+    #   --fish <($out/bin/nhost completion fish) \
+    #   --zsh <($out/bin/nhost completion zsh)
+
+    runHook postInstall
+  '';
+
+  meta = with final.lib; {
+    description = "Nhost CLI";
+    homepage = "https://nhost.io";
+    license = licenses.mit;
+    maintainers = [ "@nhost" ];
+  };
+
+}
