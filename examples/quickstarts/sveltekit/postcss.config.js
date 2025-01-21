ccbdcf7581f25a7ecfd8dfe476c3fae4a05096ea
David B.M. <correodelnino@gmail.com>
2025-01-21 08:54:30 -0500

    wip: remove sveltekit quickstart

--- examples/quickstarts/sveltekit/postcss.config.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}
-  }
-}
--- examples/quickstarts/sveltekit/src/routes/+page.svelte
+++
@@ -1,6 +0,0 @@
-<svelte:head>
-	<title>Login</title>
-	<meta name="description" content="About this app" />
-</svelte:head>
-
-<h1 class="text-2xl text-center">Hi, login/register to get started</h1>
