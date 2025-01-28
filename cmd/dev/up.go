b9c32bbf482dda0c531586a07b200b427f1abcd4
nhost-build <98952681+nhost-build@users.noreply.github.com>
2025-01-28 07:29:09 -0100

    chore: bump nhost/dashboard to 2.17.0 (#944)
    Co-authored-by: dbarrosop <dbarrosop@users.noreply.github.com>

--- cmd/dev/up.go
+++ cmd/dev/up.go
@@ -110,7 +110,7 @@ func CommandUp() *cli.Command { //nolint:funlen
 			&cli.StringFlag{ //nolint:exhaustruct
 				Name:    flagDashboardVersion,
 				Usage:   "Dashboard version to use",
-				Value:   "nhost/dashboard:2.16.0",
+				Value:   "nhost/dashboard:2.17.0",
 				EnvVars: []string{"NHOST_DASHBOARD_VERSION"},
 			},
 			&cli.StringFlag{ //nolint:exhaustruct
