84b5c2bd79f4f0fa9c9721c82c67bc353586f9cb
David B.M. <correodelnino@gmail.com>
2025-02-19 16:59:19 +0100

    fix: run services add secrets in command field

---
+++ dashboard/src/features/orgs/projects/services/components/ServiceForm/components/CommandFormSection/CommandFormSection.tsx
@@ -0,0 +1,72 @@
+import { Box } from '@/components/ui/v2/Box';
+import { Button } from '@/components/ui/v2/Button';
+import { InfoIcon } from '@/components/ui/v2/icons/InfoIcon';
+import { PlusIcon } from '@/components/ui/v2/icons/PlusIcon';
+import { TrashIcon } from '@/components/ui/v2/icons/TrashIcon';
+import { Input } from '@/components/ui/v2/Input';
+import { Text } from '@/components/ui/v2/Text';
+import { Tooltip } from '@/components/ui/v2/Tooltip';
+import type { ServiceFormValues } from '@/features/orgs/projects/services/components/ServiceForm/ServiceFormTypes';
+import { useFieldArray, useFormContext } from 'react-hook-form';
+
+export default function CommandFormSection() {
+  const {
+    register,
+    formState: { errors },
+    control,
+  } = useFormContext<ServiceFormValues>();
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'command',
+  });
+
+  return (
+    <Box className="space-y-4 rounded border-1 p-4">
+      <Box className="flex flex-row items-center justify-between">
+        <Box className="flex flex-row items-center space-x-2">
+          <Text variant="h4" className="font-semibold">
+            Command
+          </Text>
+          <Tooltip
+            title={
+              <span>
+                Specify the command and its arguments to run the service.
+              </span>
+            }
+          >
+            <InfoIcon aria-label="Info" className="h-4 w-4" color="primary" />
+          </Tooltip>
+        </Box>
+        <Button variant="borderless" onClick={() => append({ argument: '' })}>
+          <PlusIcon className="h-5 w-5" />
+        </Button>
+      </Box>
+
+      <Box className="flex flex-col space-y-4">
+        {fields.map((field, index) => (
+          <Box key={field.id} className="flex w-full items-center space-x-2">
+            <Input
+              {...register(`command.${index}.argument`)}
+              id={`command-${index}`}
+              placeholder={index === 0 ? 'mycmd' : '--myflag'}
+              className="w-full"
+              hideEmptyHelperText
+              error={!!errors.command?.[index]}
+              helperText={errors.command?.[index]?.message}
+              fullWidth
+              autoComplete="off"
+            />
+            <Button
+              variant="borderless"
+              color="error"
+              onClick={() => remove(index)}
+            >
+              <TrashIcon className="h-6 w-4" />
+            </Button>
+          </Box>
+        ))}
+      </Box>
+    </Box>
+  );
+}
