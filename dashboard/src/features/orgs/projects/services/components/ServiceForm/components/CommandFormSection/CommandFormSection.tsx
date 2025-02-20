52ad3bb30153343a0c3685a266fc00eecbeea732
David B.M. <correodelnino@gmail.com>
2025-02-20 16:43:46 +0100

    chore: ui tweaks

--- dashboard/src/features/orgs/projects/services/components/ServiceForm/components/CommandFormSection/CommandFormSection.tsx
+++ dashboard/src/features/orgs/projects/services/components/ServiceForm/components/CommandFormSection/CommandFormSection.tsx
@@ -1,3 +1,4 @@
+import { Alert } from '@/components/ui/v2/Alert';
 import { Box } from '@/components/ui/v2/Box';
 import { Button } from '@/components/ui/v2/Button';
 import { InfoIcon } from '@/components/ui/v2/icons/InfoIcon';
@@ -9,17 +10,49 @@ import { Tooltip } from '@/components/ui/v2/Tooltip';
 import type { ServiceFormValues } from '@/features/orgs/projects/services/components/ServiceForm/ServiceFormTypes';
 import { useFieldArray, useFormContext } from 'react-hook-form';
 
+function CommandTooltip() {
+  return (
+    <Tooltip
+      title={
+        <div className="flex flex-col gap-2">
+          <p>Specify the command and its arguments to run the service.</p>
+          <p>
+            Note that the command and/or its parameters need to be specified on
+            different lines.
+          </p>
+          <p>For instance:</p>
+          <ul className="list-inside list-['-_']">
+            <li>node</li>
+            <li>server.js</li>
+            <li>--port=3000</li>
+          </ul>
+        </div>
+      }
+    >
+      <InfoIcon aria-label="Info" className="h-4 w-4" color="primary" />
+    </Tooltip>
+  );
+}
+
 export default function CommandFormSection() {
   const {
     register,
     formState: { errors },
     control,
+    watch,
   } = useFormContext<ServiceFormValues>();
 
   const { fields, append, remove } = useFieldArray({
     control,
     name: 'command',
   });
+
+  // Watch for spaces
+  const commandValues = watch('command');
+
+  const hasSpaceInCommand = commandValues.some((field) =>
+    field.argument.includes(' '),
+  );
 
   return (
     <Box className="space-y-4 rounded border-1 p-4">
@@ -28,15 +61,7 @@ export default function CommandFormSection() {
           <Text variant="h4" className="font-semibold">
             Command
           </Text>
-          <Tooltip
-            title={
-              <span>
-                Specify the command and its arguments to run the service.
-              </span>
-            }
-          >
-            <InfoIcon aria-label="Info" className="h-4 w-4" color="primary" />
-          </Tooltip>
+          <CommandTooltip />
         </Box>
         <Button variant="borderless" onClick={() => append({ argument: '' })}>
           <PlusIcon className="h-5 w-5" />
@@ -66,6 +91,21 @@ export default function CommandFormSection() {
             </Button>
           </Box>
         ))}
+        {hasSpaceInCommand && (
+          <Alert severity="warning" className="flex flex-col gap-3 text-left">
+            <div className="flex flex-col gap-2 lg:flex-row lg:justify-between">
+              <Text className="flex items-start gap-1 font-semibold">
+                <span>⚠</span> Warning: Space in command
+              </Text>
+            </div>
+            <div>
+              <Text>
+                A space was detected, make sure it is intended, check the
+                tooltip for details
+              </Text>
+            </div>
+          </Alert>
+        )}
       </Box>
     </Box>
   );
