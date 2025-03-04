1252180bb72207eafda3521c7ee6dac95979d3f4
robertkasza <robert@nhost.io>
2025-03-04 11:08:29 +0100

    return isPITREnabled from hook

--- dashboard/src/features/orgs/hooks/useDatabasePITRSettings/useUpdateDatabasePITRConfig.ts
+++ dashboard/src/features/orgs/hooks/useDatabasePITRSettings/useUpdateDatabasePITRConfig.ts
@@ -14,6 +14,7 @@ function useDatabasePITRSettings() {
     isPITREnabled === isPITREnabledData || isNotSwitchTouched;
 
   return {
+    isPITREnabled,
     setIsPITREnabled,
     isSwitchDisabled,
     setIsNotSwitchTouched,
