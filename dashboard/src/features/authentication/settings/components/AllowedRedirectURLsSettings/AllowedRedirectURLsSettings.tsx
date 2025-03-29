92b5feb60c8b4017a70d61bc1c6aa96b9ba5d2de
Sumit Saurabh <sumitsaurabh927@gmail.com>
2025-03-29 21:03:58 +0530

    dashboard: allowed redirect urls

--- dashboard/src/features/authentication/settings/components/AllowedRedirectURLsSettings/AllowedRedirectURLsSettings.tsx
+++ dashboard/src/features/authentication/settings/components/AllowedRedirectURLsSettings/AllowedRedirectURLsSettings.tsx
@@ -134,7 +134,7 @@ export default function AllowedRedirectURLsSettings() {
               loading: formState.isSubmitting,
             },
           }}
-          docsLink="https://docs.nhost.io/guides/auth/overview#allowed-redirect-urls"
+          docsLink="https://docs.nhost.io/products/auth/overview#allowed-redirect-urls"
           className="grid grid-flow-row px-4 lg:grid-cols-5"
         >
           <Input
