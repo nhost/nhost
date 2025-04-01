a2cd8d388e26af76a1ab659009d8cefd54161708
Sumit Saurabh <62152915+sumitsaurabh927@users.noreply.github.com>
2025-04-01 13:20:58 +0530

    chore: update links for new org structure (#97)

--- landing/src/pages/product/run/index.tsx
+++ landing/src/pages/product/run/index.tsx
@@ -114,7 +114,7 @@ export default function NhostRunPage() {
             variant="borderless"
             className="text-base font-bold"
             size="sm"
-            href="https://docs.nhost.io/product/run"
+            href="https://docs.nhost.io/products/run/overview"
             rel="noopener noreferrer"
             target="_blank"
           >
