d219bd36335003b03ff979786f86079d7b5d9faf
MrLotU <j.koopman@jarict.nl>
2025-05-15 19:03:52 +0200

    Add jsonb specific operators to OperationComboBox

--- dashboard/e2e/database/permissions-table.test.ts
+++ dashboard/e2e/database/permissions-table.test.ts
@@ -122,3 +122,80 @@ test('should create a table with role permissions and a custom check to select r
     page.getByText(/permission has been saved successfully/i),
   ).toBeVisible();
 });
+
+test('should be able to select jsonb specific operators for filter operations', async ({
+  authenticatedNhostPage: page,
+}) => {
+    await page.getByRole('button', { name: /new table/i }).click();
+  await expect(page.getByText(/create a new table/i)).toBeVisible();
+
+  const tableName = snakeCase(faker.lorem.words(3));
+
+  await prepareTable({
+    page,
+    name: tableName,
+    primaryKey: 'id',
+    columns: [
+      { name: 'id', type: 'uuid', defaultValue: 'gen_random_uuid()' },
+      { name: 'title', type: 'text' },
+      { name: 'jsonField', type: 'jsonb' },
+    ],
+  });
+
+  // create table
+  await page.getByRole('button', { name: /create/i }).click();
+
+  await page.waitForURL(
+    `/orgs/${TEST_ORGANIZATION_SLUG}/projects/${TEST_PROJECT_SUBDOMAIN}/database/browser/default/public/${tableName}`,
+  );
+
+  await expect(
+    page.getByRole('link', { name: tableName, exact: true }),
+  ).toBeVisible();
+
+  // Press three horizontal dots more options button next to the table name
+  await page
+    .locator(`li:has-text("${tableName}") #table-management-menu button`)
+    .click();
+
+  await page.getByRole('menuitem', { name: /edit permissions/i }).click();
+
+  await clickPermissionButton({ page, role: 'user', permission: 'Select' });
+
+  await page.getByLabel('With custom check').click();
+
+  // await page.getByRole('combobox', { name: /select a column/i }).click();
+  await page.getByText('Select a column', { exact: true }).click();
+
+  const columnSelector = page.locator('input[role="combobox"]');
+
+  await columnSelector.fill('jsonField');
+
+  await columnSelector.press('Enter');
+
+  await page.getByText('_eq', { exact: true }).click();
+
+  const operatorSelector = page.locator('input[role="combobox"]');
+
+  await operatorSelector.fill('_contains')
+
+  await expect(page.getByText(/No operator found/i)).not.toBeVisible();
+
+  await columnSelector.press('Enter');
+
+    await page.getByText('Select variable...', { exact: true }).click();
+
+  const variableSelector = await page.locator('input[role="combobox"]');
+
+  await variableSelector.fill('X-Hasura-User-Id');
+
+  await variableSelector.press('Enter');
+
+  await page.getByRole('button', { name: /select all/i }).click();
+
+  await page.getByRole('button', { name: /save/i }).click();
+
+  await expect(
+    page.getByText(/permission has been saved successfully/i),
+  ).toBeVisible();
+});
