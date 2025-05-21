91f4378061866f1403556c62591ffc5d8b097f13
gssakash-nhost <akash@nhost.io>
2025-05-21 20:26:29 +0530

    feat: GitLab OAuth Integrated

--- examples/react-apollo/src/components/auth/oauth-links.tsx
+++ examples/react-apollo/src/components/auth/oauth-links.tsx
@@ -1,11 +1,11 @@
-import { SiApple, SiGithub, SiGoogle, SiLinkedin, SiSpotify, SiTwitch } from '@icons-pack/react-simple-icons'
+import { SiApple, SiGithub, SiGoogle, SiLinkedin, SiSpotify, SiTwitch, SiGitlab } from '@icons-pack/react-simple-icons'
 import { Link } from 'react-router-dom'
 import { useProviderLink } from '@nhost/react'
 import { cn } from '@/lib/utils'
 import { buttonVariants } from '@/components/ui/button'
 
 export default function OAuthLinks() {
-  const { github, apple, google, linkedin, spotify, twitch } = useProviderLink({
+  const { github, apple, google, linkedin, spotify, twitch, gitlab } = useProviderLink({
     redirectTo: window.location.origin
   })
 
@@ -74,6 +74,17 @@ export default function OAuthLinks() {
       >
         <SiTwitch className="w-4 h-4" />
         <span className="flex-1 text-center">Continue with Twitch</span>
+      </Link>
+
+      <Link
+        to={gitlab}
+        className={cn(
+          buttonVariants({ variant: 'link' }),
+          'bg-[#FCA326] text-white hover:opacity-90 hover:no-underline'
+        )}
+      >
+        <SiGitlab className="w-4 h-4" />
+        <span className="flex-1 text-center">Continue with Gitlab</span>
       </Link>
 
     </div>
