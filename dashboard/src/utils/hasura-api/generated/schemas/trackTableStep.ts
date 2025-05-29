3bc9c2c4f94803f210cb8545bbd50af9e6d450c9
David BM <correodelnino@gmail.com>
2025-05-29 13:35:25 +0200

    wip: react-query orval

--- dashboard/src/utils/hasura-api/generated/schemas/trackTableStep.ts
+++ dashboard/src/utils/hasura-api/generated/schemas/trackTableStep.ts
@@ -5,8 +5,8 @@
  * API for managing database operations, migrations, and metadata permissions
  * OpenAPI spec version: 1.0.0
  */
-import type { TrackTableStepType } from './trackTableStepType';
 import type { TrackTableArgs } from './trackTableArgs';
+import type { TrackTableStepType } from './trackTableStepType';
 
 export interface TrackTableStep {
   /** Use after creating a table with run_sql to track it. You can provide this in the same query as the run_sql step */
