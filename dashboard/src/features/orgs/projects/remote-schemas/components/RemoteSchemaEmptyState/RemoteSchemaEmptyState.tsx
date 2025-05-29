a7a5922855e334b425aa93483731175b9772c010
David BM <correodelnino@gmail.com>
2025-05-29 13:35:25 +0200

    wip: react-query orval

---
+++ dashboard/src/features/orgs/projects/remote-schemas/components/RemoteSchemaEmptyState/RemoteSchemaEmptyState.tsx
@@ -0,0 +1,52 @@
+import { Text } from '@/components/ui/v2/Text';
+import Image from 'next/image';
+import type { DetailedHTMLProps, HTMLProps, ReactNode } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+export interface RemoteSchemaEmptyStateProps
+  extends Omit<
+    DetailedHTMLProps<HTMLProps<HTMLDivElement>, HTMLDivElement>,
+    'title'
+  > {
+  /**
+   * Title of the empty state.
+   */
+  title: ReactNode;
+  /**
+   * Description of the empty state.
+   */
+  description: ReactNode;
+}
+
+export default function RemoteSchemaEmptyState({
+  title,
+  description,
+  className,
+  ...props
+}: RemoteSchemaEmptyStateProps) {
+  return (
+    <div
+      className={twMerge(
+        'grid w-full place-content-center gap-2 px-4 py-16 text-center',
+        className,
+      )}
+      {...props}
+    >
+      <div className="mx-auto">
+        <Image
+          src="/assets/database.svg"
+          width={72}
+          height={72}
+          alt="Database"
+          priority
+        />
+      </div>
+
+      <Text variant="h3" component="h1">
+        {title}
+      </Text>
+
+      <Text>{description}</Text>
+    </div>
+  );
+}
--- dashboard/src/utils/hasura-api/generated/schemas/createObjectRelationshipArgs.ts
+++ dashboard/src/utils/hasura-api/generated/schemas/createObjectRelationshipArgs.ts
@@ -5,8 +5,8 @@
  * API for managing database operations, migrations, and metadata permissions
  * OpenAPI spec version: 1.0.0
  */
-import type { TableIdentifier } from './tableIdentifier';
 import type { CreateObjectRelationshipArgsUsing } from './createObjectRelationshipArgsUsing';
+import type { TableIdentifier } from './tableIdentifier';
 
 export interface CreateObjectRelationshipArgs {
   table: TableIdentifier;
