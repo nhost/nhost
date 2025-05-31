d23a0683a6ef66009344006c5872018af0ac20b4
David BM <correodelnino@gmail.com>
2025-05-31 03:35:11 +0200

    wip: main navigation

--- dashboard/src/features/orgs/projects/remote-schemas/components/RemoteSchemaBrowserSidebar/RemoteSchemaBrowserSidebar.tsx
+++ dashboard/src/features/orgs/projects/remote-schemas/components/RemoteSchemaBrowserSidebar/RemoteSchemaBrowserSidebar.tsx
@@ -33,6 +33,7 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
+import useGetRemoteSchemasQuery from '../../hooks/useGetRemoteSchemasQuery/useGetRemoteSchemasQuery';
 
 const CreateRemoteSchemaForm = dynamic(
   () =>
