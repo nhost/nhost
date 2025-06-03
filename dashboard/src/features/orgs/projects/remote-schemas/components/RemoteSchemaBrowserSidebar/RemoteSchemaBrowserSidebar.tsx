fb2af12fa32adbc29d43546b599ba2cab8941fbc
David BM <correodelnino@gmail.com>
2025-06-04 01:22:53 +0200

    wip: edit remote schema

--- dashboard/src/features/orgs/projects/remote-schemas/components/RemoteSchemaBrowserSidebar/RemoteSchemaBrowserSidebar.tsx
+++ dashboard/src/features/orgs/projects/remote-schemas/components/RemoteSchemaBrowserSidebar/RemoteSchemaBrowserSidebar.tsx
@@ -33,7 +33,6 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
-import useGetRemoteSchemasQuery from '../../hooks/useGetRemoteSchemasQuery/useGetRemoteSchemasQuery';
 
 const CreateRemoteSchemaForm = dynamic(
   () =>
