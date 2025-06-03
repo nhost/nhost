18136ed4c94a3d8a4518320e32ff5dd3d51413ac
David BM <correodelnino@gmail.com>
2025-06-04 01:22:53 +0200

    wip: edit remote schema

---
+++ dashboard/src/features/orgs/projects/remote-schemas/hooks/useRemoveRemoteSchemaMutation/useRemoveRemoteSchemaMutation.ts
@@ -0,0 +1,53 @@
+import { generateAppServiceUrl } from '@/features/orgs/projects/common/utils/generateAppServiceUrl';
+import { useProject } from '@/features/orgs/projects/hooks/useProject';
+import { getHasuraAdminSecret } from '@/utils/env';
+import type { MetadataOperation200 } from '@/utils/hasura-api/generated/schemas/metadataOperation200';
+import type { MutationOptions } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
+import type { RemoveRemoteSchemaVariables } from './removeRemoteSchema';
+import removeRemoteSchema from './removeRemoteSchema';
+
+export interface UseRemoveRemoteSchemaMutationOptions {
+  /**
+   * Props passed to the underlying mutation hook.
+   */
+  mutationOptions?: MutationOptions<
+    MetadataOperation200,
+    unknown,
+    RemoveRemoteSchemaVariables
+  >;
+}
+
+/**
+ * This hook is a wrapper around a fetch call that removes a remote schema.
+ *
+ * @param options - Options to use for the mutation.
+ * @returns The result of the mutation.
+ */
+export default function useRemoveRemoteSchemaMutation({
+  mutationOptions,
+}: UseRemoveRemoteSchemaMutationOptions = {}) {
+  const { project } = useProject();
+
+  const appUrl = generateAppServiceUrl(
+    project?.subdomain,
+    project?.region,
+    'hasura',
+  );
+  const mutationFn = removeRemoteSchema;
+
+  const mutation = useMutation(
+    (variables) =>
+      mutationFn({
+        ...variables,
+        appUrl,
+        adminSecret:
+          process.env.NEXT_PUBLIC_ENV === 'dev'
+            ? getHasuraAdminSecret()
+            : project?.config?.hasura.adminSecret,
+      }),
+    mutationOptions,
+  );
+
+  return mutation;
+}
