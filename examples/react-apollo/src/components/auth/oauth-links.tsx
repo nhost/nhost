e353d99de8d803d7402498d09baf89fb9a6d2942
gssakash-nhost <akash@nhost.io>
2025-06-13 14:36:43 +0530

    feat (examples/react-apollo): add more social providers (#3339)
    ### **User description**
    ___
    
    ### **PR Type**
    Enhancement
    
    
    ___
    
    ### **Description**
    - **Added OAuth Integrations**  
    Enabled OAuth for Spotify, Twitch, GitLab, Bitbucket, WorkOS, Discord,
    AzureAD, Facebook, Strava, Windows Live, and Twitter.
    - **Enhanced UI**  
      Added login buttons for each provider.
    - **Updated Configuration**  
    Configured OAuth settings in `nhost.toml` and updated the authentication
    version as necessary.
    - **Included Example Credentials**  
      Added client credentials to `.secrets.example` for easy setup.
    
    ___
    
    
    
    ### **Changes walkthrough** 📝
    <table><thead><tr><th></th><th align="left">Relevant
    files</th></tr></thead><tbody><tr><td><strong>Enhancement</strong></td><td><table>
    <tr>
      <td>
        <details>
    <summary><strong>oauth-links.tsx</strong><dd><code>Add Spotify OAuth
    button to login options</code>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    </dd></summary>
    <hr>
    
    examples/react-apollo/src/components/auth/oauth-links.tsx
    
    <li>Imported Spotify icon from react-simple-icons<br> <li> Added Spotify
    to useProviderLink hook<br> <li> Implemented Spotify login button with
    styling
    
    
    </details>
    
    
      </td>
    <td><a
    href="https://github.com/nhost/nhost/pull/3339/files#diff-f5999fa99948c7a83619e69ab669da87ca10146ad5742f93112e21b00932bc0e">+14/-2</a>&nbsp;
    &nbsp; </td>
    
    </tr>
    </table></td></tr><tr><td><strong>Configuration
    changes</strong></td><td><table>
    <tr>
      <td>
        <details>
    <summary><strong>nhost.toml</strong><dd><code>Enable Spotify OAuth in
    e2e test project</code>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    </dd></summary>
    <hr>
    
    dashboard/e2e/e2e-tests-project/nhost/nhost.toml
    
    - Enabled Spotify OAuth method
    
    
    </details>
    
    
      </td>
    <td><a
    href="https://github.com/nhost/nhost/pull/3339/files#diff-9803f45ffb53ea1df64006a0bb2d3cb01ae0a3104f021945c546e97ee0e63f9e">+1/-1</a>&nbsp;
    &nbsp; &nbsp; </td>
    
    </tr>
    
    <tr>
      <td>
        <details>
    <summary><strong>.secrets.example</strong><dd><code>Add Spotify
    credentials to secrets example</code>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    </dd></summary>
    <hr>
    
    examples/react-apollo/.secrets.example
    
    - Added placeholders for Spotify client ID and secret
    
    
    </details>
    
    
      </td>
    <td><a
    href="https://github.com/nhost/nhost/pull/3339/files#diff-8df9deedb2d447b8e687d9bddcdeb89e013380307483034c4fc8973354877703">+2/-0</a>&nbsp;
    &nbsp; &nbsp; </td>
    
    </tr>
    
    <tr>
      <td>
        <details>
    <summary><strong>nhost.toml</strong><dd><code>Configure Spotify OAuth in
    example project</code>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </dd></summary>
    <hr>
    
    examples/react-apollo/nhost/nhost.toml
    
    <li>Enabled Spotify OAuth method<br> <li> Added configuration for
    Spotify client ID and secret
    
    
    </details>
    
    
      </td>
    <td><a
    href="https://github.com/nhost/nhost/pull/3339/files#diff-268d6c8dddd6990d60d62c1c923955c4e0e7549a80f0f5856192f889378416a0">+3/-1</a>&nbsp;
    &nbsp; &nbsp; </td>
    
    </tr>
    </table></td></tr></tr></tbody></table>
    
    ___
    
    > <details> <summary> Need help?</summary><li>Type <code>/help how to
    ...</code> in the comments thread for any questions about PR-Agent
    usage.</li><li>Check out the <a
    href="https://qodo-merge-docs.qodo.ai/usage-guide/">documentation</a>
    for more information.</li></details>
    
    ---------
    
    Co-authored-by: David Barroso <dbarrosop@dravetech.com>

--- examples/react-apollo/src/components/auth/oauth-links.tsx
+++ examples/react-apollo/src/components/auth/oauth-links.tsx
@@ -1,11 +1,12 @@
-import { SiApple, SiGithub, SiGoogle, SiLinkedin } from '@icons-pack/react-simple-icons'
+import { SiApple, SiGithub, SiGoogle, SiLinkedin, SiDiscord, SiSpotify, SiTwitch, SiGitlab, SiBitbucket, SiMicrosoftazure, SiFacebook, SiStrava, SiWindows, SiX } from '@icons-pack/react-simple-icons'
 import { Link } from 'react-router-dom'
 import { useProviderLink } from '@nhost/react'
 import { cn } from '@/lib/utils'
 import { buttonVariants } from '@/components/ui/button'
+import { WorkOSIcon } from '../ui/workosicon'
 
 export default function OAuthLinks() {
-  const { github, apple, google, linkedin } = useProviderLink({
+  const { github, apple, google, linkedin, discord, spotify, twitch, gitlab, bitbucket, workos, azuread, facebook, strava, windowslive, twitter } = useProviderLink({
     redirectTo: window.location.origin
   })
 
@@ -53,6 +54,128 @@ export default function OAuthLinks() {
         <SiLinkedin className="w-4 h-4" />
         <span className="flex-1 text-center">Continue with LinkedIn</span>
       </Link>
+
+      <Link
+        to={discord}
+        className={cn(
+          buttonVariants({ variant: 'link' }),
+          'bg-[#7289da] text-white hover:opacity-90 hover:no-underline'
+        )}
+      >
+        <SiDiscord className="w-4 h-4" />
+        <span className="flex-1 text-center">Continue with Discord</span>
+      </Link>
+
+      <Link
+        to={spotify}
+        className={cn(
+          buttonVariants({ variant: 'link' }),
+          'bg-[#1DB954] text-white hover:opacity-90 hover:no-underline'
+        )}
+      >
+        <SiSpotify className="w-4 h-4" />
+        <span className="flex-1 text-center">Continue with Spotify</span>
+      </Link>
+
+      <Link
+        to={twitch}
+        className={cn(
+          buttonVariants({ variant: 'link' }),
+          'bg-[#9146ff] text-white hover:opacity-90 hover:no-underline'
+        )}
+      >
+        <SiTwitch className="w-4 h-4" />
+        <span className="flex-1 text-center">Continue with Twitch</span>
+      </Link>
+
+      <Link
+        to={gitlab}
+        className={cn(
+          buttonVariants({ variant: 'link' }),
+          'bg-[#FCA326] text-white hover:opacity-90 hover:no-underline'
+        )}
+      >
+        <SiGitlab className="w-4 h-4" />
+        <span className="flex-1 text-center">Continue with Gitlab</span>
+      </Link>
+
+      <Link
+        to={bitbucket}
+        className={cn(
+          buttonVariants({ variant: 'link' }),
+          'bg-[#253858] text-white hover:opacity-90 hover:no-underline'
+        )}
+      >
+        <SiBitbucket className="w-4 h-4" />
+        <span className="flex-1 text-center">Continue with Bitbucket</span>
+      </Link>
+
+      <Link
+        to={workos}
+        className={cn(
+          buttonVariants({ variant: 'link' }),
+          'bg-[#4F46E5] text-white hover:opacity-90 hover:no-underline'
+        )}
+      >
+        <WorkOSIcon />
+        <span className="flex-1 text-center">Continue with WorkOS</span>
+      </Link>
+
+      <Link
+        to={azuread}
+        className={cn(
+          buttonVariants({ variant: 'link' }),
+          'bg-[#3CCBF4] text-white hover:opacity-90 hover:no-underline'
+        )}
+      >
+        <SiMicrosoftazure className="w-4 h-4" />
+        <span className="flex-1 text-center">Continue with Azure AD</span>
+      </Link>
+
+      <Link
+        to={facebook}
+        className={cn(
+          buttonVariants({ variant: 'link' }),
+          'bg-[#3b5998] text-white hover:opacity-90 hover:no-underline'
+        )}
+      >
+        <SiFacebook className="w-4 h-4" />
+        <span className="flex-1 text-center">Continue with Facebook</span>
+      </Link>
+
+      <Link
+        to={strava}
+        className={cn(
+          buttonVariants({ variant: 'link' }),
+          'bg-[#FC5200] text-white hover:opacity-90 hover:no-underline'
+        )}
+      >
+        <SiStrava className="w-4 h-4" />
+        <span className="flex-1 text-center">Continue with Strava</span>
+      </Link>
+
+      <Link
+        to={windowslive}
+        className={cn(
+          buttonVariants({ variant: 'link' }),
+          'bg-[#0b8de3] text-white hover:opacity-90 hover:no-underline'
+        )}
+      >
+        <SiWindows className="w-4 h-4" />
+        <span className="flex-1 text-center">Continue with Windows Live</span>
+      </Link>
+
+      <Link
+        to={twitter}
+        className={cn(
+          buttonVariants({ variant: 'link' }),
+          'bg-[#000000] text-white hover:opacity-90 hover:no-underline'
+        )}
+      >
+        <SiX className="w-4 h-4" />
+        <span className="flex-1 text-center">Continue with Twitter</span>
+      </Link>
+
     </div>
   )
 }
