08a3cd8e68fa576bd32016897f1d9702c8212b74
David Barroso <dbarrosop@dravetech.com>
2025-08-13 12:11:48 +0200

    asd

--- dashboard/src/features/orgs/projects/storage/dataGrid/components/DataGridPreviewCell/DataGridPreviewCell.tsx
+++ dashboard/src/features/orgs/projects/storage/dataGrid/components/DataGridPreviewCell/DataGridPreviewCell.tsx
@@ -219,12 +219,15 @@ export default function DataGridPreviewCell<TData extends object>({
       dispatch({ type: 'PREVIEW_LOADING' });
     }
 
-    const { body: presignedUrl } = await appClient.storage.getFilePresignedURL(id, {
-      headers: {
-        'x-hasura-admin-secret':
-          process.env.NEXT_PUBLIC_ENV === 'dev'
-            ? getHasuraAdminSecret()
-            : project!.config!.hasura.adminSecret,
+    const { body: presignedUrl } = await appClient.storage.getFilePresignedURL(
+      id,
+      {
+        headers: {
+          'x-hasura-admin-secret':
+            process.env.NEXT_PUBLIC_ENV === 'dev'
+              ? getHasuraAdminSecret()
+              : project!.config!.hasura.adminSecret,
+        },
       },
     );
 
