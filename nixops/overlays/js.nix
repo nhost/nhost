ae29426f3b7a0e4e4ef514271caab04ecda4ae16
David Barroso <dbarrosop@dravetech.com>
2025-09-04 22:43:12 +0200

    asd

--- nixops/overlays/js.nix
+++ nixops/overlays/js.nix
@@ -1,10 +1,10 @@
 (final: prev: rec {
-  nodejs = final.nodejs-slim_20;
+  nodejs = final.nodejs_20;
   nodePackages = nodejs.pkgs;
 
   pnpm_10 = final.callPackage "${final.path}/pkgs/development/tools/pnpm/generic.nix" {
-    version = "10.15.1";
-    hash = "sha256-jFOvAq4+wfsK51N3+NTWIXwtfL5vA8FjUMq/dJPebv8=";
+    version = "10.1.0";
+    hash = "sha256-PuU+kUAR7H8abjqwxYuaAkoFK/4YKVsjtoVn1qal680=";
   };
 
   ell = prev.ell.overrideAttrs (oldAttrs: {
