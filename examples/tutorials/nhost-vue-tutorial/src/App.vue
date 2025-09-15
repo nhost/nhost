6e0a81b59a89fd93e4b99cc839847c27bbdfde98
David Barroso <dbarrosop@dravetech.com>
2025-09-15 13:05:58 +0200

    asd

---
+++ examples/tutorials/nhost-vue-tutorial/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <NavigationBar />
+  <div class="app-content">
+    <RouterView />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+import NavigationBar from "./components/NavigationBar.vue";
+</script>
