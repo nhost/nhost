d37d0aee01cbb4a5088ad68b0fa481167968c4d1
David Barroso <dbarrosop@dravetech.com>
2025-09-15 15:29:32 +0200

    sad

---
+++ examples/tutorials/nhost-vue-tutorial/src/components/Navigation.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="navigation">
+    <div class="nav-container">
+      <RouterLink to="/" class="nav-logo">
+        Nhost Vue Demo
+      </RouterLink>
+
+      <div class="nav-links">
+        <RouterLink to="/" class="nav-link">
+          Home
+        </RouterLink>
+
+        <template v-if="isAuthenticated">
+          <RouterLink to="/profile" class="nav-link">
+            Profile
+          </RouterLink>
+        </template>
+        <template v-else>
+          Placeholder for signin/signup links
+        </template>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+import { useAuth } from "../lib/nhost/auth";
+
+const { isAuthenticated } = useAuth();
+</script>
