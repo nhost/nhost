d570084d24d2a161c9ba1aabe613817cb345c7b0
David Barroso <dbarrosop@dravetech.com>
2025-09-19 11:36:48 +0200

    chore (docs): udpate graphql documentation to include example with variables (#3478)
    ### **PR Type**
    Tests, Documentation
    
    
    ___
    
    ### **Description**
    - Add GraphQL request test with variables support
    
    - Update qql test to include variables option
    
    - Extend SDK docstrings with variables usage section
    
    - Enhance documentation with variables example
    
    
    ___
    
    
    
    <details> <summary><h3> File Walkthrough</h3></summary>
    
    <table><thead><tr><th></th><th align="left">Relevant
    files</th></tr></thead><tbody><tr><td><strong>Tests</strong></td><td><table>
    <tr>
      <td>
        <details>
    <summary><strong>docstrings.test.ts</strong><dd><code>Enhance tests with
    variables usage</code>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </dd></summary>
    <hr>
    
    packages/nhost-js/src/graphql/__tests__/docstrings.test.ts
    
    <ul><li>Added new "request with variables" test case<br> <li> Modified
    qql test to pass variables in request<br> <li> Removed obsolete
    commented test data</ul>
    
    
    </details>
    
    
      </td>
    <td><a
    href="https://github.com/nhost/nhost/pull/3478/files#diff-c7b7dab18f108770bcff06dc83cc9a34a5ee72c5021a035129a264a968f7d5ff">+58/-39</a>&nbsp;
    </td>
    
    </tr>
    </table></td></tr><tr><td><strong>Documentation</strong></td><td><table>
    <tr>
      <td>
        <details>
    <summary><strong>index.ts</strong><dd><code>Document variables in
    GraphQL client</code>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; </dd></summary>
    <hr>
    
    packages/nhost-js/src/graphql/index.ts
    
    <ul><li>Inserted "Using Variables" section in SDK docstrings<br> <li>
    Added includeCode references for variables examples<br> <li> Adjusted
    snippet ranges for variable usage</ul>
    
    
    </details>
    
    
      </td>
    <td><a
    href="https://github.com/nhost/nhost/pull/3478/files#diff-6124d7be08592679e7ba8ae19ab28f3fca0d9a8fcd04c62f130eb8038eeb29e4">+11/-3</a>&nbsp;
    &nbsp; </td>
    
    </tr>
    
    <tr>
      <td>
        <details>
    <summary><strong>graphql.mdx</strong><dd><code>Add variables example to
    user docs</code>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </dd></summary>
    <hr>
    
    docs/reference/javascript/nhost-js/graphql.mdx
    
    <ul><li>Added "Using Variables" section with code example<br> <li>
    Demonstrated passing variables to GraphQL requests<br> <li> Updated qql
    example to show variable usage</ul>
    
    
    </details>
    
    
      </td>
    <td><a
    href="https://github.com/nhost/nhost/pull/3478/files#diff-be11be6441756582b629e0f4f6acef5c1bb59d749c3fa3202043f7cf345696a2">+66/-3</a>&nbsp;
    &nbsp; </td>
    
    </tr>
    </table></td></tr></tr></tbody></table>
    
    </details>
    
    ___

--- packages/nhost-js/src/graphql/index.ts
+++ packages/nhost-js/src/graphql/index.ts
@@ -19,6 +19,14 @@
  *
  * {@includeCode ./__tests__/docstrings.test.ts:2,6,11-25}
  *
+ * ### Using Variables
+ *  You can pass variables to your GraphQL queries and mutations using the `variables` option:
+ *
+ * {@includeCode ./__tests__/docstrings.test.ts:2,6,195-228}
+ *
+ * This allows you to dynamically pass values to your queries and mutations, making them more
+ * flexible and reusable.
+ *
  * ### Using String Queries with Type Generics
  *
  * You can type your GraphQL queries and responses using TypeScript generics:
@@ -30,7 +38,7 @@
  * For better integration with third-party libraries like Apollo Client or The Guild's GraphQL
  * Code Generator, you can use GraphQL document nodes created with `gql` template literal tags:
  *
- * {@includeCode ./__tests__/docstrings.test.ts:2,5,6,195-223}
+ * {@includeCode ./__tests__/docstrings.test.ts:2,5,6,248-293}
  *
  * Using document nodes enables:
  * - Better IDE support with syntax highlighting and validation
@@ -43,12 +51,12 @@
  * with length > 0. The error will be an instance of `FetchError<GraphQLResponse>` and will
  * contain the response body with the errors.
  *
- * {@includeCode ./__tests__/docstrings.test.ts:2-4,6,294-334,348}
+ * {@includeCode ./__tests__/docstrings.test.ts:2-4,6,313-353,367}
  *
  * This type extends the standard `Error` type so if you want to just log the error you can
  * do so like this:
  *
- * {@includeCode ./__tests__/docstrings.test.ts:2-4,6,352-375,380}
+ * {@includeCode ./__tests__/docstrings.test.ts:2-4,6,371-394,399}
  *
  * @module graphql
  * @packageDocumentation
