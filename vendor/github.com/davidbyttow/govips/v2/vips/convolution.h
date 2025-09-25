0b560dcb5282333e25ec2c68259330e0e6117678
David Barroso <dbarrosop@dravetech.com>
2025-09-25 16:56:32 +0200

    feat(storage): import nhost/hasura-storage into ./services/storage/

--- vendor/github.com/davidbyttow/govips/v2/vips/convolution.h
+++
@@ -1,9 +0,0 @@
-// https://libvips.github.io/libvips/API/current/libvips-convolution.html
-
-#include <stdlib.h>
-#include <vips/vips.h>
-
-int gaussian_blur_image(VipsImage *in, VipsImage **out, double sigma, double min_ampl);
-int sharpen_image(VipsImage *in, VipsImage **out, double sigma, double x1,
-                  double m2);
-int sobel_image(VipsImage *in, VipsImage **out);
