b7cb67acc4ab3adb697ea7235785358e26a7325f
David BM <correodelnino@gmail.com>
2025-09-25 10:08:22 +0200

    fix: e2e test

--- dashboard/e2e/remote-schemas/create-remote-schema.test.ts
+++ dashboard/e2e/remote-schemas/create-remote-schema.test.ts
@@ -34,7 +34,9 @@ test.describe('Remote Schemas', () => {
     const detailsUrl = `/orgs/${TEST_ORGANIZATION_SLUG}/projects/${TEST_PROJECT_SUBDOMAIN}/graphql/remote-schemas/${schemaName}`;
     await page.waitForURL(detailsUrl);
 
-    await expect(page.getByRole('heading', { name: schemaName })).toBeVisible();
+    await expect(
+      page.getByRole('main').locator('p', { hasText: schemaName }),
+    ).toBeVisible();
     await expect(
       page.locator(`input[value="${REMOTE_SCHEMA_TEST_URL}"]`),
     ).toBeVisible();
