cccd685888808cb1ad7573ebc608310e09b9e441
David BM <correodelnino@gmail.com>
2025-09-30 16:45:54 +0200

    feat: initial commit

---
+++ dashboard/src/pages/orgs/[orgSlug]/projects/[appSubdomain]/settings/events/index.tsx
@@ -0,0 +1,33 @@
+import { Box } from '@/components/ui/v2/Box';
+import { OrgLayout } from '@/features/orgs/layout/OrgLayout';
+import { EventsBrowserSidebar } from '@/features/orgs/projects/events/components/EventsBrowserSidebar';
+import { EventsEmptyState } from '@/features/orgs/projects/events/components/EventsEmptyState';
+import type { ReactElement } from 'react';
+
+export default function EventsPage() {
+  return (
+    <EventsEmptyState
+      title="Events"
+      description="Select an event from the sidebar to get started."
+    />
+  );
+}
+
+EventsPage.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <OrgLayout
+      mainContainerProps={{
+        className: 'flex h-full',
+      }}
+    >
+      <EventsBrowserSidebar className="w-full max-w-sidebar" />
+
+      <Box
+        className="flex w-full flex-auto flex-col overflow-x-hidden"
+        sx={{ backgroundColor: 'background.default' }}
+      >
+        {page}
+      </Box>
+    </OrgLayout>
+  );
+};
