f24d85d174b4dfd97ec22cf2a839e1fca96e7261
David Barroso <dbarrosop@dravetech.com>
2025-10-02 14:05:28 +0200

    asd

--- nixops/overlays/default.nix
+++ nixops/overlays/default.nix
@@ -1,6 +1,10 @@
 final: prev:
 {
   nhost-cli = final.callPackage ./nhost-cli.nix { inherit final; };
+
+  certbot-full = prev.certbot.overrideAttrs (old: {
+    doCheck = false;
+  });
 }
 // import ./go.nix final prev
 // import ./js.nix final prev
