a443f90ee6ab034a6fbcdb5cf673727614cddf7f
David Barroso <dbarrosop@dravetech.com>
2025-10-02 08:55:24 +0200

    asd

--- docs/gen.sh
+++ docs/gen.sh
@@ -61,6 +61,7 @@ function build_typedoc() {
 function build_cli_docs() {
     echo "⚒️⚒️⚒️ Building CLI documentation..."
     cli docs > reference/cli/commands.mdx
+    cat reference/cli/commands.mdx
 }
 
 build_openapi
