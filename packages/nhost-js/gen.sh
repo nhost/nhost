5b809f6ac96a82ede0b787531093539f2f18e0d7
David Barroso <dbarrosop@dravetech.com>
2025-10-10 13:15:05 +0200

    chore(nhost-js): generate code from local API definitions (#3583)

--- packages/nhost-js/gen.sh
+++ packages/nhost-js/gen.sh
@@ -3,14 +3,14 @@
 set -e
 
 codegen gen \
-    --openapi-file ./api/auth.yaml \
+    --openapi-file ../../services/auth/docs/openapi.yaml \
     --output-file ./src/auth/client.ts \
     --plugin typescript
 
 pnpm prettier -w ./src/auth/client.ts
 
 codegen gen \
-    --openapi-file ./api/storage.yaml \
+    --openapi-file ../../services/storage/controller/openapi.yaml \
     --output-file ./src/storage/client.ts \
     --plugin typescript
 
