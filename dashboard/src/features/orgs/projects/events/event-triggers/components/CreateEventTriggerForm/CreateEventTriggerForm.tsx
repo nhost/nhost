7d45845500893e2dba7c910f308e9901d6618f00
David BM <correodelnino@gmail.com>
2025-10-13 11:55:08 +0200

    wip: create

--- dashboard/src/features/orgs/projects/events/event-triggers/components/CreateEventTriggerForm/CreateEventTriggerForm.tsx
+++ dashboard/src/features/orgs/projects/events/event-triggers/components/CreateEventTriggerForm/CreateEventTriggerForm.tsx
@@ -1,4 +1,13 @@
 import { Button } from '@/components/ui/v3/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/v3/form';
 import { Input } from '@/components/ui/v3/input';
 import { Label } from '@/components/ui/v3/label';
 import {
@@ -10,25 +19,60 @@ import {
   SheetHeader,
   SheetTitle,
 } from '@/components/ui/v3/sheet';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 
 export interface CreateEventTriggerFormProps {
   open: boolean;
   onOpenChange: (open: boolean) => void;
 }
 
+const formSchema = z.object({
+  username: z.string().min(1, { message: 'Username is required' }),
+});
+
 export default function CreateEventTriggerForm({
   open,
   onOpenChange,
+  onSubmit,
 }: CreateEventTriggerFormProps) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+    },
+  });
   return (
     <Sheet open={open} onOpenChange={onOpenChange}>
-      <SheetContent>
+      <SheetContent className="w-xl md:w-4xl sm:max-w-4xl">
         <SheetHeader>
           <SheetTitle>Edit profile</SheetTitle>
           <SheetDescription>
             Make changes to your profile here. Click save when you&apos;re done.
           </SheetDescription>
         </SheetHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
         <div className="grid flex-1 auto-rows-min gap-6 px-4">
           <div className="grid gap-3">
             <Label htmlFor="sheet-demo-name">Name</Label>
