4ff986da6c98c9ef33cbe850e1baaaac25962718
David BM <correodelnino@gmail.com>
2025-10-13 11:11:28 +0200

    chore: refactor

--- dashboard/src/features/orgs/projects/events/event-triggers/hooks/useGetEventAndInvocationLogsById/fetchEventAndInvocationLogsById.ts
+++ dashboard/src/features/orgs/projects/events/event-triggers/hooks/useGetEventAndInvocationLogsById/fetchEventAndInvocationLogsById.ts
@@ -1,10 +1,10 @@
-import type { MetadataOperationOptions } from '@/features/orgs/projects/remote-schemas/types';
 import { metadataOperation } from '@/utils/hasura-api/generated/default/default';
 import type {
   GetEventAndInvocationLogsByIdArgs,
   GetEventAndInvocationLogsByIdOperation,
   GetEventAndInvocationLogsByIdResponse,
 } from '@/utils/hasura-api/generated/schemas';
+import type { MetadataOperationOptions } from '@/utils/hasura-api/types';
 
 /**
  * This function fetches the event and invocation logs for a given event id.
