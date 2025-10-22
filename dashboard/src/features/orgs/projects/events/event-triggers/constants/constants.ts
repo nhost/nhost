1aa4f98941bf58d10cb9340b37df8cdfa2a6d4af
David BM <correodelnino@gmail.com>
2025-10-22 16:57:06 +0200

    wip: asd form

--- dashboard/src/features/orgs/projects/events/event-triggers/constants/constants.ts
+++ dashboard/src/features/orgs/projects/events/event-triggers/constants/constants.ts
@@ -1,1 +1,10 @@
 export const DEFAULT_RETRY_TIMEOUT_SECONDS = 60;
+
+export const TRIGGER_OPERATIONS = [
+  'insert',
+  'update',
+  'delete',
+  'manual',
+] as const;
+
+export const UPDATE_TRIGGER_ON = ['all', 'choose'] as const;
