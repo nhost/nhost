509d4f716a6d2ba5d3d876dbca086335dc82c853
David BM <correodelnino@gmail.com>
2025-10-26 19:58:12 +0100

    feat: headers section

--- dashboard/src/features/orgs/projects/events/event-triggers/utils/buildCreateEventTriggerDTO/buildCreateEventTriggerDTO.ts
+++ dashboard/src/features/orgs/projects/events/event-triggers/utils/buildCreateEventTriggerDTO/buildCreateEventTriggerDTO.ts
@@ -1,9 +1,52 @@
-import type { CreateEventTriggerFormValues } from '@/features/orgs/projects/events/event-triggers/components/CreateEventTriggerForm';
+import type { CreateEventTriggerFormValues } from '@/features/orgs/projects/events/event-triggers/components/CreateEventTriggerForm/CreateEventTriggerFormTypes';
 import type { CreateEventTriggerArgs } from '@/utils/hasura-api/generated/schemas';
+import type { OperationSpec } from '@/utils/hasura-api/generated/schemas/operationSpec';
 
 export default function buildCreateEventTriggerDTO(
   values: CreateEventTriggerFormValues,
 ): CreateEventTriggerArgs {
+  const headers = values.headers.map((header) => {
+    if (header.type === 'fromEnv') {
+      return {
+        name: header.name,
+        value_from_env: header.value,
+      };
+    }
+    return {
+      name: header.name,
+      value: header.value,
+    };
+  });
+
+  const insert: OperationSpec = values.triggerOperations.includes('insert')
+    ? {
+        columns: '*',
+      }
+    : null;
+
+  const deleteTrigger: OperationSpec = values.triggerOperations.includes(
+    'delete',
+  )
+    ? {
+        columns: '*',
+      }
+    : null;
+
+  const update: OperationSpec = values.triggerOperations.includes('update')
+    ? {
+        columns:
+          values.updateTriggerOn === 'all' ? '*' : values.updateTriggerColumns,
+      }
+    : null;
+
+  const retry_conf = {
+    num_retries: values.retryConf.numRetries,
+    interval_sec: values.retryConf.intervalSec,
+    timeout_sec: values.retryConf.timeoutSec,
+  };
+
+  const enable_manual = values.triggerOperations.includes('manual');
+
   return {
     name: values.triggerName,
     source: values.dataSource,
@@ -12,6 +55,13 @@ export default function buildCreateEventTriggerDTO(
       schema: values.tableSchema,
     },
     webhook: values.webhook,
-    triggerOperations: values.triggerOperations,
+    webhook_from_env: null,
+    insert,
+    update,
+    delete: deleteTrigger,
+    headers,
+    retry_conf,
+    enable_manual,
+    replace: false,
   };
 }
