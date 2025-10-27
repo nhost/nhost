1e4305e1aff95348fcb5bd0b0534f7df0a52e000
David BM <correodelnino@gmail.com>
2025-10-27 11:03:00 +0100

    wip: request options

---
+++ dashboard/src/features/orgs/projects/events/event-triggers/components/CreateEventTriggerForm/sections/RequestOptionsSection.tsx
@@ -0,0 +1,69 @@
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+  FieldLegend,
+  FieldSet,
+} from '@/components/ui/v3/field';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/v3/radio-group';
+import {
+  requestTransformMethods,
+  type CreateEventTriggerFormValues,
+} from '@/features/orgs/projects/events/event-triggers/components/CreateEventTriggerForm/CreateEventTriggerFormTypes';
+import { Controller, useFormContext } from 'react-hook-form';
+
+export default function RequestOptionsSection() {
+  const form = useFormContext<CreateEventTriggerFormValues>();
+  return (
+    <FieldSet className="px-6">
+      <FieldLegend className="text-foreground">Request Options</FieldLegend>
+      <FieldDescription>
+        Configuration to transform the request before sending it to the webhook
+      </FieldDescription>
+      <FieldGroup className="flex flex-col gap-8 lg:flex-row">
+        <Controller
+          name="requestTransform.method"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <FieldSet data-invalid={fieldState.invalid}>
+              <FieldLegend variant="label" className="text-foreground">
+                Request Method
+              </FieldLegend>
+              <RadioGroup
+                name={field.name}
+                value={field.value}
+                onValueChange={field.onChange}
+                aria-invalid={fieldState.invalid}
+                className="flex flex-row items-center gap-12"
+              >
+                {requestTransformMethods.map((requestTransformMethod) => (
+                  <Field
+                    key={requestTransformMethod}
+                    orientation="horizontal"
+                    data-invalid={fieldState.invalid}
+                    className="w-auto"
+                  >
+                    <RadioGroupItem
+                      value={requestTransformMethod}
+                      id={`request-transform-method-${requestTransformMethod}`}
+                      aria-invalid={fieldState.invalid}
+                    />
+                    <FieldLabel
+                      htmlFor={`request-transform-method-${requestTransformMethod}`}
+                      className="font-normal text-foreground"
+                    >
+                      {requestTransformMethod}
+                    </FieldLabel>
+                  </Field>
+                ))}
+              </RadioGroup>
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </FieldSet>
+          )}
+        />
+      </FieldGroup>
+    </FieldSet>
+  );
+}
