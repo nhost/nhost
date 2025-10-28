2612ab53c4e2796f5f6c3b814f4064dd362a2cd0
David BM <correodelnino@gmail.com>
2025-10-28 15:56:03 +0100

    fix: first redirect

--- dashboard/src/features/auth/SignUp/SignUpWithGithub/SignUpWithGithub.tsx
+++ dashboard/src/features/auth/SignUp/SignUpWithGithub/SignUpWithGithub.tsx
@@ -2,7 +2,7 @@ import { GithubAuthButton } from '@/features/auth/AuthProviders/Github/component
 import { useHostName } from '@/features/orgs/projects/common/hooks/useHostName';
 
 function SignUpWithGithub() {
-  const redirectTo = `${useHostName()}/github-app?signinProvider=github&state=normal-path`;
+  const redirectTo = useHostName();
   return (
     <GithubAuthButton
       redirectTo={redirectTo}
