8bed2393c399f4d862f5b86395cfb1c21510639f
David Barroso <dbarrosop@dravetech.com>
2025-11-03 14:47:35 +0100

    asd

--- lib/oapi/example/main.go
+++ lib/oapi/example/main.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"time"
 
-	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/getkin/kin-openapi/openapi3filter"
 	"github.com/gin-gonic/gin"
 	"github.com/lmittmann/tint"
@@ -16,7 +15,6 @@ import (
 	"github.com/nhost/nhost/lib/oapi/example/api"
 	"github.com/nhost/nhost/lib/oapi/example/controller"
 	"github.com/nhost/nhost/services/auth/docs"
-	"github.com/nhost/nhost/services/auth/go/middleware"
 )
 
 const apiPrefix = "/"
@@ -31,77 +29,6 @@ func getLogger() *slog.Logger {
 	})
 
 	return slog.New(handler)
-}
-
-func surfaceErrorsMiddleWare(c *gin.Context) {
-	// this captures two cases as far as I can see:
-	// 1. request validation errors where the strict generated code fails
-	//    to bind the request to the struct (i.e. "invalid param" test)
-	// 2. when a handler returns an error instead of a response
-	c.Next()
-
-	if len(c.Errors) > 0 && !c.IsAborted() {
-		var errorCode string
-		switch c.Writer.Status() {
-		case http.StatusBadRequest:
-			errorCode = "bad-request"
-		default:
-			errorCode = "internal-server-error"
-		}
-
-		c.JSON(
-			c.Writer.Status(),
-			gin.H{"errors": errorCode, "message": c.Errors[0].Error()},
-		)
-	}
-}
-
-func asd[T any](
-	schema []byte,
-	apiPrefix string,
-	handler T,
-	registerHandlerFn func(gin.IRouter, T, api.GinServerOptions),
-	authenticationFunc openapi3filter.AuthenticationFunc,
-	logger *slog.Logger,
-) (*gin.Engine, error) {
-	router := gin.New()
-
-	loader := openapi3.NewLoader()
-
-	doc, err := loader.LoadFromData(schema)
-	if err != nil {
-		return nil, fmt.Errorf("failed to load OpenAPI schema: %w", err)
-	}
-
-	doc.AddServer(&openapi3.Server{ //nolint:exhaustruct
-		URL: apiPrefix,
-	})
-
-	router.Use(
-		gin.Recovery(),
-		middleware.Logger(logger),
-		surfaceErrorsMiddleWare,
-	)
-
-	mw := api.MiddlewareFunc(oapi.OapiRequestValidatorWithOptions(
-		doc, &oapi.Options{ //nolint:exhaustruct
-			Options: openapi3filter.Options{ //nolint:exhaustruct
-				AuthenticationFunc: authenticationFunc,
-			},
-		},
-	))
-
-	registerHandlerFn(
-		router,
-		handler,
-		api.GinServerOptions{
-			BaseURL:      apiPrefix,
-			Middlewares:  []api.MiddlewareFunc{mw},
-			ErrorHandler: nil,
-		},
-	)
-
-	return router, nil
 }
 
 func authFn(
@@ -128,9 +55,14 @@ func setupRouter(logger *slog.Logger) (*gin.Engine, error) {
 	ctrl := controller.NewController()
 	handler := api.NewStrictHandler(ctrl, []api.StrictMiddlewareFunc{})
 
-	return asd(
+	router, err := oapi.NewRouter(
 		docs.OpenAPISchema, apiPrefix, handler, api.RegisterHandlersWithOptions, authFn, logger,
 	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create oapi router: %w", err)
+	}
+
+	return router, nil
 }
 
 func run(ctx context.Context) error {
--- lib/oapi/oapi.go
+++ lib/oapi/oapi.go
@@ -1,113 +1,78 @@
 package oapi
 
 import (
-	"context"
-	"errors"
 	"fmt"
+	"log/slog"
 	"net/http"
 
 	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/getkin/kin-openapi/openapi3filter"
-	"github.com/getkin/kin-openapi/routers"
-	"github.com/getkin/kin-openapi/routers/gorillamux"
 	"github.com/gin-gonic/gin"
-	"github.com/nhost/nhost/services/auth/go/controller"
+	"github.com/nhost/nhost/lib/oapi/example/api"
+	"github.com/nhost/nhost/services/auth/go/middleware"
 )
 
-type ContextKey string
+func surfaceErrorsMiddleWare(c *gin.Context) {
+	// this captures two cases as far as I can see:
+	// 1. request validation errors where the strict generated code fails
+	//    to bind the request to the struct (i.e. "invalid param" test)
+	// 2. when a handler returns an error instead of a response
+	c.Next()
 
-const (
-	GinContextKey ContextKey = "nhost-oapi/gin-context"
-)
-
-// ErrorHandler is called when there is an error in validation.
-type ErrorHandler func(c *gin.Context, err error, statusCode int)
-
-// Options to customize request validation. These are passed through to
-// openapi3filter.
-type Options struct {
-	Options      openapi3filter.Options
-	ParamDecoder openapi3filter.ContentParameterDecoder
-}
-
-func HandleError(c *gin.Context, err error) {
-	var (
-		errReq    *openapi3filter.RequestError
-		errSchema *openapi3.SchemaError
-		errAuth   *AuthenticatorError
-		errSec    *openapi3filter.SecurityRequirementsError
-	)
-	switch {
-	case errors.Is(err, controller.ErrElevatedClaimRequired):
-		c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "Elevated claim required"})
-	case errors.As(err, &errSchema):
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-			"error":  "schema-validation-error",
-			"reason": errSchema.Reason,
-		})
-	case errors.As(err, &errReq):
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-			"error":  "request-validation-error",
-			"reason": errReq.Err.Error(),
-		})
-	case errors.As(err, &errAuth):
-		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-			"error":          errAuth.Code,
-			"reason":         errAuth.Message,
-			"securityScheme": errAuth.Scheme,
-		})
-	case errors.As(err, &errSec):
-		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-			"error":  "unauthorized",
-			"reason": errSec.Error(),
-		})
-	default:
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	}
-}
-
-func OapiRequestValidatorWithOptions(swagger *openapi3.T, options *Options) gin.HandlerFunc {
-	router, err := gorillamux.NewRouter(swagger)
-	if err != nil {
-		panic(err)
-	}
-
-	return func(c *gin.Context) {
-		if err := ValidateRequestFromContext(c, router, options); err != nil {
-			HandleError(c, err)
-		}
-
-		c.Next()
-	}
-}
-
-func ValidateRequestFromContext(c *gin.Context, router routers.Router, options *Options) error {
-	route, pathParams, err := router.FindRoute(c.Request)
-	if err != nil {
-		var e *routers.RouteError
-		switch {
-		case errors.As(err, &e):
-			return e
+	if len(c.Errors) > 0 && !c.IsAborted() {
+		var errorCode string
+		switch c.Writer.Status() {
+		case http.StatusBadRequest:
+			errorCode = "bad-request"
 		default:
-			return fmt.Errorf("error validating route: %w", err)
+			errorCode = "internal-server-error"
 		}
+
+		c.JSON(
+			c.Writer.Status(),
+			gin.H{"errors": errorCode, "message": c.Errors[0].Error()},
+		)
+	}
+}
+
+func NewRouter[T any](
+	schema []byte,
+	apiPrefix string,
+	handler T,
+	registerHandlerFn func(gin.IRouter, T, api.GinServerOptions),
+	authenticationFunc openapi3filter.AuthenticationFunc,
+	logger *slog.Logger,
+) (*gin.Engine, error) {
+	router := gin.New()
+
+	loader := openapi3.NewLoader()
+
+	doc, err := loader.LoadFromData(schema)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load OpenAPI schema: %w", err)
 	}
 
-	validationInput := &openapi3filter.RequestValidationInput{ //nolint:exhaustruct
-		Request:    c.Request,
-		PathParams: pathParams,
-		Route:      route,
-	}
+	doc.AddServer(&openapi3.Server{ //nolint:exhaustruct
+		URL: apiPrefix,
+	})
 
-	if options != nil {
-		validationInput.Options = &options.Options
-		validationInput.ParamDecoder = options.ParamDecoder
-	}
+	router.Use(
+		gin.Recovery(),
+		middleware.Logger(logger),
+		surfaceErrorsMiddleWare,
+	)
 
-	requestContext := context.WithValue(c.Request.Context(), GinContextKey, c)
-	if err := openapi3filter.ValidateRequest(requestContext, validationInput); err != nil {
-		return err //nolint:wrapcheck
-	}
+	mw := api.MiddlewareFunc(requestValidatorWithOptions(doc, authenticationFunc))
 
-	return nil
+	registerHandlerFn(
+		router,
+		handler,
+		api.GinServerOptions{
+			BaseURL:      apiPrefix,
+			Middlewares:  []api.MiddlewareFunc{mw},
+			ErrorHandler: nil,
+		},
+	)
+
+	return router, nil
 }
