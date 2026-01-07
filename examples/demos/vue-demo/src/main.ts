b38840471b2e09d886303ab53a6da9a9ff5c1e5a
robertkasza <robert.kasza@bishop-co.com>
2026-01-07 12:38:57 +0100

    chore(examples): run biome format in demos/vue-demo

--- examples/demos/vue-demo/src/main.ts
+++ examples/demos/vue-demo/src/main.ts
@@ -1,9 +1,9 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
 const app = createApp(App);
 
 app.use(router);
 
-app.mount("#app");
+app.mount('#app');
