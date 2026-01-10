abdc9021d02084e4057cda0b3722b2392675e48c
David Barroso <dbarrosop@dravetech.com>
2026-01-10 12:33:48 +0100

    asd

--- cli/examples/myproject/functions/echo.ts
+++ cli/examples/myproject/functions/echo.ts
@@ -2,6 +2,8 @@ import { Request, Response } from 'express'
 import process from 'process'
 
 export default (req: Request, res: Response) => {
+    console.log(`Request context: ${JSON.stringify(req.context)}`)
+    console.log(`Request context2: ${JSON.stringify(req.requestContext)}`)
     res.status(200).json(
         {
             headers: req.headers,
