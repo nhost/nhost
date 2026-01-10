efcb28805c9e703578b54d259751820e5770a7cb
David Barroso <dbarrosop@dravetech.com>
2026-01-10 17:17:11 +0100

    asd

--- cli/examples/myproject/functions/remote-schema.ts
+++ cli/examples/myproject/functions/remote-schema.ts
@@ -1,4 +1,4 @@
-import { createSchema, createYoga, type Plugin } from 'graphql-yoga';
+import { createSchema, createYoga } from 'graphql-yoga';
 import type { Request, Response } from 'express';
 
 const typeDefs = `
@@ -15,38 +15,16 @@ const resolvers = {
 
 const schema = createSchema({ typeDefs, resolvers });
 
-// Plugin to handle pre-parsed body from Express middleware
-const useExpressParsedBody: Plugin = {
-  onRequestParse({ serverContext, setRequestParser }) {
-    const expressReq = (serverContext as any)?.req;
-    if (expressReq?.body) {
-      setRequestParser(async () => ({
-        query: expressReq.body.query,
-        variables: expressReq.body.variables,
-        operationName: expressReq.body.operationName,
-        extensions: expressReq.body.extensions,
-      }));
-    }
-  },
-};
-
-const yoga = createYoga({
-  schema,
-  graphqlEndpoint: '*',
-  plugins: [useExpressParsedBody],
-});
+const yoga = createYoga({ schema });
 
 export default async function handler(req: Request, res: Response) {
   const url = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
 
-  const response = await yoga.fetch(
-    url,
-    {
-      method: req.method,
-      headers: req.headers as HeadersInit,
-    },
-    { req } // serverContext - makes req.body available to the plugin
-  );
+  const response = await yoga.fetch(url, {
+    method: req.method,
+    headers: req.headers as HeadersInit,
+    body: req.method !== 'GET' ? JSON.stringify(req.body) : undefined,
+  });
 
   res.status(response.status);
   response.headers.forEach((value, key) => {
