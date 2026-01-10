11c2fcb9a57d1cc19146656f3d08d0a9272469df
David Barroso <dbarrosop@dravetech.com>
2026-01-10 17:04:50 +0100

    asd

--- cli/examples/myproject/functions/remote-schema.ts
+++ cli/examples/myproject/functions/remote-schema.ts
@@ -1,4 +1,5 @@
 import { createSchema, createYoga, type Plugin } from 'graphql-yoga';
+import type { Request, Response } from 'express';
 
 const typeDefs = `
   type Query {
@@ -35,4 +36,23 @@ const yoga = createYoga({
   plugins: [useExpressParsedBody],
 });
 
-export default yoga;
+export default async function handler(req: Request, res: Response) {
+  const url = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+
+  const response = await yoga.fetch(
+    url,
+    {
+      method: req.method,
+      headers: req.headers as HeadersInit,
+    },
+    { req } // serverContext - makes req.body available to the plugin
+  );
+
+  res.status(response.status);
+  response.headers.forEach((value, key) => {
+    res.setHeader(key, value);
+  });
+
+  const body = await response.text();
+  res.send(body);
+}
