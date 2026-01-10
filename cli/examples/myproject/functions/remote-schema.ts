ff3638395dc4063175f68b3a9cd422d582fff80b
David Barroso <dbarrosop@dravetech.com>
2026-01-10 17:06:43 +0100

    asd

--- cli/examples/myproject/functions/remote-schema.ts
+++ cli/examples/myproject/functions/remote-schema.ts
@@ -15,25 +15,9 @@ const resolvers = {
 
 const schema = createSchema({ typeDefs, resolvers });
 
-// Plugin to handle pre-parsed body from Express middleware
-const useExpressParsedBody: Plugin = {
-  onRequestParse({ serverContext, setRequestParser }) {
-    const expressReq = (serverContext as any)?.req;
-    if (expressReq?.body) {
-      setRequestParser(async () => ({
-        query: expressReq.body.query,
-        variables: expressReq.body.variables,
-        operationName: expressReq.body.operationName,
-        extensions: expressReq.body.extensions,
-      }));
-    }
-  },
-};
-
 const yoga = createYoga({
   schema,
   graphqlEndpoint: '*',
-  plugins: [useExpressParsedBody],
 });
 
 export default async function handler(req: Request, res: Response) {
