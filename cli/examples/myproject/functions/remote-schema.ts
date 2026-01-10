4f9a11c865deffd88b838161fc7a31756053a14f
David Barroso <dbarrosop@dravetech.com>
2026-01-10 17:12:48 +0100

    asd

--- cli/examples/myproject/functions/remote-schema.ts
+++ cli/examples/myproject/functions/remote-schema.ts
@@ -36,25 +36,23 @@ const yoga = createYoga({
   plugins: [useExpressParsedBody],
 });
 
-export default yoga;
+export default async function handler(req: Request, res: Response) {
+  const url = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
 
-// export default async function handler(req: Request, res: Response) {
-//   const url = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+  const response = await yoga.fetch(
+    url,
+    {
+      method: req.method,
+      headers: req.headers as HeadersInit,
+    },
+    { req } // serverContext - makes req.body available to the plugin
+  );
 
-//   const response = await yoga.fetch(
-//     url,
-//     {
-//       method: req.method,
-//       headers: req.headers as HeadersInit,
-//     },
-//     { req } // serverContext - makes req.body available to the plugin
-//   );
+  res.status(response.status);
+  response.headers.forEach((value, key) => {
+    res.setHeader(key, value);
+  });
 
-//   res.status(response.status);
-//   response.headers.forEach((value, key) => {
-//     res.setHeader(key, value);
-//   });
-
-//   const body = await response.text();
-//   res.send(body);
-// }
+  const body = await response.text();
+  res.send(body);
+}
