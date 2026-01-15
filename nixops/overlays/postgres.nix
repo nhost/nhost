0dcf91c7d560ef873a77dc84ee2fc8b9b2898a8e
David Barroso <dbarrosop@dravetech.com>
2026-01-15 08:27:44 +0100

    asd

--- nixops/overlays/postgres.nix
+++ nixops/overlays/postgres.nix
@@ -1,4 +1,58 @@
 final: prev: rec {
+  postgresql_14_20 = (prev.postgresql_14.override { systemdSupport = false; }).overrideAttrs
+    (finalAttrs: previousAttrs: rec {
+      pname = "postgresql";
+      version = "14.20";
+
+      src = final.fetchurl {
+        url = "mirror://postgresql/source/v${version}/${pname}-${version}.tar.bz2";
+        hash = "sha256-dSfxDxZAdhvCgK2X0QXShtDPcuVNNteM9o5eX3UrZGs=";
+      };
+
+      doCheck = false;
+      doInstallCheck = false;
+    });
+
+  postgresql_14_20-client = final.stdenv.mkDerivation {
+    pname = "postgresql-client";
+    version = postgresql_14_20.version;
+
+    phases = [ "installPhase" ];
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp ${postgresql_14_20}/bin/psql $out/bin/
+      cp ${postgresql_14_20}/bin/pg_dump $out/bin/
+      cp ${postgresql_14_20}/bin/pg_dumpall $out/bin/
+      cp ${postgresql_14_20}/bin/pg_restore $out/bin/
+    '';
+  };
+
+  postgresql_15_15 = (prev.postgresql_15.override { systemdSupport = false; }).overrideAttrs (finalAttrs: previousAttrs: rec {
+    pname = "postgresql";
+    version = "15.15";
+
+    src = final.fetchurl {
+      url = "mirror://postgresql/source/v${version}/${pname}-${version}.tar.bz2";
+      hash = "sha256-V1Oq64sJy/YQFveKppv1y98BtDJj8BDL8WjIKJYhOqo=";
+    };
+  });
+
+  postgresql_15_15-client = final.stdenv.mkDerivation {
+    pname = "postgresql-client";
+    version = postgresql_15_15.version;
+
+    phases = [ "installPhase" ];
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp ${postgresql_15_15}/bin/psql $out/bin/
+      cp ${postgresql_15_15}/bin/pg_dump $out/bin/
+      cp ${postgresql_15_15}/bin/pg_dumpall $out/bin/
+      cp ${postgresql_15_15}/bin/pg_restore $out/bin/
+    '';
+  };
+
   postgresql_16_11 = (prev.postgresql_16.override { systemdSupport = false; }).overrideAttrs (finalAttrs: previousAttrs: rec {
     pname = "postgresql";
     version = "16.11";
