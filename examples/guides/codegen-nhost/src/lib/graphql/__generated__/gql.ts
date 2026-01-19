121f45a2ffd18060b096d61f62d211c72c150bc6
Godefroy <github.com@skreo.net>
2026-01-19 12:59:54 +0100

    fix(codegen): Include fragments in queries (#3746)
    Co-authored-by: David Barroso <dbarrosop@dravetech.com>

--- examples/guides/codegen-nhost/src/lib/graphql/__generated__/gql.ts
+++ examples/guides/codegen-nhost/src/lib/graphql/__generated__/gql.ts
@@ -15,10 +15,10 @@ import * as types from './graphql';
  * Learn more about it here: https://the-guild.dev/graphql/codegen/plugins/presets/preset-client#reducing-bundle-size
  */
 type Documents = {
-  'query GetNinjaTurtlesWithComments {\n  ninjaTurtles {\n    id\n    name\n    description\n    createdAt\n    updatedAt\n    comments {\n      id\n      comment\n      createdAt\n      user {\n        id\n        displayName\n        email\n      }\n    }\n  }\n}\n\nmutation AddComment($ninjaTurtleId: uuid!, $comment: String!) {\n  insertComment(object: {ninjaTurtleId: $ninjaTurtleId, comment: $comment}) {\n    id\n    comment\n    createdAt\n    ninjaTurtleId\n  }\n}': typeof types.GetNinjaTurtlesWithCommentsDocument;
+  'query GetNinjaTurtlesWithComments {\n  ninjaTurtles {\n    id\n    name\n    description\n    createdAt\n    updatedAt\n    comments {\n      ...CommentDetails\n    }\n  }\n}\n\nmutation AddComment($ninjaTurtleId: uuid!, $comment: String!) {\n  insertComment(object: {ninjaTurtleId: $ninjaTurtleId, comment: $comment}) {\n    id\n    comment\n    createdAt\n    ninjaTurtleId\n  }\n}\n\nfragment CommentDetails on comments {\n  id\n  comment\n  createdAt\n  user {\n    id\n    displayName\n    email\n  }\n}': typeof types.GetNinjaTurtlesWithCommentsDocument;
 };
 const documents: Documents = {
-  'query GetNinjaTurtlesWithComments {\n  ninjaTurtles {\n    id\n    name\n    description\n    createdAt\n    updatedAt\n    comments {\n      id\n      comment\n      createdAt\n      user {\n        id\n        displayName\n        email\n      }\n    }\n  }\n}\n\nmutation AddComment($ninjaTurtleId: uuid!, $comment: String!) {\n  insertComment(object: {ninjaTurtleId: $ninjaTurtleId, comment: $comment}) {\n    id\n    comment\n    createdAt\n    ninjaTurtleId\n  }\n}':
+  'query GetNinjaTurtlesWithComments {\n  ninjaTurtles {\n    id\n    name\n    description\n    createdAt\n    updatedAt\n    comments {\n      ...CommentDetails\n    }\n  }\n}\n\nmutation AddComment($ninjaTurtleId: uuid!, $comment: String!) {\n  insertComment(object: {ninjaTurtleId: $ninjaTurtleId, comment: $comment}) {\n    id\n    comment\n    createdAt\n    ninjaTurtleId\n  }\n}\n\nfragment CommentDetails on comments {\n  id\n  comment\n  createdAt\n  user {\n    id\n    displayName\n    email\n  }\n}':
     types.GetNinjaTurtlesWithCommentsDocument,
 };
 
@@ -40,8 +40,8 @@ export function graphql(source: string): unknown;
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
 export function graphql(
-  source: 'query GetNinjaTurtlesWithComments {\n  ninjaTurtles {\n    id\n    name\n    description\n    createdAt\n    updatedAt\n    comments {\n      id\n      comment\n      createdAt\n      user {\n        id\n        displayName\n        email\n      }\n    }\n  }\n}\n\nmutation AddComment($ninjaTurtleId: uuid!, $comment: String!) {\n  insertComment(object: {ninjaTurtleId: $ninjaTurtleId, comment: $comment}) {\n    id\n    comment\n    createdAt\n    ninjaTurtleId\n  }\n}',
-): (typeof documents)['query GetNinjaTurtlesWithComments {\n  ninjaTurtles {\n    id\n    name\n    description\n    createdAt\n    updatedAt\n    comments {\n      id\n      comment\n      createdAt\n      user {\n        id\n        displayName\n        email\n      }\n    }\n  }\n}\n\nmutation AddComment($ninjaTurtleId: uuid!, $comment: String!) {\n  insertComment(object: {ninjaTurtleId: $ninjaTurtleId, comment: $comment}) {\n    id\n    comment\n    createdAt\n    ninjaTurtleId\n  }\n}'];
+  source: 'query GetNinjaTurtlesWithComments {\n  ninjaTurtles {\n    id\n    name\n    description\n    createdAt\n    updatedAt\n    comments {\n      ...CommentDetails\n    }\n  }\n}\n\nmutation AddComment($ninjaTurtleId: uuid!, $comment: String!) {\n  insertComment(object: {ninjaTurtleId: $ninjaTurtleId, comment: $comment}) {\n    id\n    comment\n    createdAt\n    ninjaTurtleId\n  }\n}\n\nfragment CommentDetails on comments {\n  id\n  comment\n  createdAt\n  user {\n    id\n    displayName\n    email\n  }\n}',
+): (typeof documents)['query GetNinjaTurtlesWithComments {\n  ninjaTurtles {\n    id\n    name\n    description\n    createdAt\n    updatedAt\n    comments {\n      ...CommentDetails\n    }\n  }\n}\n\nmutation AddComment($ninjaTurtleId: uuid!, $comment: String!) {\n  insertComment(object: {ninjaTurtleId: $ninjaTurtleId, comment: $comment}) {\n    id\n    comment\n    createdAt\n    ninjaTurtleId\n  }\n}\n\nfragment CommentDetails on comments {\n  id\n  comment\n  createdAt\n  user {\n    id\n    displayName\n    email\n  }\n}'];
 
 export function graphql(source: string) {
   return (documents as any)[source] ?? {};
