1d6a1a1d0781d4c7a31ad5dbbdf728f88c59258a
David BM <correodelnino@gmail.com>
2026-02-20 15:18:29 +0100

    wip: fix type errors

--- dashboard/src/features/orgs/projects/database/dataGrid/components/DataBrowserGrid/DataBrowserGrid.tsx
+++ dashboard/src/features/orgs/projects/database/dataGrid/components/DataBrowserGrid/DataBrowserGrid.tsx
@@ -54,7 +54,7 @@ export interface DataBrowserGridProps extends Partial<DataGridProps> {}
 export function createDataGridColumn(
   column: NormalizedQueryDataRow,
   isEditable: boolean = true,
-): DataBrowserGridColumnDef {
+): DataBrowserGridColumnDef<UnknownDataGridRow, any> {
   const { normalizedDefaultValue, custom: isDefaultValueCustom } =
     normalizeDefaultValue(column.column_default);
 
