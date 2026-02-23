ba2b36688eca61b73d0b82835cf42b8056ae198c
David BM <correodelnino@gmail.com>
2026-02-23 16:47:11 +0100

    fix: refactor

--- dashboard/src/features/orgs/projects/database/dataGrid/hooks/useDataBrowserActions/useDataBrowserActions.tsx
+++ dashboard/src/features/orgs/projects/database/dataGrid/hooks/useDataBrowserActions/useDataBrowserActions.tsx
@@ -6,7 +6,7 @@ import { useDialog } from '@/components/common/DialogProvider';
 import { FormActivityIndicator } from '@/components/form/FormActivityIndicator';
 import { Badge } from '@/components/ui/v3/badge';
 import { InlineCode } from '@/components/ui/v3/inline-code';
-import { useDeleteTableWithToastMutation } from '@/features/orgs/projects/database/dataGrid/hooks/useDeleteTableMutation';
+import { useDeleteDatabaseObjectWithToastMutation } from '@/features/orgs/projects/database/dataGrid/hooks/useDeleteDatabaseObjectMutation';
 import { isNotEmptyValue } from '@/lib/utils';
 
 const CreateTableForm = dynamic(
@@ -98,7 +98,8 @@ export function useDataBrowserActions({
     query: { orgSlug, appSubdomain },
   } = router;
 
-  const { mutateAsync: deleteTable } = useDeleteTableWithToastMutation();
+  const { mutateAsync: deleteDatabaseObject } =
+    useDeleteDatabaseObjectWithToastMutation();
 
   const [removableTable, setRemovableTable] = useState<string>();
   const [optimisticlyRemovedTable, setOptimisticlyRemovedTable] =
@@ -134,7 +135,7 @@ export function useDataBrowserActions({
           ? allObjects[nextTableIndex]
           : null;
 
-      await deleteTable({ schema, table, type: 'BASE TABLE' });
+      await deleteDatabaseObject({ schema, table, type: 'BASE TABLE' });
       queryClient.removeQueries({
         queryKey: [`${dataSourceSlug}.${schema}.${table}`],
       });
--- dashboard/src/features/orgs/projects/database/dataGrid/hooks/useDeleteDatabaseObjectMutation/useDeleteDatabaseObjectWithToastMutation.ts
+++ dashboard/src/features/orgs/projects/database/dataGrid/hooks/useDeleteDatabaseObjectMutation/useDeleteDatabaseObjectWithToastMutation.ts
@@ -1,25 +1,25 @@
 import { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
-import type { UseDeleteTableMutationOptions } from '@/features/orgs/projects/database/dataGrid/hooks/useDeleteTableMutation';
+import type { UseDeleteDatabaseObjectMutationOptions } from '@/features/orgs/projects/database/dataGrid/hooks/useDeleteDatabaseObjectMutation';
 import { showLoadingToast, triggerToast } from '@/utils/toast';
-import useDeleteTableMutation from './useDeleteTableMutation';
+import useDeleteDatabaseObjectMutation from './useDeleteDatabaseObjectMutation';
 
-export interface UseDeleteTableWithToastMutationOptions
-  extends UseDeleteTableMutationOptions {}
+export interface UseDeleteDatabaseObjectWithToastMutationOptions
+  extends UseDeleteDatabaseObjectMutationOptions {}
 
 /**
- * This hook is a wrapper around a fetch call that deletes one or more tables
- * from the schema. It also shows toast messages based on the result of the
- * mutation.
+ * This hook is a wrapper around a fetch call that deletes one or more database
+ * objects from the schema. It also shows toast messages based on the result of
+ * the mutation.
  *
  * @param options - Options to use for the mutation.
  * @returns The result of the mutation.
  */
-export default function useDeleteTableWithToastMutation(
-  options: UseDeleteTableWithToastMutationOptions = {},
+export default function useDeleteDatabaseObjectWithToastMutation(
+  options: UseDeleteDatabaseObjectWithToastMutationOptions = {},
 ) {
   const [toastId, setToastId] = useState<string>();
-  const { status, error, ...rest } = useDeleteTableMutation(options);
+  const { status, error, ...rest } = useDeleteDatabaseObjectMutation(options);
 
   useEffect(() => {
     if (status === 'loading') {
