e914f1a2df5a4832b881c08fd9092df3238bc6e0
David BM <correodelnino@gmail.com>
2026-03-20 11:17:34 +0100

    fix: force dark mode regardless of OS theme preference (#112)

--- landing/src/pages/_document.tsx
+++ landing/src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="dark">
       <Head>
         <link
           rel="preload"
--- landing/tailwind.config.js
+++ landing/tailwind.config.js
@@ -2,6 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
