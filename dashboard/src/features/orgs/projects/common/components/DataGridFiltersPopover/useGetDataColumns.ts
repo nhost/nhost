be69af245e6c7cb50a8c183b0ecd3b075ec66164
David BM <correodelnino@gmail.com>
2026-03-24 11:54:19 +0100

    fix: type error in useGetDataColumns

--- dashboard/src/features/orgs/projects/common/components/DataGridFiltersPopover/useGetDataColumns.ts
+++ dashboard/src/features/orgs/projects/common/components/DataGridFiltersPopover/useGetDataColumns.ts
@@ -1,7 +1,10 @@
+import type { UnknownDataGridRow } from '@/features/orgs/projects/storage/dataGrid/components/DataGrid';
 import { SELECTION_COLUMN_ID } from '@/features/orgs/projects/storage/dataGrid/components/DataGrid/useDataGrid';
 import { useDataGridConfig } from '@/features/orgs/projects/storage/dataGrid/components/DataGridConfigProvider';
 
-export function useGetDataColumns<T = unknown>() {
+export function useGetDataColumns<
+  T extends UnknownDataGridRow = UnknownDataGridRow,
+>() {
   const { getAllColumns } = useDataGridConfig<T>();
   const columns = getAllColumns().filter(
     ({ id }) => id !== SELECTION_COLUMN_ID,
