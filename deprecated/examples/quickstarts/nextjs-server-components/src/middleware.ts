676fea9ded4846ba328b18ef0943cd737762a013
David Barroso <dbarrosop@dravetech.com>
2026-03-25 18:29:54 +0100

    chore(docs): remove all deprecated packages except stripe integration (#4054)

--- deprecated/examples/quickstarts/nextjs-server-components/src/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { manageAuthSession } from '@utils/nhost'
-import { NextRequest, NextResponse } from 'next/server'
-
-export async function middleware(request: NextRequest) {
-  return manageAuthSession(request, () =>
-    NextResponse.redirect(new URL('/auth/sign-in', request.url))
-  )
-}
--- deprecated/integrations/react-urql/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: '../../config/.eslintrc.js',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname
-  }
-}
