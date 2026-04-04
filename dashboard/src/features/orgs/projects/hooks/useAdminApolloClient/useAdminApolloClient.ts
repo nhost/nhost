3e39a62a700df77523bc5f0bd68f0b860af97391
syed waheed <waheedsyed291@gmail.com>
2026-04-04 09:18:30 +0530

    refactor(useAdminApolloClient): remove unnecessary Apollo Client setup

--- dashboard/src/features/orgs/projects/hooks/useAdminApolloClient/useAdminApolloClient.ts
+++ dashboard/src/features/orgs/projects/hooks/useAdminApolloClient/useAdminApolloClient.ts
@@ -1,37 +1,7 @@
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
-import { useMemo } from 'react';
-import { generateAppServiceUrl } from '@/features/orgs/projects/common/utils/generateAppServiceUrl';
-import { useProject } from '@/features/orgs/projects/hooks/useProject';
-import { isNotEmptyValue } from '@/lib/utils';
-import { getHasuraAdminSecret } from '@/utils/env';
+import {useRemoteApplicationGQLClient}from '@/features/orgs/hooks/useRemoteApplicationGQLClient';
 
 export default function useAdminApolloClient() {
-  const { project } = useProject();
-
-  const adminClient = useMemo(() => {
-    if (isNotEmptyValue(project)) {
-      const serviceUrl = generateAppServiceUrl(
-        project.subdomain,
-        project.region,
-        'graphql',
-      );
-      const projectAdminSecret = project.config!.hasura.adminSecret;
-
-      return new ApolloClient({
-        cache: new InMemoryCache(),
-        link: new HttpLink({
-          uri: serviceUrl,
-          headers: {
-            'x-hasura-admin-secret':
-              process.env.NEXT_PUBLIC_ENV === 'dev'
-                ? getHasuraAdminSecret()
-                : projectAdminSecret,
-          },
-        }),
-      });
-    }
-    return new ApolloClient({ cache: new InMemoryCache() });
-  }, [project]);
+  const adminClient = useRemoteApplicationGQLClient();
 
   return {
     adminClient,
