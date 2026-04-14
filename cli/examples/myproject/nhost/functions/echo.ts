9f4114fadc1d3de4342a25f9e68b8aa3c63c2ce0
David Barroso <dbarrosop@dravetech.com>
2026-04-14 08:18:05 +0200

    asd

--- cli/examples/myproject/nhost/functions/echo.ts
+++ cli/examples/myproject/nhost/functions/echo.ts
@@ -8,6 +8,6 @@ export default (req: Request, res: Response) => {
     node: process.version,
     arch: process.arch,
     invocationId: req.invocationId, // internal to cloud infrastructure
-    asd: '4',
+    asd: '5',
   });
 };
