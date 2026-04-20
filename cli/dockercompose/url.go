9fb5eca7bdc8e794c4c749e76936a578f6a6a9db
David BM <correodelnino@gmail.com>
2026-04-20 15:55:13 +0200

    feat: add needed env vars to CLI

--- cli/dockercompose/url.go
+++ cli/dockercompose/url.go
@@ -2,6 +2,11 @@ package dockercompose
 
 import "fmt"
 
+const (
+	schemeWS  = "ws"
+	schemeWSS = "wss"
+)
+
 func URL(host, service string, port uint, useTLS bool) string {
 	if useTLS && port == 443 {
 		return fmt.Sprintf("https://%s.%s.local.nhost.run", host, service)
@@ -12,6 +17,21 @@ func URL(host, service string, port uint, useTLS bool) string {
 	protocol := schemeHTTP
 	if useTLS {
 		protocol = schemeHTTPS
+	}
+
+	return fmt.Sprintf("%s://%s.%s.local.nhost.run:%d", protocol, host, service, port)
+}
+
+func WebsocketURL(host, service string, port uint, useTLS bool) string {
+	if useTLS && port == 443 {
+		return fmt.Sprintf("wss://%s.%s.local.nhost.run", host, service)
+	} else if !useTLS && port == 80 {
+		return fmt.Sprintf("ws://%s.%s.local.nhost.run", host, service)
+	}
+
+	protocol := schemeWS
+	if useTLS {
+		protocol = schemeWSS
 	}
 
 	return fmt.Sprintf("%s://%s.%s.local.nhost.run:%d", protocol, host, service, port)
