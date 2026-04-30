5421e41ac6b096936c4689df178ea81156671ab2
David BM <correodelnino@gmail.com>
2026-04-30 14:06:07 +0200

    fix(dashboard): remove graphite dev assistant from topbar

--- dashboard/src/features/orgs/projects/ai/DevAssistant/state/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './messages';
-export { default as messagesState } from './messages';
-export { default as projectMessagesState } from './projectMessages';
-export { default as sessionIDState } from './session';
