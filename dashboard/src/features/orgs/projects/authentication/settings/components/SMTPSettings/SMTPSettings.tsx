0cd2f1d66e71e089555dcd8143e9794a24249455
47th <vardhmanbengani+47th@icloud.com>
2026-05-13 10:40:27 +0530

    fix(dashboard): make SMTP and Postmark Settings layout mobile responsive

--- dashboard/src/features/orgs/projects/authentication/settings/components/SMTPSettings/SMTPSettings.tsx
+++ dashboard/src/features/orgs/projects/authentication/settings/components/SMTPSettings/SMTPSettings.tsx
@@ -131,7 +131,7 @@ export default function SMTPSettings() {
           title="SMTP Settings"
           description="Configure your SMTP settings to send emails from your email domain."
           submitButtonText="Save"
-          className="grid grid-cols-9 gap-4"
+          className="grid grid-cols-1 lg:grid-cols-9 gap-4"
           slotProps={{
             submitButton: {
               disabled: !isDirty,
