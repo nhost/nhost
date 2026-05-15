3adb5d21afd81b0783d24242af69b1e5a7e5c7bd
David Barroso <dbarrosop@dravetech.com>
2026-05-15 15:15:06 +0200

    asd

--- services/auth/go/migrations/postgres/00023_user_new_phone_number.up.sql
+++ services/auth/go/migrations/postgres/00023_user_new_phone_number.up.sql
@@ -1,2 +1,8 @@
 ALTER TABLE auth.users
     ADD COLUMN new_phone_number text;
+
+UPDATE auth.users
+SET new_phone_number = phone_number,
+    phone_number = NULL
+WHERE phone_number IS NOT NULL
+  AND phone_number_verified = false;
