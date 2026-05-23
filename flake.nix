329c0678d9ab845927c25b3c6bb85778a0af4650
David Barroso <6246622+dbarrosop@users.noreply.github.com>
2026-05-23 19:31:02 +0200

    asd

--- flake.nix
+++ flake.nix
@@ -309,6 +309,19 @@
             ];
           };
 
+          security-updates = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              # pnpm audit --fix=update
+              nodejs
+              pnpm
+
+              # govulncheck-wrapper -fix → go get / go mod tidy / go mod vendor
+              go
+              govulncheck
+              self.packages.${system}.govulncheck-wrapper
+            ];
+          };
+
           skopeo = pkgs.mkShell {
             buildInputs = with pkgs; [
               skopeo
