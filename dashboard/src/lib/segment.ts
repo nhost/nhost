7f39b3e8003d1063d74117a40fb24c6241bb50d6
Nuno Pato <nunopato@gmail.com>
2026-06-09 19:37:06 +0000

    asd

--- dashboard/src/lib/segment.ts
+++ dashboard/src/lib/segment.ts
@@ -1,10 +1,9 @@
+import { AnalyticsBrowser, type ID } from '@segment/analytics-next';
 import { isPlatform } from '@/utils/env';
 import { isDevOrStaging } from '@/utils/helpers';
-import { AnalyticsBrowser, type ID } from '@segment/analytics-next';
 
 export const analytics = AnalyticsBrowser.load(
   {
-    cdnURL: process.env.NEXT_PUBLIC_SEGMENT_CDN_URL!,
     writeKey: process.env.NEXT_PUBLIC_ANALYTICS_WRITE_KEY!,
   },
   {
