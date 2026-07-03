569bef58f42e653f5620b8e5895688c324c37903
meh. <meh@schizofreni.co>
2026-07-03 10:36:15 +0200

    fix(cli): make sure local files belong to the user running nhost (#4538)
    Co-authored-by: David Barroso <6246622+dbarrosop@users.noreply.github.com>

--- cli/dockercompose/constellation.go
+++ cli/dockercompose/constellation.go
@@ -16,6 +16,7 @@ func constellation( //nolint:funlen
 	httpPort uint,
 	nhostFolder string,
 	image string,
+	hostOS string,
 ) (*Service, error) {
 	envars, err := appconfig.ConstellationEnv(
 		cfg,
@@ -72,6 +73,7 @@ func constellation( //nolint:funlen
 		Networks: networkAliases("constellation-service"),
 		Ports:    nil,
 		Restart:  "always",
+		User:     hostUserSpec(hostOS),
 		Volumes: []Volume{
 			{
 				Type:     "bind",
