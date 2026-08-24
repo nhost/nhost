24f7e519dce70b3041bfa9113ef116af4697689d
robertkasza <robert.kasza@bishop-co.com>
2026-08-24 11:28:21 +0200

    feat(dashboard): Clean up old sidebar

--- dashboard/src/features/orgs/layout/OrganizationLayout/OrganizationLayout.tsx
+++ dashboard/src/features/orgs/layout/OrganizationLayout/OrganizationLayout.tsx
@@ -11,7 +11,7 @@ export default function OrganizationLayout({
   ...authenticatedLayoutProps
 }: OrganizationLayoutProps) {
   return (
-    <AuthenticatedLayout {...authenticatedLayoutProps} withMainNav={false}>
+    <AuthenticatedLayout {...authenticatedLayoutProps}>
       <OrganizationGuard>
         <main className="relative h-full min-w-0 flex-1 overflow-y-auto">
           {children}
