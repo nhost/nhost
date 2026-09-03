5d7fba5a7083b252e3307daa435d1778b4830a55
David Barroso <6246622+dbarrosop@users.noreply.github.com>
2026-09-03 09:16:48 +0200

    asd

--- dashboard/src/utils/hasura-api/orval.config.ts
+++ dashboard/src/utils/hasura-api/orval.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
       target: 'generated',
       schemas: 'generated/schemas',
       client: 'fetch',
-      biome: true,
+      formatter: 'biome',
       clean: true,
     },
   },
