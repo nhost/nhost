4fc32bb8a9d3058c586a37ab4ffd5bcff0e755b9
sepples <git@sepples.xyz>
2026-09-08 20:57:26 +1000

    removed

--- services/constellation/cmd/serve.go
+++ services/constellation/cmd/serve.go
@@ -250,7 +250,6 @@ func serveFlags() []cli.Flag {
 // CommandServe returns the "serve" CLI command, which starts the
 // constellation HTTP server.
 func CommandServe() *cli.Command {
-	fmt.Println("REMOVE ME")
 	return &cli.Command{ //nolint:exhaustruct
 		Name:   "serve",
 		Usage:  "Serve the application",
